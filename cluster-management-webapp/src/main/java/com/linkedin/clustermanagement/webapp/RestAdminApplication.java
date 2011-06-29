d32023e33c081a820dc9727631631eff73d5d7aa
Shi Lu <lushi04@gmail.com>
2011-06-29 12:53:21 -0700

    Add jar target to cluster management web app, also add command line option to start the web app.

--- cluster-management-webapp/src/main/java/com/linkedin/clustermanagement/webapp/RestAdminApplication.java
+++ cluster-management-webapp/src/main/java/com/linkedin/clustermanagement/webapp/RestAdminApplication.java
@@ -1,5 +1,13 @@
 package com.linkedin.clustermanagement.webapp;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.restlet.Application;
 import org.restlet.Component;
 import org.restlet.Context;
@@ -20,9 +28,15 @@ import com.linkedin.clustermanagement.webapp.resources.HostedEntityResource;
 import com.linkedin.clustermanagement.webapp.resources.IdealStateResource;
 import com.linkedin.clustermanagement.webapp.resources.InstanceResource;
 import com.linkedin.clustermanagement.webapp.resources.InstancesResource;
+import com.linkedin.clustermanager.tools.ClusterSetup;
 
 public class RestAdminApplication extends Application
 {
+  public static final String HELP = "help";
+  public static final String ZKSERVERADDRESS = "zkSvr";
+  public static final String PORT = "port";
+  public static final int DEFAULT_PORT = 8100;
+  
   public RestAdminApplication()
   {
     super();
@@ -72,23 +86,88 @@ public class RestAdminApplication extends Application
     router.attach("", mainpage);
     return router;
   }
+  
+  public static void printUsage(Options cliOptions)
+  {
+    HelpFormatter helpFormatter = new HelpFormatter();
+    helpFormatter.printHelp("java " + RestAdminApplication.class.getName(), cliOptions);
+  }
 
+  @SuppressWarnings("static-access")
+  private static Options constructCommandLineOptions()
+  {
+    Option helpOption = OptionBuilder.withLongOpt(HELP)
+        .withDescription("Prints command-line options info").create();
+    helpOption.setArgs(0);
+    helpOption.setRequired(false);
+    helpOption.setArgName("print help message");
+    
+    Option zkServerOption = OptionBuilder.withLongOpt(ZKSERVERADDRESS)
+        .withDescription("Provide zookeeper address").create();
+    zkServerOption.setArgs(1);
+    zkServerOption.setRequired(true);
+    zkServerOption.setArgName("ZookeeperServerAddress(Required)");
+    
+    Option portOption = OptionBuilder.withLongOpt(PORT)
+    .withDescription("Provide web service port").create();
+    portOption.setArgs(1);
+    portOption.setRequired(false);
+    portOption.setArgName("web service port, default: "+ DEFAULT_PORT);
+        
+    Options options = new Options();
+    options.addOption(helpOption);
+    options.addOption(zkServerOption);
+    options.addOption(portOption);
+    
+    return options;
+  }
+  
+  public static void processCommandLineArgs(String[] cliArgs) throws Exception
+  {
+    CommandLineParser cliParser = new GnuParser();
+    Options cliOptions = constructCommandLineOptions();
+    CommandLine cmd = null;
+
+    try
+    {
+      cmd = cliParser.parse(cliOptions, cliArgs);
+    } 
+    catch (ParseException pe)
+    {
+      System.err.println("RestAdminApplication: failed to parse command-line options: "
+          + pe.toString());
+      printUsage(cliOptions);
+      System.exit(1);
+    }
+    int port = DEFAULT_PORT;
+    if(cmd.hasOption(HELP))
+    {
+      printUsage(cliOptions);
+      return;
+    }
+    else if(cmd.hasOption(PORT))
+    {
+      port = Integer.parseInt(cmd.getOptionValue(PORT));
+    }
+    // start web server with the zkServer address
+    Component component = new Component();
+    component.getServers().add(Protocol.HTTP, port);
+    Context applicationContext = component.getContext().createChildContext();
+    applicationContext.getAttributes().put("zkServer", cmd.getOptionValue(ZKSERVERADDRESS));
+
+    RestAdminApplication application = new RestAdminApplication(
+        applicationContext); 
+    // Attach the application to the component and start it
+    component.getDefaultHost().attach(application);
+    component.start();
+  }
   /**
    * @param args
    * @throws Exception
    */
   public static void main(String[] args) throws Exception
   {
-    Component component = new Component();
-    component.getServers().add(Protocol.HTTP, 8100);
-    Context applicationContext = component.getContext().createChildContext();
-    applicationContext.getAttributes().put("zkServer", "localhost:2188");
-    System.out.println("server started");
-    RestAdminApplication application = new RestAdminApplication(
-        applicationContext); // Attach the application to the
-    // component and start it
-    component.getDefaultHost().attach(application);
-    component.start();
+    processCommandLineArgs(args);
   }
 
 }

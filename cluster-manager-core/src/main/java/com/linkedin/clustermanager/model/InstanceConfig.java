cbaa693ca11e9491f79fcd935439777a5ba26937
Kishore Gopalakrishna <g.kishore@gmail.com>
2011-07-07 18:58:11 -0700

    Adding additional functionality to routingtableprovider. get all nodes  for a given group,state

--- cluster-manager-core/src/main/java/com/linkedin/clustermanager/model/InstanceConfig.java
+++ cluster-manager-core/src/main/java/com/linkedin/clustermanager/model/InstanceConfig.java
@@ -2,22 +2,52 @@ package com.linkedin.clustermanager.model;
 
 public class InstanceConfig
 {
-  String _hostName;
-  String _port;
+  // setting defaults to avoid lot of null checks
+  String _hostName = "UNK";
+  String _port = "-1";
+
   public String getHostName()
   {
     return _hostName;
   }
+
   public void setHostName(String hostName)
   {
     _hostName = hostName;
   }
+
   public String getPort()
   {
     return _port;
   }
+
   public void setPort(String port)
   {
     _port = port;
+  }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (obj instanceof InstanceConfig)
+    {
+      InstanceConfig that = (InstanceConfig) obj;
+
+      if (this._hostName.equals(that._hostName) && _port.equals(that._port))
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode()
+  {
+
+    StringBuffer sb = new StringBuffer();
+    sb.append(_hostName);
+    sb.append(_port);
+    return sb.toString().hashCode();
   }
 }
--- cluster-manager-core/src/test/java/com/linkedin/clustermanager/TestRoutingTable.java
+++ cluster-manager-core/src/test/java/com/linkedin/clustermanager/TestRoutingTable.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -27,7 +28,7 @@ public class TestRoutingTable
   }
 
   @Test
- public  void testSimple()
+  public void testSimple()
   {
     List<InstanceConfig> instances;
     RoutingTableProvider routingTable = new RoutingTableProvider();
@@ -35,63 +36,106 @@ public class TestRoutingTable
     List<ZNRecord> externalViewList = new ArrayList<ZNRecord>();
     ZNRecord record = new ZNRecord();
     externalViewList.add(record);
-    //one master
+    // one master
     record.setId("TESTDB");
     add(record, "TESTDB_0", "localhost_8900", "MASTER");
     routingTable.onExternalViewChange(externalViewList, changeContext);
-    
-    instances = routingTable.getInstances("TESTDB",
-        "TESTDB_0", "MASTER");
+
+    instances = routingTable.getInstances("TESTDB", "TESTDB_0", "MASTER");
     Assert.assertNotNull(instances);
     Assert.assertEquals(instances.size(), 1);
-    
-    //additions 
+
+    // additions
     add(record, "TESTDB_0", "localhost_8901", "MASTER");
     add(record, "TESTDB_1", "localhost_8900", "SLAVE");
-    
+
     routingTable.onExternalViewChange(externalViewList, changeContext);
-    instances = routingTable.getInstances("TESTDB",
-        "TESTDB_0", "MASTER");
+    instances = routingTable.getInstances("TESTDB", "TESTDB_0", "MASTER");
     Assert.assertNotNull(instances);
     Assert.assertEquals(instances.size(), 2);
-    
-    instances = routingTable.getInstances("TESTDB",
-        "TESTDB_1", "SLAVE");
+
+    instances = routingTable.getInstances("TESTDB", "TESTDB_1", "SLAVE");
     Assert.assertNotNull(instances);
     Assert.assertEquals(instances.size(), 1);
-    
-    //updates
+
+    // updates
     add(record, "TESTDB_0", "localhost_8901", "SLAVE");
     routingTable.onExternalViewChange(externalViewList, changeContext);
-    instances = routingTable.getInstances("TESTDB",
-        "TESTDB_0", "SLAVE");
+    instances = routingTable.getInstances("TESTDB", "TESTDB_0", "SLAVE");
     Assert.assertNotNull(instances);
     Assert.assertEquals(instances.size(), 1);
   }
+
   @Test
-  public void testStateUnitGroupDeletion(){
+  public void testStateUnitGroupDeletion()
+  {
     List<InstanceConfig> instances;
     RoutingTableProvider routingTable = new RoutingTableProvider();
     NotificationContext changeContext = null;
     List<ZNRecord> externalViewList = new ArrayList<ZNRecord>();
     ZNRecord record = new ZNRecord();
     externalViewList.add(record);
-    //one master
+    // one master
     record.setId("TESTDB");
     add(record, "TESTDB_0", "localhost_8900", "MASTER");
     routingTable.onExternalViewChange(externalViewList, changeContext);
-    instances = routingTable.getInstances("TESTDB",
-        "TESTDB_0", "MASTER");
+    instances = routingTable.getInstances("TESTDB", "TESTDB_0", "MASTER");
     Assert.assertNotNull(instances);
     Assert.assertEquals(instances.size(), 1);
-    
+
     externalViewList.clear();
     routingTable.onExternalViewChange(externalViewList, changeContext);
-    instances = routingTable.getInstances("TESTDB",
-        "TESTDB_0", "MASTER");
+    instances = routingTable.getInstances("TESTDB", "TESTDB_0", "MASTER");
     Assert.assertNotNull(instances);
-    Assert.assertEquals(instances.size(),0);
+    Assert.assertEquals(instances.size(), 0);
   }
+
+  @Test
+  public void testGetInstanceForAllStateUnits()
+  {
+    List<InstanceConfig> instancesList;
+    Set<InstanceConfig> instancesSet;
+    InstanceConfig instancesArray[];
+    RoutingTableProvider routingTable = new RoutingTableProvider();
+    NotificationContext changeContext = null;
+    List<ZNRecord> externalViewList = new ArrayList<ZNRecord>();
+    ZNRecord record = new ZNRecord();
+    externalViewList.add(record);
+    // one master
+    record.setId("TESTDB");
+    add(record, "TESTDB_0", "localhost_8900", "MASTER");
+    add(record, "TESTDB_1", "localhost_8900", "MASTER");
+    add(record, "TESTDB_2", "localhost_8900", "MASTER");
+    add(record, "TESTDB_3", "localhost_8900", "SLAVE");
+    add(record, "TESTDB_4", "localhost_8900", "SLAVE");
+    add(record, "TESTDB_5", "localhost_8900", "SLAVE");
+
+    add(record, "TESTDB_0", "localhost_8901", "SLAVE");
+    add(record, "TESTDB_1", "localhost_8901", "SLAVE");
+    add(record, "TESTDB_2", "localhost_8901", "SLAVE");
+    add(record, "TESTDB_3", "localhost_8901", "MASTER");
+    add(record, "TESTDB_4", "localhost_8901", "MASTER");
+    add(record, "TESTDB_5", "localhost_8901", "MASTER");
+    routingTable.onExternalViewChange(externalViewList, changeContext);
+    instancesList = routingTable.getInstances("TESTDB", "TESTDB_0", "MASTER");
+    Assert.assertNotNull(instancesList);
+    Assert.assertEquals(instancesList.size(), 1);
+    instancesSet = routingTable.getInstances("TESTDB", "MASTER");
+    Assert.assertNotNull(instancesSet);
+    Assert.assertEquals(instancesSet.size(), 2);
+    instancesSet = routingTable.getInstances("TESTDB", "SLAVE");
+    Assert.assertNotNull(instancesSet);
+    Assert.assertEquals(instancesSet.size(), 2);
+    instancesArray = new InstanceConfig[instancesSet.size()];
+    instancesSet.toArray(instancesArray);
+    Assert.assertEquals(instancesArray[0].getHostName(), "localhost");
+    Assert.assertEquals(instancesArray[0].getPort(), "8900");
+    Assert.assertEquals(instancesArray[1].getHostName(), "localhost");
+    Assert.assertEquals(instancesArray[1].getPort(), "8901");
+    
+
+  }
+
   private void add(ZNRecord record, String stateUnitKey, String instanceName,
       String state)
   {

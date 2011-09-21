6862741d91c657d1f516f9d004db05aed0e925dc
Jason Zhang <zzhang@linkedin.com>
2011-09-21 14:44:46 -0700

    update tests

--- cluster-manager-core/src/test/java/com/linkedin/clustermanager/TestDropResource.java
+++ cluster-manager-core/src/test/java/com/linkedin/clustermanager/TestDropResource.java
@@ -1,152 +1,59 @@
 package com.linkedin.clustermanager;
 
-import java.io.File;
-import java.util.Arrays;
 import java.util.List;
 
-import org.I0Itec.zkclient.IDefaultNameSpace;
-import org.I0Itec.zkclient.ZkServer;
-import org.testng.AssertJUnit;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.linkedin.clustermanager.ClusterDataAccessor.InstancePropertyType;
 import com.linkedin.clustermanager.agent.zk.ZKDataAccessor;
-import com.linkedin.clustermanager.agent.zk.ZNRecordSerializer;
-import com.linkedin.clustermanager.controller.ClusterManagerMain;
-import com.linkedin.clustermanager.mock.storage.DummyProcess;
-import com.linkedin.clustermanager.tools.ClusterSetup;
-import com.linkedin.clustermanager.tools.ClusterStateVerifier;
-import com.linkedin.clustermanager.tools.StateModelConfigGenerator;
-import com.linkedin.clustermanager.util.CMUtil;
 import com.linkedin.clustermanager.agent.zk.ZkClient;
+import com.linkedin.clustermanager.tools.ClusterStateVerifier;
+import com.linkedin.clustermanager.util.CMUtil;
 
-public class TestDropResource
+public class TestDropResource extends ZkStandAloneCMHandler
 {
-  void verifyEmptyCurrentState(ClusterDataAccessor accessor, ZkClient client, String clusterName, String instanceName, String dbName)
+  @Test
+  public void testDropResource() throws Exception
   {
-    String path = CMUtil.getInstancePropertyPath(clusterName, instanceName, InstancePropertyType.CURRENTSTATES);
-    List<String> subPaths = accessor.getInstancePropertySubPaths(instanceName, InstancePropertyType.CURRENTSTATES);
+    // add a resource group to be dropped
+    _setupTool.addResourceGroupToCluster(CLUSTER_NAME, "MyDB", 6, STATE_MODEL);
+    _setupTool.rebalanceStorageCluster(CLUSTER_NAME, "MyDB", 3);
+    Thread.sleep(10000);
     
-    for(String previousSessionId : subPaths)
+    boolean result = ClusterStateVerifier.VerifyClusterStates(ZK_ADDR, CLUSTER_NAME);
+    Assert.assertTrue(result);
+    
+    _setupTool.dropResourceGroupToCluster(CLUSTER_NAME, "MyDB");
+    Thread.sleep(10000);
+    
+    for (int i = 0; i < NODE_NR; i++)
     {
-      if(client.exists(path+"/"+previousSessionId+"/" + dbName))
+      verifyEmptyCurrentState(_controllerZkClient, CLUSTER_NAME, 
+                              "localhost_" + (START_PORT + i), "MyDB");
+    }
+  }
+
+  protected void verifyEmptyCurrentState(ZkClient zkClient, String clusterName, 
+                      String instanceName, String dbName)
+  {
+    ClusterDataAccessor accessor = new ZKDataAccessor(CLUSTER_NAME, zkClient);
+    String path = CMUtil
+        .getInstancePropertyPath(clusterName, instanceName, InstancePropertyType.CURRENTSTATES);
+    
+    List<String> subPaths = accessor
+        .getInstancePropertySubPaths(instanceName, InstancePropertyType.CURRENTSTATES);
+    
+    for (String previousSessionId : subPaths)
+    {
+      if (zkClient.exists(path+ "/" + previousSessionId + "/" + dbName))
       {
-        ZNRecord previousCurrentState = accessor.getInstanceProperty(instanceName, InstancePropertyType.CURRENTSTATES, previousSessionId, dbName);
-        AssertJUnit.assertTrue(previousCurrentState.getMapFields().size() == 0);
+        ZNRecord previousCurrentState = accessor.getInstanceProperty(instanceName, 
+                              InstancePropertyType.CURRENTSTATES, previousSessionId, dbName);
+
+        Assert.assertEquals(previousCurrentState.getMapFields().size(), 0);
       }
     }
   }
   
-  @Test
-  public void testInvocation() throws Exception
-  {
-    // Logger.getRootLogger().setLevel(Level.ERROR);
-    String logDir = "/tmp/logs";
-    String dataDir = "/tmp/dataDir";
-    new File(dataDir).delete();
-    IDefaultNameSpace defaultNameSpace = new IDefaultNameSpace()
-    {
-      @Override
-      public void createDefaultNameSpace(org.I0Itec.zkclient.ZkClient zkClient)
-      {
-        zkClient.deleteRecursive("/ESPRESSO_STORAGE_DROP");
-        zkClient.deleteRecursive("/relay-cluster-12345");
-      }
-    };
-    int port = 2181;
-    ZkServer zkServer = new ZkServer(dataDir, logDir, defaultNameSpace, port);
-    zkServer.start();
-    ZkClient zkClient = new ZkClient("localhost:2181");
-    zkClient.setZkSerializer(new ZNRecordSerializer());
-    ClusterDataAccessor accessor = new ZKDataAccessor("ESPRESSO_STORAGE_DROP", zkClient);
-    ClusterSetup setup = new ClusterSetup("localhost:" + port);
-    ClusterSetup
-        .processCommandLineArgs(createArgs("-zkSvr localhost:2181 -addCluster ESPRESSO_STORAGE_DROP"));
-    ClusterSetup
-        .processCommandLineArgs(createArgs("-zkSvr localhost:2181 -addCluster relay-cluster-12345"));
-    ClusterSetup
-        .processCommandLineArgs(createArgs("-zkSvr localhost:2181 -addResourceGroup ESPRESSO_STORAGE_DROP db-12345 50 MasterSlave"));
-    ClusterSetup
-        .processCommandLineArgs(createArgs("-zkSvr localhost:2181 -addNode ESPRESSO_STORAGE_DROP localhost:8900"));
-    ClusterSetup
-        .processCommandLineArgs(createArgs("-zkSvr localhost:2181 -addNode ESPRESSO_STORAGE_DROP localhost:8901"));
-    ClusterSetup
-        .processCommandLineArgs(createArgs("-zkSvr localhost:2181 -addNode ESPRESSO_STORAGE_DROP localhost:8902"));
-    ClusterSetup
-        .processCommandLineArgs(createArgs("-zkSvr localhost:2181 -addNode ESPRESSO_STORAGE_DROP localhost:8903"));
-    ClusterSetup
-        .processCommandLineArgs(createArgs("-zkSvr localhost:2181 -addNode ESPRESSO_STORAGE_DROP localhost:8904"));
-    ClusterSetup
-        .processCommandLineArgs(createArgs("-zkSvr localhost:2181 -rebalance ESPRESSO_STORAGE_DROP db-12345 2"));
-    startDummyProcess(createArgs("-zkSvr localhost:2181 -cluster ESPRESSO_STORAGE_DROP -host localhost -port 8900"));
-    startDummyProcess(createArgs("-zkSvr localhost:2181 -cluster ESPRESSO_STORAGE_DROP -host localhost -port 8901"));
-    startDummyProcess(createArgs("-zkSvr localhost:2181 -cluster ESPRESSO_STORAGE_DROP -host localhost -port 8902"));
-    startDummyProcess(createArgs("-zkSvr localhost:2181 -cluster ESPRESSO_STORAGE_DROP -host localhost -port 8903"));
-    startDummyProcess(createArgs("-zkSvr localhost:2181 -cluster ESPRESSO_STORAGE_DROP -host localhost -port 8904"));
-    startClusterManager(createArgs("-zkSvr localhost:2181 -cluster ESPRESSO_STORAGE_DROP"));
-    Thread.sleep(15000);
-    //Thread.currentThread().join();
-    setup.dropResourceGroupToCluster("ESPRESSO_STORAGE_DROP", "db-12345");
-    Thread.sleep(35000);
-    verifyEmptyCurrentState(accessor, zkClient, "ESPRESSO_STORAGE_DROP", "localhost_8900", "db-12345");
-    verifyEmptyCurrentState(accessor, zkClient, "ESPRESSO_STORAGE_DROP", "localhost_8901", "db-12345");
-    verifyEmptyCurrentState(accessor, zkClient, "ESPRESSO_STORAGE_DROP", "localhost_8902", "db-12345");
-    verifyEmptyCurrentState(accessor, zkClient, "ESPRESSO_STORAGE_DROP", "localhost_8903", "db-12345");
-    verifyEmptyCurrentState(accessor, zkClient, "ESPRESSO_STORAGE_DROP", "localhost_8904", "db-12345");
-    
-    zkServer.shutdown();
-  }
-
-  private static void startClusterManager(final String[] args)
-  {
-    Thread t = new Thread(new Runnable()
-    {
-      @Override
-      public void run()
-      {
-        try
-        {
-          ClusterManagerMain.main(args);
-        } catch (Exception e)
-        {
-          e.printStackTrace();
-        }
-      }
-    });
-    t.start();
-  }
-
-  private static void startDummyProcess(final String[] args)
-  {
-    Thread t = new Thread(new Runnable()
-    {
-      @Override
-      public void run()
-      {
-        try
-        {
-          DummyProcess.main(args);
-        } catch (Exception e)
-        {
-          e.printStackTrace();
-        }
-      }
-    });
-    t.start();
-    try
-    {
-      Thread.sleep(1000);
-    } catch (InterruptedException e)
-    {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-  }
-
-  private static String[] createArgs(String str)
-  {
-    String[] split = str.split("[ ]+");
-    System.out.println(Arrays.toString(split));
-    return split;
-  }
 }
--- cluster-manager-core/src/test/java/com/linkedin/clustermanager/TestDynamicFileClusterManager.java
+++ cluster-manager-core/src/test/java/com/linkedin/clustermanager/TestDynamicFileClusterManager.java
@@ -20,22 +20,22 @@ public class TestDynamicFileClusterManager extends FileClusterManagerHandler
     logger.info("RUN at " + new Date(System.currentTimeMillis()));
 
     // add a new db
-    mgmtTool.addResourceGroup(storageCluster, "MyDB", 6, stateModel);
-    rebalanceStorageCluster(storageCluster, "MyDB", 0);
+    _mgmtTool.addResourceGroup(CLUSTER_NAME, "MyDB", 6, STATE_MODEL);
+    rebalanceStorageCluster(CLUSTER_NAME, "MyDB", 0);
     Thread.sleep(5000);
     
     // verify current state
-    ZNRecord idealStates = accessor.getClusterProperty(ClusterPropertyType.IDEALSTATES, "MyDB");
-    ZNRecord curStates =  accessor.getInstanceProperty("localhost_12918", InstancePropertyType.CURRENTSTATES, 
-                                              manager.getSessionId(), "MyDB");
+    ZNRecord idealStates = _accessor.getClusterProperty(ClusterPropertyType.IDEALSTATES, "MyDB");
+    ZNRecord curStates =  _accessor.getInstanceProperty("localhost_12918", InstancePropertyType.CURRENTSTATES, 
+                                              _manager.getSessionId(), "MyDB");
     boolean result = verifyCurStateAndIdealState(curStates, idealStates, "localhost_12918", "MyDB");
     Assert.assertTrue(result);
     
     // drop db
-    mgmtTool.dropResourceGroup(storageCluster, "MyDB");
+    _mgmtTool.dropResourceGroup(CLUSTER_NAME, "MyDB");
     Thread.sleep(10000);
     
-    for (int i = 0; i < storageNodeNr; i++)
+    for (int i = 0; i < NODE_NR; i++)
     {
       String instanceName = "localhost_" + (12918 + i);
       verifyEmptyCurrentState(instanceName, "MyDB");

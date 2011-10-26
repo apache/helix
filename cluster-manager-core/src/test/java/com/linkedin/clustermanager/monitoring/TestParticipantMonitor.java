ad749321404ffdee80b661dbfbad38d107801a1e
Shi Lu <lushi04@gmail.com>
2011-10-25 17:49:51 -0700

    Fix participant monitor test

--- cluster-manager-core/src/test/java/com/linkedin/clustermanager/monitoring/TestParticipantMonitor.java
+++ cluster-manager-core/src/test/java/com/linkedin/clustermanager/monitoring/TestParticipantMonitor.java
@@ -35,20 +35,22 @@ public class TestParticipantMonitor
     
     monitor.addTransStatMonitorChangedListener(monitorListener);
     
+    int monitorNum = monitorListener._monitors.size();
+    
     StateTransitionContext cxt = new StateTransitionContext("cluster", "instance", "db_1","a-b");
     StateTransitionDataPoint data = new StateTransitionDataPoint(1000,1000,true);
     
     monitor.reportTransitionStat(cxt, data);
-    AssertJUnit.assertTrue(monitorListener._monitors.size() == 1);
+    AssertJUnit.assertTrue(monitorListener._monitors.size() == monitorNum + 1);
     
     data = new StateTransitionDataPoint(1000,500,true);
     monitor.reportTransitionStat(cxt, data);
-    AssertJUnit.assertTrue(monitorListener._monitors.size() == 1);
+    AssertJUnit.assertTrue(monitorListener._monitors.size() == monitorNum + 1);
     
     data = new StateTransitionDataPoint(1000,500,true);
     StateTransitionContext cxt2 = new StateTransitionContext("cluster", "instance", "db_2","a-b");
     monitor.reportTransitionStat(cxt2, data);
-    AssertJUnit.assertTrue(monitorListener._monitors.size() == 2);
+    AssertJUnit.assertTrue(monitorListener._monitors.size() == monitorNum + 2);
     
     AssertJUnit.assertFalse(cxt.equals(cxt2));
     AssertJUnit.assertFalse(cxt.equals(new Object()));
@@ -59,6 +61,6 @@ public class TestParticipantMonitor
     ParticipantMonitorListener monitorListener2 = new ParticipantMonitorListener();
     
     monitor.addTransStatMonitorChangedListener(monitorListener2);
-    AssertJUnit.assertTrue(monitorListener2._monitors.size() == 2);
+    AssertJUnit.assertTrue(monitorListener2._monitors.size() == monitorNum + 2);
   }
 }

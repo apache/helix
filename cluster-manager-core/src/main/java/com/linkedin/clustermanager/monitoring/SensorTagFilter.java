05c4267e4dbca792f04873687b300abbc1a0fd6d
slu2011 <lushi04@gmail.com>
2011-11-14 16:07:11 -0800

    checkin a missed file

---
+++ cluster-manager-core/src/main/java/com/linkedin/clustermanager/monitoring/SensorTagFilter.java
@@ -0,0 +1,45 @@
+package com.linkedin.clustermanager.monitoring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SensorTagFilter extends SensorContextTags
+{
+  public SensorTagFilter(Map<String, String> tagFilter)
+  {
+    super(tagFilter);
+  }
+  
+  public boolean matchs(SensorContextTags tags)
+  {
+    for(String key : _tagPairs.keySet())
+    {
+      if(!tags.getTags().containsKey(key))
+      {
+        return false;
+      }
+      if(!_tagPairs.get(key).equals("*"))
+      {
+        if(!_tagPairs.get(key).equalsIgnoreCase(tags.getTags().get(key)))
+        {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  
+  public SensorContextTags getFilteredTags(SensorContextTags tags)
+  {
+    if(!matchs(tags))
+    {
+      return null;
+    }
+    Map<String, String> resultTags = new HashMap<String, String>();
+    for(String key : _tagPairs.keySet())
+    {
+      resultTags.put(key, tags.getTags().get(key));
+    }
+    return new SensorContextTags(resultTags);
+  }
+}

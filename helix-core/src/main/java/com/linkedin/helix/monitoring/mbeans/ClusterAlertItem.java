6e31d210340f2240ff86221a2afc3903ce21ea9f
slu2011 <lushi04@gmail.com>
2012-02-06 10:32:02 -0800

    Fix a exception in the case that an Alert first get exposed, an exception is thrown due to the size of the Alert value elements is empty.

--- helix-core/src/main/java/com/linkedin/helix/monitoring/mbeans/ClusterAlertItem.java
+++ helix-core/src/main/java/com/linkedin/helix/monitoring/mbeans/ClusterAlertItem.java
@@ -36,7 +36,14 @@ public class ClusterAlertItem implements ClusterAlertItemMBean
   
   void refreshValues()
   {
-    _alertValue = Double.parseDouble(_valueAndStatus.getValue().getElements().get(0));
+    if(_valueAndStatus.getValue().getElements().size() > 0)
+    {
+      _alertValue = Double.parseDouble(_valueAndStatus.getValue().getElements().get(0));
+    }
+    else
+    {
+      _alertValue = 0;
+    }
     _alertFired = _valueAndStatus.isFired() ?  1 : 0;
   }
   @Override

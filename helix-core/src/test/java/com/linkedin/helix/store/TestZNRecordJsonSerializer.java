949f32af2dcf28e8244a252a000b5162e21f48a0
zzhang <zzhang@linkedin.com>
2012-03-26 10:32:09 -0700

    add more tests for znrecord list length bound

--- helix-core/src/test/java/com/linkedin/helix/store/TestZNRecordJsonSerializer.java
+++ helix-core/src/test/java/com/linkedin/helix/store/TestZNRecordJsonSerializer.java
@@ -24,17 +24,37 @@ public class TestZNRecordJsonSerializer extends ZkUnitTestBase
 
     ZNRecord record = new ZNRecord("node1");
     record.setSimpleField(ZNRecord.LIST_FIELD_BOUND, "" + 3);
-    List<String> list = Arrays.asList("one", "two", "three", "four");
-    record.setListField("list1", list);
+    List<String> list1 = Arrays.asList("one", "two", "three", "four");
+    List<String> list2 = Arrays.asList("a", "b", "c", "d");
+    List<String> list3 = Arrays.asList("x", "y");
+    record.setListField("list1", list1);
+    record.setListField("list2", list2);
+    record.setListField("list3", list3);
 
     ZKPropertyStore<ZNRecord> store = new ZKPropertyStore<ZNRecord>(new ZkClient(ZK_ADDR),
         new ZNRecordJsonSerializer(), "/" + testRoot);
 
     store.setProperty("node1", record);
     ZNRecord newRecord = store.getProperty("node1");
-    Assert.assertTrue(newRecord.getListField("list1").size() == 3);
+    list1 = newRecord.getListField("list1");
+    Assert.assertTrue(list1.size() == 3);
+    Assert.assertTrue(list1.contains("two"));
+    Assert.assertTrue(list1.contains("three"));
+    Assert.assertTrue(list1.contains("four"));
+
+    list2 = newRecord.getListField("list2");
+    Assert.assertTrue(list2.size() == 3);
+    Assert.assertTrue(list2.contains("b"));
+    Assert.assertTrue(list2.contains("c"));
+    Assert.assertTrue(list2.contains("d"));
+
+    list3 = newRecord.getListField("list3");
+    Assert.assertTrue(list3.size() == 2);
+    Assert.assertTrue(list3.contains("x"));
+    Assert.assertTrue(list3.contains("y"));
 
     System.out.println("START " + testRoot + " at " + new Date(System.currentTimeMillis()));
 
   }
 }
+

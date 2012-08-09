1b366c6c4be1548418888632a5f82e6c06a2b5a9
zzhang <zzhang@linkedin.com>
2012-08-09 14:46:50 -0700

    add ExternalCommand and a dummy test for testing executing external command using java

---
+++ helix-core/src/test/java/com/linkedin/helix/integration/TestExternalCmd.java
@@ -0,0 +1,36 @@
+package com.linkedin.helix.integration;
+
+import java.util.Date;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.linkedin.helix.ExternalCommand;
+import com.linkedin.helix.IntegrationTestHelper;
+import com.linkedin.helix.TestHelper;
+
+public class TestExternalCmd
+{
+
+  @Test
+  public void testExternalCmd() throws Exception
+  {
+    // Logger.getRootLogger().setLevel(Level.INFO);
+
+    String className = TestHelper.getTestClassName();
+    String methodName = TestHelper.getTestMethodName();
+    String testName = className + "_" + methodName;
+    
+    System.out.println("START " + testName + " at "
+        + new Date(System.currentTimeMillis()));
+
+    ExternalCommand cmd = IntegrationTestHelper.runCommandLineTest("dummy.sh");
+    String output = cmd.getStringOutput("UTF8");
+    int idx = output.indexOf("this is a dummy test for verify ExternalCommand works");
+    Assert.assertNotSame(idx, -1);
+    
+    System.out.println("END " + testName + " at "
+        + new Date(System.currentTimeMillis()));
+
+  }
+}

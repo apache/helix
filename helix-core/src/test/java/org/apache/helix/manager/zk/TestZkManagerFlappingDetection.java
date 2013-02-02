3b473667df9b063409cd0a7e135218cd30a3df21
slu2011 <lushi04@gmail.com>
2013-02-01 16:42:36 -0800

    Update test paramaters for testZkManagerFlappingdetection

--- helix-core/src/test/java/org/apache/helix/manager/zk/TestZkManagerFlappingDetection.java
+++ helix-core/src/test/java/org/apache/helix/manager/zk/TestZkManagerFlappingDetection.java
@@ -61,9 +61,10 @@ public class TestZkManagerFlappingDetection extends ZkIntegrationTestBase
       for(int i = 0;i < 4; i++)
       {
         ZkTestHelper.expireSession(zkClient);
-        Thread.sleep(500);
+        Thread.sleep(1000);
         if(i < 5)
         {
+          //System.out.println(i);
           Assert.assertTrue(manager.isConnected());
         }
       }
@@ -71,6 +72,7 @@ public class TestZkManagerFlappingDetection extends ZkIntegrationTestBase
       for(int i = 0; i < 20; i++)
       {
         Thread.sleep(500);
+        System.out.println(i);
         if(!manager.isConnected()) break;
       }
       Assert.assertFalse(manager.isConnected());
@@ -84,15 +86,6 @@ public class TestZkManagerFlappingDetection extends ZkIntegrationTestBase
     String instanceName = "localhost_" + (12918 + 1);
     final String clusterName = className + "_" + methodName + UUID.randomUUID();
 
-    TestHelper.setupCluster(clusterName, ZK_ADDR, 12918, // participant port
-                            "localhost", // participant name prefix
-                            "TestDB", // resource name prefix
-                            1, // resources
-                            10, // partitions per resource
-                            5, // number of nodes
-                            3, // replicas
-                            "MasterSlave",
-                            true); // do rebalance
     testDisconnectFlappingWindow2(instanceName, InstanceType.PARTICIPANT);
     testDisconnectFlappingWindow2("admin", InstanceType.ADMINISTRATOR);
   }
@@ -115,7 +108,7 @@ public class TestZkManagerFlappingDetection extends ZkIntegrationTestBase
     
     
       // flapping time window to 5 sec
-      System.setProperty("helixmanager.flappingTimeWindow", "10000");
+      System.setProperty("helixmanager.flappingTimeWindow", "15000");
       System.setProperty("helixmanager.maxDisconnectThreshold", "7");
       ZkHelixTestManager manager2 =
           new ZkHelixTestManager(clusterName,
@@ -130,12 +123,12 @@ public class TestZkManagerFlappingDetection extends ZkIntegrationTestBase
         Thread.sleep(500);
         Assert.assertTrue(manager2.isConnected());
       }
-      Thread.sleep(10000);
+      Thread.sleep(15000);
       // Old entries should be cleaned up
       for(int i = 0;i < 7; i++)
       {
         ZkTestHelper.expireSession(zkClient);
-        Thread.sleep(500);
+        Thread.sleep(1000);
         Assert.assertTrue(manager2.isConnected());
       }
       ZkTestHelper.disconnectSession(zkClient);

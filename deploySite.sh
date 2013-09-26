52717e40d523d1a5cd0314277493258540ed15e6
Kanak Biscuitwala <kanak.b@hotmail.com>
2013-09-25 23:30:39 -0700

    [HELIX-250] Add Kanak as committer to website, fix bug in deploy script

--- deploySite.sh
+++ deploySite.sh
@@ -21,4 +21,4 @@
 read -s -p "Enter Apache Username: " myusername
 echo ""
 read -s -p "Enter Apache Password: " mypassword
-mvn clean site-deploy scm-publish:publish-scm -Dusername=$myusername -Dpassword=$mypassword -DskipTests $@
+mvn clean site-deploy scm-publish:publish-scm -Dusername="$myusername" -Dpassword="$mypassword" -DskipTests $@

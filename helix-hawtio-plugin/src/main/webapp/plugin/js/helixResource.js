ff2e112ec64730f38ceb68b1c53369852c428eb9
ayolajayamaha <ayolajayamaha28@gmail.com>
2014-08-03 20:02:14 +0530

    fixing resourceManager

--- helix-hawtio-plugin/src/main/webapp/plugin/js/helixResource.js
+++ helix-hawtio-plugin/src/main/webapp/plugin/js/helixResource.js
@@ -4,9 +4,10 @@
  *
  * The main entry point for the Helix Resource Controllermodule
  * can list all resources in a cluster by calling /clusters/{clusterName}/resourceGroups
- * able to add a resource to cluster 
+ * able to add a resource to cluster
  *
  */
+
 var Helix = (function(Helix) {
 
     Helix.HelixResourceController = function($scope, $http) {
@@ -16,17 +17,15 @@ var Helix = (function(Helix) {
                 $scope.clusters = data;
             })
 
-        //Add a resource to cluster    
         $scope.addResources = function(cluster) {
             $http.post("http://localhost:8100/clusters/" + cluster.clusterName + "/resourceGroups", 'jsonParameters={"command":"addResource","resourceGroupName":"' + cluster.resourceName + '","partitions":"8","stateModelDefRef":"MasterSlave"}')
                 .success(function(data) {
-                    // $scope.cluster.clusterName = null;
+                    //$scope.cluster.clusterName = null;
                     // $scope.clusters = data;
                     $scope.listResources(cluster);
                 })
         }
 
-        //List all clusters
         $scope.listClusters = function() {
             $http.get("http://localhost:8100/clusters")
                 .success(function(data) {
@@ -34,7 +33,6 @@ var Helix = (function(Helix) {
                 })
         }
 
-        //List all resources in a cluster
         $scope.listResources = function(cluster) {
             console.log(cluster.clusterName)
             $http.get("http://localhost:8100/clusters/" + cluster.clusterName + "/resourceGroups")
@@ -42,6 +40,10 @@ var Helix = (function(Helix) {
                     $scope.resources = data;
                 })
         }
+
+
+
+    };
 
 
     return Helix;

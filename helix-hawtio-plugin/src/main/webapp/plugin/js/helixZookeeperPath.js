15f2e99a5d3dae75d23c93fe8958649d2ec8d93b
ayolajayamaha <ayolajayamaha28@gmail.com>
2014-08-09 19:58:21 +0530

    zookeeper Path Manager updating with coding cleaning and formatting

--- helix-hawtio-plugin/src/main/webapp/plugin/js/helixZookeeperPath.js
+++ helix-hawtio-plugin/src/main/webapp/plugin/js/helixZookeeperPath.js
@@ -2,9 +2,8 @@
  * @module Helix
  * @mail Helix
  *
- * The main entry point for the Helix Resource Controllermodule
- * can list all resources in a cluster by calling /clusters/{clusterName}/resourceGroups
- * able to add a resource to cluster
+ * The main entry point for the Helix Get information for zookeeper path
+ * using http://localhost:8100/zkPath/{ClusterName}
  *
  */
 
@@ -12,56 +11,21 @@ var Helix = (function(Helix) {
 
     Helix.HelixZookeeperPathController = function($scope, $http) {
 
+    	//listing clusters
         $http.get("http://localhost:8100/clusters")
             .success(function(data) {
                 $scope.clusters = data;
                 $scope.resource = null;
-            })
+            })        
 
-        $scope.addResources = function(cluster) {
-            $http.post("http://localhost:8100/clusters/" + cluster.clusterName + "/resourceGroups", 'jsonParameters={"command":"addResource","resourceGroupName":"' + cluster.resourceName + '","partitions":"8","stateModelDefRef":"MasterSlave"}')
-                .success(function(data) {
-                    $scope.cluster.clusterName = null;
-                    // $scope.clusters = data;
-                    $scope.listResources(cluster);
-                })
-        }
-
-        $scope.listClusters = function() {
-            $http.get("http://localhost:8100/clusters")
-                .success(function(data) {
-                    $scope.clusters = data;
-                    $scope.resource = null;
-                })
-        }
         
-        $scope.removeResource = function(cluster,resourceName) {
-            $http.delete("http://localhost:8100/clusters/" + cluster.clusterName + "/resourceGroups/"+resourceName)
+        //getting path from zkPath (zookeeper path)
+        $scope.showPath = function(cluster) {
+            $http.get("http://localhost:8100/zkPath/" + cluster.clusterName)
                 .success(function(data) {
-                    $scope.clusters = data;
-                    $scope.resource = null;
+                    $scope.data = data;
                 })
-        }
-        
-        //getInformation of resources /clusters/{clusterName}/resourceGroups/{resourceName}
-        $scope.getInformation = function(cluster,resourceName) {
-            $http.get("http://localhost:8100/clusters/" + cluster.clusterName + "/resourceGroups/"+resourceName)
-                .success(function(data) {
-                    $scope.resource = data;
-                })
-        }
-        
-        $scope.listResources = function(cluster) {
-            console.log(cluster.clusterName)
-            $http.get("http://localhost:8100/clusters/" + cluster.clusterName + "/resourceGroups")
-                .success(function(data) {
-                    $scope.resources = data;
-                    $scope.resource = null;
-                })
-        }
-
-
-
+        }  
     };
 
 

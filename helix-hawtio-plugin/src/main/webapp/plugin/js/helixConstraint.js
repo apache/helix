4b5792c222089adafbdc620a9c0c695831fba670
ayolajayamaha <ayolajayamaha28@gmail.com>
2014-08-10 16:40:01 +0530

    adding add and remove constraint manager features with alerting features

--- helix-hawtio-plugin/src/main/webapp/plugin/js/helixConstraint.js
+++ helix-hawtio-plugin/src/main/webapp/plugin/js/helixConstraint.js
@@ -2,9 +2,8 @@
  * @module Helix
  * @mail Helix
  *
- * The main entry point for the Helix Resource Controllermodule
- * can list all resources in a cluster by calling /clusters/{clusterName}/resourceGroups
- * able to add a resource to cluster
+ * The main entry point for the Helix Constraints module
+ * list/remove and add Constraints
  *
  */
 
@@ -18,14 +17,7 @@ var Helix = (function(Helix) {
                 $scope.resource = null;
             })
 
-        $scope.addResources = function(cluster) {
-            $http.post("http://localhost:8100/clusters/" + cluster.clusterName + "/resourceGroups", 'jsonParameters={"command":"addResource","resourceGroupName":"' + cluster.resourceName + '","partitions":"8","stateModelDefRef":"MasterSlave"}')
-                .success(function(data) {
-                    $scope.cluster.clusterName = null;
-                    // $scope.clusters = data;
-                    $scope.listResources(cluster);
-                })
-        }
+
 
         $scope.listClusters = function() {
             $http.get("http://localhost:8100/clusters")
@@ -35,31 +27,43 @@ var Helix = (function(Helix) {
                 })
         }
         
-        $scope.removeResource = function(cluster,resourceName) {
-            $http.delete("http://localhost:8100/clusters/" + cluster.clusterName + "/resourceGroups/"+resourceName)
-                .success(function(data) {
-                    $scope.clusters = data;
-                    $scope.resource = null;
-                })
-        }
-        
-        //getInformation of resources /clusters/{clusterName}/resourceGroups/{resourceName}
-        $scope.getInformation = function(cluster,resourceName) {
-            $http.get("http://localhost:8100/clusters/" + cluster.clusterName + "/resourceGroups/"+resourceName)
-                .success(function(data) {
-                    $scope.resource = data;
-                })
-        }
-        
-        $scope.listResources = function(cluster) {
-            console.log(cluster.clusterName)
-            $http.get("http://localhost:8100/clusters/" + cluster.clusterName + "/resourceGroups")
-                .success(function(data) {
-                    $scope.resources = data;
-                    $scope.resource = null;
-                })
-        }
 
+        
+        //Show all contraints using  /clusters/{clusterName}/constraints/{constraintType}
+        $scope.listConstraints = function(cluster) {
+            $http.get("http://localhost:8100/clusters/" + cluster.clusterName + "/constraints/"+cluster.constraintType)
+                .success(function(data) {
+                    $scope.constraints = data;
+                })
+        }
+        
+        //DELETE constraints by /clusters/MyCluster/constraints/MESSAGE_CONSTRAINT/MyConstraint
+        $scope.removeConstraint = function(cluster,constraintName) {          
+            $http.delete("http://localhost:8100/clusters/" + cluster.clusterName + "/constraints/"+cluster.constraintType+"/"+constraintName)
+                .success(function(data) {
+                    $scope.callback = constraintName + ' in cluster "'+cluster.clusterName+ '" is successfully removed ';
+                    $scope.listConstraints(cluster);
+                })
+                .error(function(data, status, headers, config) {
+                	console.log(data);
+                	log.info(data);
+                  });
+
+        }
+        
+        
+          $scope.addConstraints = function(cluster) {          
+            $http.post("http://localhost:8100/clusters/" + cluster.clusterName + "/constraints/"+cluster.constraintType,)
+                .success(function(data) {
+                    $scope.callback = constraintName + ' in cluster "'+cluster.clusterName+ '" is successfully removed ';
+                    $scope.listConstraints(cluster);
+                })
+                .error(function(data, status, headers, config) {
+                	console.log(data);
+                	log.info(data);
+                  });
+
+        }
 
 
     };

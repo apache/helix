46407b31dd1a1a99af35365b0ca8c7c3552005a5
Greg Brandt <brandt.greg@gmail.com>
2015-03-28 12:46:47 -0700

    Add apache license to helix-ui; 4 -> 2 spaces indent

--- helix-ui/src/main/java/org/apache/helix/ui/resource/AdminResource.java
+++ helix-ui/src/main/java/org/apache/helix/ui/resource/AdminResource.java
@@ -1,5 +1,24 @@
 package org.apache.helix.ui.resource;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import org.apache.helix.manager.zk.ZKUtil;
 import org.apache.helix.model.IdealState;
 import org.apache.helix.model.InstanceConfig;
@@ -14,168 +33,168 @@ import java.util.List;
 
 @Path("/admin")
 public class AdminResource {
-    private final ClientCache clientCache;
-    private final DataCache dataCache;
+  private final ClientCache clientCache;
+  private final DataCache dataCache;
 
-    public AdminResource(ClientCache clientCache, DataCache dataCache) {
-        this.clientCache = clientCache;
-        this.dataCache = dataCache;
+  public AdminResource(ClientCache clientCache, DataCache dataCache) {
+    this.clientCache = clientCache;
+    this.dataCache = dataCache;
+  }
+
+  @Path("/{zkAddress}/{clusterName}")
+  @POST
+  public Response addCluster(@PathParam("zkAddress") String zkAddress,
+                             @PathParam("clusterName") String clusterName) throws Exception {
+    dataCache.invalidate();
+
+    ClusterSetup clusterSetup = clientCache.get(zkAddress).getClusterSetup();
+
+    if (clusterSetup.getClusterManagementTool().getClusters().contains(clusterName)) {
+      return Response.status(Response.Status.CONFLICT).build();
     }
 
-    @Path("/{zkAddress}/{clusterName}")
-    @POST
-    public Response addCluster(@PathParam("zkAddress") String zkAddress,
-                               @PathParam("clusterName") String clusterName) throws Exception {
-        dataCache.invalidate();
+    clusterSetup.addCluster(clusterName, false);
 
-        ClusterSetup clusterSetup = clientCache.get(zkAddress).getClusterSetup();
+    dataCache.invalidate();
 
-        if (clusterSetup.getClusterManagementTool().getClusters().contains(clusterName)) {
-            return Response.status(Response.Status.CONFLICT).build();
-        }
+    return Response.ok().build();
+  }
 
-        clusterSetup.addCluster(clusterName, false);
+  @Path("/{zkAddress}/{clusterName}")
+  @DELETE
+  public Response dropCluster(@PathParam("zkAddress") String zkAddress,
+                              @PathParam("clusterName") String clusterName) throws Exception {
+    dataCache.invalidate();
 
-        dataCache.invalidate();
+    ClusterSetup clusterSetup = clientCache.get(zkAddress).getClusterSetup();
 
-        return Response.ok().build();
+    if (!clusterSetup.getClusterManagementTool().getClusters().contains(clusterName)) {
+      throw new NotFoundException();
     }
 
-    @Path("/{zkAddress}/{clusterName}")
-    @DELETE
-    public Response dropCluster(@PathParam("zkAddress") String zkAddress,
-                                @PathParam("clusterName") String clusterName) throws Exception {
-        dataCache.invalidate();
+    clusterSetup.getClusterManagementTool().dropCluster(clusterName);
 
-        ClusterSetup clusterSetup = clientCache.get(zkAddress).getClusterSetup();
+    return Response.noContent().build();
+  }
 
-        if (!clusterSetup.getClusterManagementTool().getClusters().contains(clusterName)) {
-            throw new NotFoundException();
-        }
+  @Path("/{zkAddress}/{clusterName}/instances/{instanceName}")
+  @POST
+  public Response addInstance(@PathParam("zkAddress") String zkAddress,
+                              @PathParam("clusterName") String clusterName,
+                              @PathParam("instanceName") String instanceName,
+                              @QueryParam("disable") boolean disable,
+                              @QueryParam("failIfNoInstance") boolean failIfNoInstance) throws Exception {
+    dataCache.invalidate();
 
-        clusterSetup.getClusterManagementTool().dropCluster(clusterName);
+    ClusterSetup clusterSetup = clientCache.get(zkAddress).getClusterSetup();
 
-        return Response.noContent().build();
+    List<String> instances = clusterSetup.getClusterManagementTool().getInstancesInCluster(clusterName);
+
+    Response response;
+
+    if (instances.contains(instanceName)) {
+      response = Response.notModified().build();
+    } else if (failIfNoInstance) {
+      return Response.status(Response.Status.BAD_REQUEST).build();
+    } else {
+      clusterSetup.addInstanceToCluster(clusterName, instanceName);
+      response = Response.ok().build();
     }
 
-    @Path("/{zkAddress}/{clusterName}/instances/{instanceName}")
-    @POST
-    public Response addInstance(@PathParam("zkAddress") String zkAddress,
-                                @PathParam("clusterName") String clusterName,
-                                @PathParam("instanceName") String instanceName,
-                                @QueryParam("disable") boolean disable,
-                                @QueryParam("failIfNoInstance") boolean failIfNoInstance) throws Exception {
-        dataCache.invalidate();
+    clusterSetup.getClusterManagementTool().enableInstance(clusterName, instanceName, !disable);
 
-        ClusterSetup clusterSetup = clientCache.get(zkAddress).getClusterSetup();
+    return response;
+  }
 
-        List<String> instances = clusterSetup.getClusterManagementTool().getInstancesInCluster(clusterName);
+  @Path("/{zkAddress}/{clusterName}/instances/{instanceName}")
+  @DELETE
+  public Response dropInstance(@PathParam("zkAddress") String zkAddress,
+                               @PathParam("clusterName") String clusterName,
+                               @PathParam("instanceName") String instanceName) throws Exception {
+    dataCache.invalidate();
 
-        Response response;
+    ClusterSetup clusterSetup = clientCache.get(zkAddress).getClusterSetup();
 
-        if (instances.contains(instanceName)) {
-            response = Response.notModified().build();
-        } else if (failIfNoInstance) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        } else {
-            clusterSetup.addInstanceToCluster(clusterName, instanceName);
-            response = Response.ok().build();
-        }
+    InstanceConfig instanceConfig
+            = clusterSetup.getClusterManagementTool().getInstanceConfig(clusterName, instanceName);
 
-        clusterSetup.getClusterManagementTool().enableInstance(clusterName, instanceName, !disable);
-
-        return response;
+    if (instanceConfig == null) {
+      throw new NotFoundException();
+    } else if (instanceConfig.getInstanceEnabled()) {
+      return Response.status(Response.Status.BAD_REQUEST)
+              .header("X-Error-Message", "Cannot drop instance that is enabled")
+              .build();
     }
 
-    @Path("/{zkAddress}/{clusterName}/instances/{instanceName}")
-    @DELETE
-    public Response dropInstance(@PathParam("zkAddress") String zkAddress,
-                                 @PathParam("clusterName") String clusterName,
-                                 @PathParam("instanceName") String instanceName) throws Exception {
-        dataCache.invalidate();
+    clusterSetup.dropInstanceFromCluster(clusterName, instanceName);
 
-        ClusterSetup clusterSetup = clientCache.get(zkAddress).getClusterSetup();
+    return Response.noContent().build();
+  }
 
-        InstanceConfig instanceConfig
-                = clusterSetup.getClusterManagementTool().getInstanceConfig(clusterName, instanceName);
+  @Path("/{zkAddress}/{clusterName}/resources/{resourceName}/{partitions}/{replicas}")
+  @POST
+  public Response addResource(@PathParam("zkAddress") String zkAddress,
+                              @PathParam("clusterName") String clusterName,
+                              @PathParam("resourceName") String resourceName,
+                              @PathParam("partitions") int partitions,
+                              @PathParam("replicas") String replicas,
+                              @QueryParam("rebalance") boolean rebalance,
+                              @QueryParam("stateModel") String stateModel,
+                              @QueryParam("rebalanceMode") String rebalanceMode) throws Exception {
+    dataCache.invalidate();
 
-        if (instanceConfig == null) {
-            throw new NotFoundException();
-        } else if (instanceConfig.getInstanceEnabled()) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .header("X-Error-Message", "Cannot drop instance that is enabled")
-                    .build();
-        }
+    ClusterConnection conn = clientCache.get(zkAddress);
+    ClusterSetup clusterSetup = conn.getClusterSetup();
 
-        clusterSetup.dropInstanceFromCluster(clusterName, instanceName);
-
-        return Response.noContent().build();
+    if (!ZKUtil.isClusterSetup(clusterName, conn.getZkClient())) {
+      return Response.status(Response.Status.BAD_REQUEST).build();
     }
 
-    @Path("/{zkAddress}/{clusterName}/resources/{resourceName}/{partitions}/{replicas}")
-    @POST
-    public Response addResource(@PathParam("zkAddress") String zkAddress,
-                                @PathParam("clusterName") String clusterName,
-                                @PathParam("resourceName") String resourceName,
-                                @PathParam("partitions") int partitions,
-                                @PathParam("replicas") String replicas,
-                                @QueryParam("rebalance") boolean rebalance,
-                                @QueryParam("stateModel") String stateModel,
-                                @QueryParam("rebalanceMode") String rebalanceMode) throws Exception {
-        dataCache.invalidate();
+    IdealState existingIdealState
+            = clusterSetup.getClusterManagementTool().getResourceIdealState(clusterName, resourceName);
 
-        ClusterConnection conn = clientCache.get(zkAddress);
-        ClusterSetup clusterSetup = conn.getClusterSetup();
+    IdealState idealState = new IdealState(resourceName);
+    idealState.setNumPartitions(partitions);
+    idealState.setReplicas(replicas);
+    idealState.setStateModelDefRef(stateModel == null
+            ? "OnlineOffline" : stateModel);
+    idealState.setRebalanceMode(rebalanceMode == null
+            ? IdealState.RebalanceMode.FULL_AUTO : IdealState.RebalanceMode.valueOf(rebalanceMode));
 
-        if (!ZKUtil.isClusterSetup(clusterName, conn.getZkClient())) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
+    Response response;
 
-        IdealState existingIdealState
-                = clusterSetup.getClusterManagementTool().getResourceIdealState(clusterName, resourceName);
-
-        IdealState idealState = new IdealState(resourceName);
-        idealState.setNumPartitions(partitions);
-        idealState.setReplicas(replicas);
-        idealState.setStateModelDefRef(stateModel == null
-                ? "OnlineOffline" : stateModel);
-        idealState.setRebalanceMode(rebalanceMode == null
-                ? IdealState.RebalanceMode.FULL_AUTO : IdealState.RebalanceMode.valueOf(rebalanceMode));
-
-        Response response;
-
-        if (existingIdealState == null) {
-            clusterSetup.getClusterManagementTool().addResource(clusterName, resourceName, idealState);
-            response = Response.ok().build();
-        } else if (!existingIdealState.equals(idealState)) {
-            return Response.status(Response.Status.CONFLICT).build();
-        } else {
-            response = Response.notModified().build();
-        }
-
-        if (rebalance) { // TODO this will break if replicas is not integer (e.g. N)
-            clusterSetup.rebalanceResource(clusterName, resourceName, Integer.valueOf(replicas));
-        }
-
-        return response;
+    if (existingIdealState == null) {
+      clusterSetup.getClusterManagementTool().addResource(clusterName, resourceName, idealState);
+      response = Response.ok().build();
+    } else if (!existingIdealState.equals(idealState)) {
+      return Response.status(Response.Status.CONFLICT).build();
+    } else {
+      response = Response.notModified().build();
     }
 
-    @Path("/{zkAddress}/{clusterName}/resources/{resourceName}")
-    @DELETE
-    public Response dropResource(@PathParam("zkAddress") String zkAddress,
-                                @PathParam("clusterName") String clusterName,
-                                @PathParam("resourceName") String resourceName) throws Exception {
-        dataCache.invalidate();
-
-        ClusterSetup clusterSetup = clientCache.get(zkAddress).getClusterSetup();
-
-        List<String> resources = clusterSetup.getClusterManagementTool().getResourcesInCluster(clusterName);
-        if (!resources.contains(resourceName)) {
-            throw new NotFoundException();
-        }
-
-        clusterSetup.dropResourceFromCluster(clusterName, resourceName);
-
-        return Response.noContent().build();
+    if (rebalance) { // TODO this will break if replicas is not integer (e.g. N)
+      clusterSetup.rebalanceResource(clusterName, resourceName, Integer.valueOf(replicas));
     }
+
+    return response;
+  }
+
+  @Path("/{zkAddress}/{clusterName}/resources/{resourceName}")
+  @DELETE
+  public Response dropResource(@PathParam("zkAddress") String zkAddress,
+                               @PathParam("clusterName") String clusterName,
+                               @PathParam("resourceName") String resourceName) throws Exception {
+    dataCache.invalidate();
+
+    ClusterSetup clusterSetup = clientCache.get(zkAddress).getClusterSetup();
+
+    List<String> resources = clusterSetup.getClusterManagementTool().getResourcesInCluster(clusterName);
+    if (!resources.contains(resourceName)) {
+      throw new NotFoundException();
+    }
+
+    clusterSetup.dropResourceFromCluster(clusterName, resourceName);
+
+    return Response.noContent().build();
+  }
 }
--- helix-ui/src/main/java/org/apache/helix/ui/util/ClientCache.java
+++ helix-ui/src/main/java/org/apache/helix/ui/util/ClientCache.java
@@ -1,7 +1,25 @@
 package org.apache.helix.ui.util;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import com.google.common.cache.*;
-import org.I0Itec.zkclient.exception.ZkTimeoutException;
 import org.apache.helix.manager.zk.ZNRecordSerializer;
 import org.apache.helix.manager.zk.ZkClient;
 import org.apache.helix.ui.api.ClusterConnection;
@@ -18,83 +36,83 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class ClientCache {
-    private static final Logger LOG = LoggerFactory.getLogger(ClientCache.class);
-    private static final int DEFAULT_SESSION_TIMEOUT_MILLIS = 5000;
-    private static final int DEFAULT_CONNECTION_TIMEOUT_MILLIS = 5000;
+  private static final Logger LOG = LoggerFactory.getLogger(ClientCache.class);
+  private static final int DEFAULT_SESSION_TIMEOUT_MILLIS = 5000;
+  private static final int DEFAULT_CONNECTION_TIMEOUT_MILLIS = 5000;
 
-    private final ZkAddressValidator zkAddressValidator;
+  private final ZkAddressValidator zkAddressValidator;
 
-    public ClientCache(ZkAddressValidator zkAddressValidator) {
-        this.zkAddressValidator = zkAddressValidator;
-    }
+  public ClientCache(ZkAddressValidator zkAddressValidator) {
+    this.zkAddressValidator = zkAddressValidator;
+  }
 
-    // Manages and caches lifecycle of connections to ZK
-    final LoadingCache<String, ClusterConnection> clientCache = CacheBuilder.newBuilder()
-            .maximumSize(3)
-            .expireAfterAccess(5, TimeUnit.MINUTES)
-            .removalListener(new RemovalListener<String, ClusterConnection>() {
-                @Override
-                public void onRemoval(RemovalNotification<String, ClusterConnection> removalNotification) {
-                    if (removalNotification.getValue() != null) {
-                        ZkClient zkClient = removalNotification.getValue().getZkClient();
-                        if (zkClient != null) {
-                            zkClient.close();
-                            LOG.info("Disconnected from {}", removalNotification.getKey());
-                        }
-                    }
+  // Manages and caches lifecycle of connections to ZK
+  final LoadingCache<String, ClusterConnection> clientCache = CacheBuilder.newBuilder()
+          .maximumSize(3)
+          .expireAfterAccess(5, TimeUnit.MINUTES)
+          .removalListener(new RemovalListener<String, ClusterConnection>() {
+            @Override
+            public void onRemoval(RemovalNotification<String, ClusterConnection> removalNotification) {
+              if (removalNotification.getValue() != null) {
+                ZkClient zkClient = removalNotification.getValue().getZkClient();
+                if (zkClient != null) {
+                  zkClient.close();
+                  LOG.info("Disconnected from {}", removalNotification.getKey());
                 }
-            })
-            .build(new CacheLoader<String, ClusterConnection>() {
-                @Override
-                public ClusterConnection load(String zkAddress) throws Exception {
-                    ZkClient zkClient = new ZkClient(
-                            zkAddress,
-                            DEFAULT_SESSION_TIMEOUT_MILLIS,
-                            DEFAULT_CONNECTION_TIMEOUT_MILLIS,
-                            new ZNRecordSerializer());
-                    zkClient.waitUntilConnected();
-                    LOG.info("Connected to {}", zkAddress);
-                    return new ClusterConnection(zkClient);
-                }
-            });
-
-    public ClusterConnection get(String zkAddress) {
-        try {
-            zkAddress = URLDecoder.decode(zkAddress, "UTF-8");
-        } catch (Exception e) {
-            throw new IllegalArgumentException(e);
-        }
-
-        if (!zkAddressValidator.validate(zkAddress)) {
-            throw new WebApplicationException("Cannot access " + zkAddress, Response.Status.UNAUTHORIZED);
-        }
-
-        ClusterConnection clusterConnection;
-        try {
-            clusterConnection = clientCache.get(zkAddress);
-        } catch (Exception e) {
-            throw new WebApplicationException(e, Response.Status.GATEWAY_TIMEOUT);
-        }
-
-        if (!clusterConnection.getZkClient().getConnection().getZookeeperState().equals(ZooKeeper.States.CONNECTED)) {
-            clientCache.invalidate(zkAddress);
-            throw new WebApplicationException("ZooKeeper connection was dead", Response.Status.GATEWAY_TIMEOUT);
-        }
-
-        return clusterConnection;
-    }
-
-    public void invalidateAll() {
-        clientCache.invalidateAll();
-    }
-
-    public Set<String> getDeadConnections() {
-        Set<String> deadConnections = new HashSet<String>();
-        for (Map.Entry<String, ClusterConnection> entry : clientCache.asMap().entrySet()) {
-            if (!entry.getValue().getZkClient().getConnection().getZookeeperState().isAlive()) {
-                deadConnections.add(entry.getKey());
+              }
             }
-        }
-        return deadConnections;
+          })
+          .build(new CacheLoader<String, ClusterConnection>() {
+            @Override
+            public ClusterConnection load(String zkAddress) throws Exception {
+              ZkClient zkClient = new ZkClient(
+                      zkAddress,
+                      DEFAULT_SESSION_TIMEOUT_MILLIS,
+                      DEFAULT_CONNECTION_TIMEOUT_MILLIS,
+                      new ZNRecordSerializer());
+              zkClient.waitUntilConnected();
+              LOG.info("Connected to {}", zkAddress);
+              return new ClusterConnection(zkClient);
+            }
+          });
+
+  public ClusterConnection get(String zkAddress) {
+    try {
+      zkAddress = URLDecoder.decode(zkAddress, "UTF-8");
+    } catch (Exception e) {
+      throw new IllegalArgumentException(e);
     }
+
+    if (!zkAddressValidator.validate(zkAddress)) {
+      throw new WebApplicationException("Cannot access " + zkAddress, Response.Status.UNAUTHORIZED);
+    }
+
+    ClusterConnection clusterConnection;
+    try {
+      clusterConnection = clientCache.get(zkAddress);
+    } catch (Exception e) {
+      throw new WebApplicationException(e, Response.Status.GATEWAY_TIMEOUT);
+    }
+
+    if (!clusterConnection.getZkClient().getConnection().getZookeeperState().equals(ZooKeeper.States.CONNECTED)) {
+      clientCache.invalidate(zkAddress);
+      throw new WebApplicationException("ZooKeeper connection was dead", Response.Status.GATEWAY_TIMEOUT);
+    }
+
+    return clusterConnection;
+  }
+
+  public void invalidateAll() {
+    clientCache.invalidateAll();
+  }
+
+  public Set<String> getDeadConnections() {
+    Set<String> deadConnections = new HashSet<String>();
+    for (Map.Entry<String, ClusterConnection> entry : clientCache.asMap().entrySet()) {
+      if (!entry.getValue().getZkClient().getConnection().getZookeeperState().isAlive()) {
+        deadConnections.add(entry.getKey());
+      }
+    }
+    return deadConnections;
+  }
 }

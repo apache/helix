cb06fb2ea0e17d2fc989edf96e2f136690bb96ef
Junkai Xue <jxue@linkedin.com>
2016-04-20 16:33:50 -0700

    Add job dependency workflow test
    1. Add an option to enable number of partion varies in TaskTestBase
    2. Add new test for workflow test for parallel jobs testing.
    
    This test has been disabled.

--- helix-core/src/test/java/org/apache/helix/integration/task/TaskTestBase.java
+++ helix-core/src/test/java/org/apache/helix/integration/task/TaskTestBase.java
@@ -1,5 +1,24 @@
 package org.apache.helix.integration.task;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -31,6 +50,8 @@ public class TaskTestBase extends ZkIntegrationTestBase {
   protected int _numReplicas = 3;
   protected int _numDbs = 1;
 
+  protected Boolean _partitionVary = true;
+
   protected ClusterControllerManager _controller;
 
   protected HelixManager _manager;
@@ -60,9 +81,10 @@ public class TaskTestBase extends ZkIntegrationTestBase {
     // Set up target db
     if (_numDbs > 1) {
       for (int i = 0; i < _numDbs; i++) {
+        int varyNum = _partitionVary == true ? 10 * i : 0;
         String db = WorkflowGenerator.DEFAULT_TGT_DB + i;
         _setupTool
-            .addResourceToCluster(CLUSTER_NAME, db, _numParitions + 10 * i, MASTER_SLAVE_STATE_MODEL,
+            .addResourceToCluster(CLUSTER_NAME, db, _numParitions + varyNum, MASTER_SLAVE_STATE_MODEL,
                 IdealState.RebalanceMode.FULL_AUTO.toString());
         _setupTool.rebalanceStorageCluster(CLUSTER_NAME, db, _numReplicas);
         _testDbs.add(db);

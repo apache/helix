d7061ea49f82a5905c4c99aea9bfb5553575b654
Junkai Xue <jxue@linkedin.com>
2016-06-16 12:28:51 -0700

    Task Framework monitor MBean

---
+++ helix-core/src/main/java/org/apache/helix/monitoring/mbeans/WorkflowMonitor.java
@@ -0,0 +1,55 @@
+package org.apache.helix.monitoring.mbeans;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+public class WorkflowMonitor implements WorkflowMonitorMBean {
+  private String _clusterName;
+  private String _workflowType;
+
+  @Override
+  public long getAllWorkflowCount() {
+    return 0;
+  }
+
+  @Override
+  public long getSuccessfulWorkflowCount() {
+    return 0;
+  }
+
+  @Override public long getFailedWorkflowCount() {
+    return 0;
+  }
+
+  @Override public long getExistingWorkflowGauge() {
+    return 0;
+  }
+
+  @Override public long getQueuedWorkflowGauge() {
+    return 0;
+  }
+
+  @Override public long getRunningWorkflowGauge() {
+    return 0;
+  }
+
+  @Override public String getSensorName() {
+    return null;
+  }
+}
---
+++ helix-core/src/main/java/org/apache/helix/monitoring/mbeans/WorkflowMonitorMBean.java
@@ -0,0 +1,64 @@
+package org.apache.helix.monitoring.mbeans;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.helix.monitoring.SensorNameProvider;
+
+/**
+ * Workflow monitor MBean for workflows, which are shared among workflows with the same type.
+ */
+public interface WorkflowMonitorMBean extends SensorNameProvider {
+
+  /**
+   * Get number of all workflows registered
+   * @return
+   */
+  public long getAllWorkflowCount();
+
+  /**
+   * Get number of succeeded workflows
+   * @return
+   */
+  public long getSuccessfulWorkflowCount();
+
+  /**
+   * Get number of failed workflows
+   * @return
+   */
+  public long getFailedWorkflowCount();
+
+  /**
+   * Get number of current existing workflows
+   * @return
+   */
+  public long getExistingWorkflowGauge();
+
+  /**
+   * Get number of queued but not started workflows
+   * @return
+   */
+  public long getQueuedWorkflowGauge();
+
+  /**
+   * Get number of running workflows
+   * @return
+   */
+  public long getRunningWorkflowGauge();
+}

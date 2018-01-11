4e7eca5171f509bc3669ad018beef1e149eca6a5
Junkai Xue <jxue@linkedin.com>
2018-01-11 10:38:16 -0800

    Add instance support for CriteriaEvalutator

--- helix-core/src/main/java/org/apache/helix/messaging/CriteriaEvaluator.java
+++ helix-core/src/main/java/org/apache/helix/messaging/CriteriaEvaluator.java
@@ -41,7 +41,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 public class CriteriaEvaluator {
-  private static Logger logger = Logger.getLogger(CriteriaEvaluator.class);
+  private static Logger logger = LoggerFactory.getLogger(CriteriaEvaluator.class);
   public static final String MATCH_ALL_SYM = "%";
 
   /**

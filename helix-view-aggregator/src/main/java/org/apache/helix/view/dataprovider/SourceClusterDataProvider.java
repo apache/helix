440d109db0e275c0b5938c2ac79ee787706a2416
hrzhang <hrzhang@linkedin.com>
2018-02-06 11:21:27 -0800

    HELIX-705: implemented SourceClusterDataProvider's core logic and related tests
    RB=1205694
    BUG=HELIX-775
    G=helix-reviewers
    R=lxia,jjwang,jxue,erkim
    A=jjwang

--- helix-view-aggregator/src/main/java/org/apache/helix/view/dataprovider/SourceClusterDataProvider.java
+++ helix-view-aggregator/src/main/java/org/apache/helix/view/dataprovider/SourceClusterDataProvider.java
@@ -51,29 +51,25 @@ import org.apache.helix.model.LiveInstance;
 public class SourceClusterDataProvider extends BasicClusterDataCache
     implements InstanceConfigChangeListener, LiveInstanceChangeListener,
     ExternalViewChangeListener {
-  private HelixManager _helixManager;
+  private final HelixManager _helixManager;
+  private final ViewClusterSourceConfig _sourceClusterConfig;
+  private final ClusterEventProcessor _eventProcessor;
+
   private HelixDataAccessor _dataAccessor;
   private PropertyKey.Builder _propertyKeyBuilder;
-  private ViewClusterSourceConfig _sourceClusterConfig;
-  private ClusterEventProcessor _eventProcessor;
 
   public SourceClusterDataProvider(ViewClusterSourceConfig config,
       ClusterEventProcessor eventProcessor) {
     super(config.getName());
     _eventProcessor = eventProcessor;
     _sourceClusterConfig = config;
-    _helixManager = HelixManagerFactory
-        .getZKHelixManager(config.getName(), generateHelixManagerInstanceName(config.getName()),
-            InstanceType.SPECTATOR, config.getZkAddress());
-    requireFullRefreshBasedOnConfig();
+    _helixManager = HelixManagerFactory.getZKHelixManager(config.getName(),
+        generateHelixManagerInstanceName(config.getName()),
+        InstanceType.SPECTATOR, config.getZkAddress());
   }
 
   public String getName() {
     return _helixManager.getInstanceName();
-  }
-
-  public ViewClusterSourceConfig getSourceClusterConfig() {
-    return _sourceClusterConfig;
   }
 
   /**
@@ -82,15 +78,39 @@ public class SourceClusterDataProvider extends BasicClusterDataCache
    * @throws Exception
    */
   public void setup() throws Exception {
+    if (_helixManager != null && _helixManager.isConnected()) {
+      LOG.info(String.format("Data provider %s is already setup", _helixManager.getInstanceName()));
+      return;
+    }
     try {
-      LOG.info(String.format("%s setting up ...", _helixManager.getInstanceName()));
       _helixManager.connect();
-      _helixManager.addInstanceConfigChangeListener(this);
-      _helixManager.addLiveInstanceChangeListener(this);
-      _helixManager.addExternalViewChangeListener(this);
+      for (PropertyType property : _sourceClusterConfig.getProperties()) {
+        HelixConstants.ChangeType changeType;
+        switch (property) {
+        case INSTANCES:
+          _helixManager.addInstanceConfigChangeListener(this);
+          changeType = HelixConstants.ChangeType.INSTANCE_CONFIG;
+          break;
+        case LIVEINSTANCES:
+          _helixManager.addLiveInstanceChangeListener(this);
+          changeType = HelixConstants.ChangeType.LIVE_INSTANCE;
+          break;
+        case EXTERNALVIEW:
+          _helixManager.addExternalViewChangeListener(this);
+          changeType = HelixConstants.ChangeType.EXTERNAL_VIEW;
+          break;
+        default:
+          LOG.warn(String
+              .format("Unsupported property type: %s. Skip adding listener", property.name()));
+          continue;
+        }
+        notifyDataChange(changeType);
+      }
       _dataAccessor = _helixManager.getHelixDataAccessor();
       _propertyKeyBuilder = _dataAccessor.keyBuilder();
-      LOG.info(String.format("%s started.", _helixManager.getInstanceName()));
+      LOG.info(String
+          .format("%s started. Source cluster detail: %s", _helixManager.getInstanceName(),
+              _sourceClusterConfig.toString()));
     } catch(Exception e) {
       shutdown();
       throw e;
@@ -105,6 +125,8 @@ public class SourceClusterDataProvider extends BasicClusterDataCache
     if (_helixManager != null && _helixManager.isConnected()) {
       try {
         _helixManager.disconnect();
+        LOG.info(
+            String.format("Data provider %s shutdown cleanly.", _helixManager.getInstanceName()));
       } catch (ZkInterruptedException e) {
         // OK
       }
@@ -116,66 +138,40 @@ public class SourceClusterDataProvider extends BasicClusterDataCache
   }
 
   /**
-   * Re-list current instance config names. ListName is a more reliable way to find
+   * Get current instance config names. ListName is a more reliable way to find
    * current instance config names. This is needed for ViewClusterRefresher when
    * it is generating diffs to push to ViewCluster
    * @return
    */
-  public List<String> listInstanceConfigNames() {
+  public List<String> getInstanceConfigNames() {
     return _dataAccessor.getChildNames(_propertyKeyBuilder.instanceConfigs());
   }
 
   /**
-   * Re-list current live instance names.
+   * Get current live instance names.
    * @return
    */
-  public List<String> listLiveInstanceNames() {
+  public List<String> getLiveInstanceNames() {
     return _dataAccessor.getChildNames(_propertyKeyBuilder.liveInstances());
   }
 
   /**
-   * re-list external view names
+   * Get external view names
    * @return
    */
-  public List<String> listExternalViewNames() {
+  public List<String> getExternalViewNames() {
     return _dataAccessor.getChildNames(_propertyKeyBuilder.externalViews());
   }
 
-  /**
-   * Based on current ViewClusterSourceConfig, decide whether caller should
-   * read instance config. Used by ViewClusterRefresher
-   * @return
-   */
-  public boolean shouldReadInstanceConfigs() {
-    // TODO: implement logic
-    return false;
-  }
-
-  /**
-   * Based on current ViewClusterSourceConfig, decide whether caller should
-   * read live instances. Used by ViewClusterRefresher
-   * @return
-   */
-  public boolean shouldReadLiveInstances() {
-    // TODO: implement logic
-    return false;
-  }
-
-  /**
-   * Based on current ViewClusterSourceConfig, decide whether caller should
-   * read external view. Used by ViewClusterRefresher
-   * @return
-   */
-  public boolean shouldReadExternalViews() {
-    // TODO: implement logic
-    return false;
+  public List<PropertyType> getPropertiesToAggregate() {
+    return _sourceClusterConfig.getProperties();
   }
 
   @Override
   @PreFetch(enabled = false)
   public void onInstanceConfigChange(List<InstanceConfig> instanceConfigs,
       NotificationContext context) {
-    queueEventToProcessor(context, ClusterEventType.InstanceConfigChange,
+    queueEvent(context, ClusterEventType.InstanceConfigChange,
         HelixConstants.ChangeType.INSTANCE_CONFIG);
   }
 
@@ -183,7 +179,7 @@ public class SourceClusterDataProvider extends BasicClusterDataCache
   @PreFetch(enabled = false)
   public void onLiveInstanceChange(List<LiveInstance> liveInstances,
       NotificationContext changeContext) {
-    queueEventToProcessor(changeContext, ClusterEventType.LiveInstanceChange,
+    queueEvent(changeContext, ClusterEventType.LiveInstanceChange,
         HelixConstants.ChangeType.LIVE_INSTANCE);
   }
 
@@ -191,53 +187,22 @@ public class SourceClusterDataProvider extends BasicClusterDataCache
   @PreFetch(enabled = false)
   public void onExternalViewChange(List<ExternalView> externalViewList,
       NotificationContext changeContext) {
-    queueEventToProcessor(changeContext, ClusterEventType.ExternalViewChange,
+    queueEvent(changeContext, ClusterEventType.ExternalViewChange,
         HelixConstants.ChangeType.EXTERNAL_VIEW);
   }
 
-  private void queueEventToProcessor(NotificationContext context,
+  private void queueEvent(NotificationContext context,
       ClusterEventType clusterEventType, HelixConstants.ChangeType cacheChangeType)
       throws IllegalStateException {
-    if (!shouldProcessEvent(cacheChangeType)) {
-      LOG.info(String.format(
-          "Skip processing event based on ViewClusterSourceConfig: ClusterName=%s; ClusterEventType=%s, ChangeType=%s",
-          _clusterName, clusterEventType, cacheChangeType));
-      return;
-    }
-    ClusterEvent event = new ClusterEvent(_clusterName, clusterEventType);
+    // TODO: in case of FINALIZE, if we are not shutdown, re-connect helix manager and report error
     if (context != null && context.getType() != NotificationContext.Type.FINALIZE) {
       notifyDataChange(cacheChangeType);
-      _eventProcessor.queueEvent(event);
+      _eventProcessor.queueEvent(new ClusterEvent(_clusterName, clusterEventType));
     } else {
-      LOG.info(String.format("SourceClusterDataProvider: skip queuing event %s", event));
+      LOG.info(String.format("Skip queuing event. EventType: %s, ChangeType: %s, ContextType: %s",
+          clusterEventType.name(), cacheChangeType.name(),
+          context == null ? "NoContext" : context.getType().name()));
     }
-  }
-
-  /**
-   * Replace current source cluster config properties (A list of PropertyType we should aggregate)
-   * with the given ones, and update corresponding provider mechanisms
-   * @param properties
-   */
-  public synchronized void setSourceClusterConfigProperty(List<PropertyType> properties) {
-    // TODO: implement logic
-  }
-
-  /**
-   * Based on current ViewClusterSourceConfig, notify cache accordingly.
-   */
-  private void requireFullRefreshBasedOnConfig() {
-    // TODO: implement logic
-  }
-
-  /**
-   * Check source cluster config and decide whether this event should be processed
-   * @param sourceClusterChangeType
-   * @return
-   */
-  private synchronized boolean shouldProcessEvent(
-      HelixConstants.ChangeType sourceClusterChangeType) {
-    // TODO: implement
-    return false;
   }
 
   private static String generateHelixManagerInstanceName(String clusterName) {

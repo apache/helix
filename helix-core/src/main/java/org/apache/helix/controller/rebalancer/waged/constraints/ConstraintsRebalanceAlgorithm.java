ae2b516091b3d888144685c5010649d33841d58b
jiajunwang <ericwang1985@gmail.com>
2019-07-18 14:50:20 -0700

    Define the WAGED rebalancer interfaces.
    This is the intial check in for the future development of the WAGED rebalancer.
    All the components are placeholders. They will be implemented gradually.

---
+++ helix-core/src/main/java/org/apache/helix/controller/rebalancer/waged/constraints/ConstraintsRebalanceAlgorithm.java
@@ -0,0 +1,49 @@
+package org.apache.helix.controller.rebalancer.waged.constraints;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.helix.controller.rebalancer.waged.RebalanceAlgorithm;
+import org.apache.helix.controller.rebalancer.waged.model.ClusterModel;
+import org.apache.helix.model.IdealState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A placeholder before we have the implementation.
+ * The constraint-based rebalance algorithm that is used in the WAGED rebalancer.
+ */
+public class ConstraintsRebalanceAlgorithm implements RebalanceAlgorithm {
+  private static final Logger LOG = LoggerFactory.getLogger(ConstraintsRebalanceAlgorithm.class);
+
+  private Map<HardConstraint.FailureReason, Integer> _failureReasonCounterMap = new HashMap<>();
+
+  public ConstraintsRebalanceAlgorithm() {
+    // TODO Constraints initialization
+  }
+
+  @Override
+  public Map<String, IdealState> rebalance(ClusterModel clusterModel,
+      Map<String, Map<HardConstraint.FailureReason, Integer>> failureReasons) {
+    return new HashMap<>();
+  }
+}

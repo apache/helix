2ba35e306b6907590104e1dd74051d8ccc09d804
kaisun2000 <52840222+kaisun2000@users.noreply.github.com>
2019-08-06 11:58:16 -0700

    Fix RoutingTableProvider statePropagationLatency metric reporting bug (#365)
    Issue:
    
    CurrentStateCache updating snapshot would miss all the existing partitions that having state change.
    
    RoutingTableProvider callback on the main event thread. Time is not accounted in log.
    
    Description:
    fix the bug by updating the snapshot with the correct reloadkeys.
    
    enhanced log to accout for user callback code separately.
    
    Tests:
    mvn test passed.

--- helix-core/src/main/java/org/apache/helix/common/caches/CurrentStateSnapshot.java
+++ helix-core/src/main/java/org/apache/helix/common/caches/CurrentStateSnapshot.java
@@ -8,8 +8,13 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class CurrentStateSnapshot extends AbstractDataSnapshot<CurrentState> {
+  private static final Logger LOG = LoggerFactory.getLogger(CurrentStateSnapshot.class.getName());
+
   private Set<PropertyKey> _updatedStateKeys = null;
   private Map<PropertyKey, CurrentState> _prevStateMap = null;
 
@@ -32,6 +37,7 @@ public class CurrentStateSnapshot extends AbstractDataSnapshot<CurrentState> {
     if (_updatedStateKeys != null && _prevStateMap != null) {
       // Note if the prev state map is empty, this is the first time refresh.
       // So the update is not considered as "recent" change.
+      int driftCnt = 0; // clock drift count for comparing timestamp
       for (PropertyKey propertyKey : _updatedStateKeys) {
         CurrentState prevState = _prevStateMap.get(propertyKey);
         CurrentState curState = _properties.get(propertyKey);
@@ -39,11 +45,25 @@ public class CurrentStateSnapshot extends AbstractDataSnapshot<CurrentState> {
         Map<String, Long> partitionUpdateEndTimes = null;
         for (String partition : curState.getPartitionStateMap().keySet()) {
           long newEndTime = curState.getEndTime(partition);
-          if (prevState == null || prevState.getEndTime(partition) < newEndTime) {
+          // if prevState is null, and newEndTime is -1, we should not record -1 in endTimeMap; otherwise,
+          // statePropagation latency calculation in RoutingTableProvider would spit out extremely large metrics.
+          if ((prevState == null || prevState.getEndTime(partition) < newEndTime) && newEndTime != -1) {
             if (partitionUpdateEndTimes == null) {
               partitionUpdateEndTimes = new HashMap<>();
             }
             partitionUpdateEndTimes.put(partition, newEndTime);
+          } else if (prevState != null && prevState.getEndTime(partition) > newEndTime) {
+            // This can happen due to clock drift.
+            // updatedStateKeys is the path to resource in an instance config.
+            // Thus, the space of inner loop is Sigma{replica(i) * partition(i)}; i over all resources in the cluster
+            // This space can be large. In order not to print too many lines, we print first warning for the first case.
+            // If clock drift turns out to be common, we can consider print out more logs, or expose an metric.
+            if (driftCnt < 1) {
+              LOG.warn(
+                  "clock drift. partition:" + partition + " curState:" + curState.getState(partition) + " prevState: "
+                      + prevState.getState(partition));
+            }
+            driftCnt++;
           }
         }
 

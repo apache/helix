78d9eafbb11b6024a514915261ae5f527343ab25
Yi Wang <i3.wangyi@gmail.com>
2019-08-08 12:11:19 -0700

    Merge with the lastest optimization on batch get zookeeper properties

--- helix-rest/src/test/java/org/apache/helix/rest/server/TestInstancesAccessor.java
+++ helix-rest/src/test/java/org/apache/helix/rest/server/TestInstancesAccessor.java
@@ -16,17 +16,20 @@ import org.apache.helix.model.InstanceConfig;
 import org.apache.helix.rest.server.resources.helix.InstancesAccessor;
 import org.apache.helix.rest.server.util.JerseyUriRequestBuilder;
 import org.apache.helix.tools.ClusterVerifiers.BestPossibleExternalViewVerifier;
-import org.codehaus.jackson.JsonNode;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 
 public class TestInstancesAccessor extends AbstractTestClass {
   private final static String CLUSTER_NAME = "TestCluster_0";
+  private ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
   @Test
-  public void testInstancesStoppable_zoneBased() {
+  public void testInstancesStoppable_zoneBased() throws IOException {
     System.out.println("Start test :" + TestHelper.getTestMethodName());
     // Select instances with zone based
     String content =
@@ -38,21 +41,31 @@ public class TestInstancesAccessor extends AbstractTestClass {
     Response response = new JerseyUriRequestBuilder("clusters/{}/instances?command=stoppable")
         .format(STOPPABLE_CLUSTER)
         .post(this, Entity.entity(content, MediaType.APPLICATION_JSON_TYPE));
-    String checkResult = response.readEntity(String.class);
-    Assert.assertEquals(checkResult, "{\n" + "  \"instance_stoppable_parallel\" : [ ],\n"
-        + "  \"instance_not_stoppable_with_reasons\" : {\n"
-        + "    \"instance0\" : [ \"Helix:MIN_ACTIVE_REPLICA_CHECK_FAILED\" ],\n"
-        + "    \"instance1\" : [ \"Helix:EMPTY_RESOURCE_ASSIGNMENT\", \"Helix:INSTANCE_NOT_ENABLED\", \"Helix:INSTANCE_NOT_STABLE\" ],\n"
-        + "    \"instance2\" : [ \"Helix:MIN_ACTIVE_REPLICA_CHECK_FAILED\" ],\n"
-        + "    \"instance3\" : [ \"Helix:HAS_DISABLED_PARTITION\", \"Helix:MIN_ACTIVE_REPLICA_CHECK_FAILED\" ],\n"
-        + "    \"instance4\" : [ \"Helix:EMPTY_RESOURCE_ASSIGNMENT\", \"Helix:INSTANCE_NOT_ALIVE\", \"Helix:INSTANCE_NOT_STABLE\" ],\n"
-        + "    \"invalidInstance\" : [ \"Helix:INSTANCE_NOT_EXIST\" ]\n"
-        + "  }\n" + "}\n");
+    JsonNode jsonNode = OBJECT_MAPPER.readTree(response.readEntity(String.class));
+    Assert.assertFalse(
+        jsonNode.withArray(InstancesAccessor.InstancesProperties.instance_stoppable_parallel.name())
+            .elements().hasNext());
+    JsonNode nonStoppableInstances = jsonNode
+        .get(InstancesAccessor.InstancesProperties.instance_not_stoppable_with_reasons.name());
+    Assert.assertEquals(getStringSet(nonStoppableInstances, "instance0"),
+        ImmutableSet.of("Helix:MIN_ACTIVE_REPLICA_CHECK_FAILED"));
+    Assert.assertEquals(getStringSet(nonStoppableInstances, "instance1"),
+        ImmutableSet.of("Helix:EMPTY_RESOURCE_ASSIGNMENT", "Helix:INSTANCE_NOT_ENABLED",
+            "Helix:INSTANCE_NOT_STABLE"));
+    Assert.assertEquals(getStringSet(nonStoppableInstances, "instance2"),
+        ImmutableSet.of("Helix:MIN_ACTIVE_REPLICA_CHECK_FAILED"));
+    Assert.assertEquals(getStringSet(nonStoppableInstances, "instance3"),
+        ImmutableSet.of("Helix:HAS_DISABLED_PARTITION", "Helix:MIN_ACTIVE_REPLICA_CHECK_FAILED"));
+    Assert.assertEquals(getStringSet(nonStoppableInstances, "instance4"),
+        ImmutableSet.of("Helix:EMPTY_RESOURCE_ASSIGNMENT", "Helix:INSTANCE_NOT_ALIVE",
+            "Helix:INSTANCE_NOT_STABLE"));
+    Assert.assertEquals(getStringSet(nonStoppableInstances, "invalidInstance"),
+        ImmutableSet.of("Helix:INSTANCE_NOT_EXIST"));
     System.out.println("End test :" + TestHelper.getTestMethodName());
   }
 
   @Test(dependsOnMethods = "testInstancesStoppable_zoneBased")
-  public void testInstancesStoppable_disableOneInstance() throws InterruptedException {
+  public void testInstancesStoppable_disableOneInstance() throws IOException {
     // Disable one selected instance0, it should failed to check
     String instance = "instance0";
     InstanceConfig instanceConfig = _configAccessor.getInstanceConfig(STOPPABLE_CLUSTER, instance);
@@ -68,9 +81,10 @@ public class TestInstancesAccessor extends AbstractTestClass {
     Entity entity = Entity.entity("", MediaType.APPLICATION_JSON_TYPE);
     Response response = new JerseyUriRequestBuilder("clusters/{}/instances/{}/stoppable")
         .format(STOPPABLE_CLUSTER, instance).post(this, entity);
-    String checkResult = response.readEntity(String.class);
-    Assert.assertEquals(checkResult,
-        "{\"stoppable\":false,\"failedChecks\":[\"Helix:HAS_DISABLED_PARTITION\",\"Helix:INSTANCE_NOT_ENABLED\",\"Helix:INSTANCE_NOT_STABLE\",\"Helix:MIN_ACTIVE_REPLICA_CHECK_FAILED\"]}");
+    JsonNode jsonResult = OBJECT_MAPPER.readTree(response.readEntity(String.class));
+    Assert.assertFalse(jsonResult.get("stoppable").asBoolean());
+    Assert.assertEquals(getStringSet(jsonResult, "failedChecks"),
+            ImmutableSet.of("Helix:HAS_DISABLED_PARTITION","Helix:INSTANCE_NOT_ENABLED","Helix:INSTANCE_NOT_STABLE","Helix:MIN_ACTIVE_REPLICA_CHECK_FAILED"));
 
     // Reenable instance0, it should passed the check
     instanceConfig.setInstanceEnabled(true);
@@ -81,9 +95,10 @@ public class TestInstancesAccessor extends AbstractTestClass {
     entity = Entity.entity("", MediaType.APPLICATION_JSON_TYPE);
     response = new JerseyUriRequestBuilder("clusters/{}/instances/{}/stoppable")
         .format(STOPPABLE_CLUSTER, instance).post(this, entity);
-    checkResult = response.readEntity(String.class);
-    Assert.assertEquals(checkResult,
-        "{\"stoppable\":false,\"failedChecks\":[\"Helix:MIN_ACTIVE_REPLICA_CHECK_FAILED\"]}");
+    jsonResult = OBJECT_MAPPER.readTree(response.readEntity(String.class));
+
+    Assert.assertFalse(jsonResult.get("stoppable").asBoolean());
+    Assert.assertEquals(getStringSet(jsonResult, "failedChecks"), ImmutableSet.of("Helix:MIN_ACTIVE_REPLICA_CHECK_FAILED"));
     System.out.println("End test :" + TestHelper.getTestMethodName());
   }
 
@@ -94,7 +109,8 @@ public class TestInstancesAccessor extends AbstractTestClass {
         .format(CLUSTER_NAME).get(this);
 
     JsonNode node = OBJECT_MAPPER.readTree(body);
-    String instancesStr = node.get(InstancesAccessor.InstancesProperties.instances.name()).toString();
+    String instancesStr =
+        node.get(InstancesAccessor.InstancesProperties.instances.name()).toString();
     Assert.assertNotNull(instancesStr);
 
     Set<String> instances = OBJECT_MAPPER.readValue(instancesStr,
@@ -109,12 +125,13 @@ public class TestInstancesAccessor extends AbstractTestClass {
     // TODO: Reenable the test after storage node fix the problem
     // Batch disable instances
 
-    List<String> instancesToDisable = Arrays.asList(
-        new String[] { CLUSTER_NAME + "localhost_12918", CLUSTER_NAME + "localhost_12919",
-            CLUSTER_NAME + "localhost_12920"
-        });
-    Entity entity = Entity.entity(OBJECT_MAPPER.writeValueAsString(
-        ImmutableMap.of(InstancesAccessor.InstancesProperties.instances.name(), instancesToDisable)),
+    List<String> instancesToDisable = Arrays.asList(new String[] {
+        CLUSTER_NAME + "localhost_12918", CLUSTER_NAME + "localhost_12919",
+        CLUSTER_NAME + "localhost_12920"
+    });
+    Entity entity = Entity.entity(
+        OBJECT_MAPPER.writeValueAsString(ImmutableMap
+            .of(InstancesAccessor.InstancesProperties.instances.name(), instancesToDisable)),
         MediaType.APPLICATION_JSON_TYPE);
     post("clusters/" + CLUSTER_NAME + "/instances", ImmutableMap.of("command", "disable"), entity,
         Response.Status.OK.getStatusCode());
@@ -122,11 +139,12 @@ public class TestInstancesAccessor extends AbstractTestClass {
     Assert.assertEquals(clusterConfig.getDisabledInstances().keySet(),
         new HashSet<>(instancesToDisable));
 
-    instancesToDisable = Arrays
-        .asList(new String[] { CLUSTER_NAME + "localhost_12918", CLUSTER_NAME + "localhost_12920"
-        });
-    entity = Entity.entity(OBJECT_MAPPER.writeValueAsString(
-        ImmutableMap.of(InstancesAccessor.InstancesProperties.instances.name(), instancesToDisable)),
+    instancesToDisable = Arrays.asList(new String[] {
+        CLUSTER_NAME + "localhost_12918", CLUSTER_NAME + "localhost_12920"
+    });
+    entity = Entity.entity(
+        OBJECT_MAPPER.writeValueAsString(ImmutableMap
+            .of(InstancesAccessor.InstancesProperties.instances.name(), instancesToDisable)),
         MediaType.APPLICATION_JSON_TYPE);
     post("clusters/" + CLUSTER_NAME + "/instances", ImmutableMap.of("command", "enable"), entity,
         Response.Status.OK.getStatusCode());
@@ -134,5 +152,11 @@ public class TestInstancesAccessor extends AbstractTestClass {
     Assert.assertEquals(clusterConfig.getDisabledInstances().keySet(),
         new HashSet<>(Arrays.asList(CLUSTER_NAME + "localhost_12919")));
     System.out.println("End test :" + TestHelper.getTestMethodName());
+  }
+
+  private Set<String> getStringSet(JsonNode jsonNode, String key) {
+    Set<String> result = new HashSet<>();
+    jsonNode.withArray(key).forEach(s -> result.add(s.textValue()));
+    return result;
   }
 }

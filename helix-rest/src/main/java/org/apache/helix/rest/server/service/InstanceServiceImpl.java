173ecd335248499ee706cb4f5e2e189d5c7f6920
Yi Wang <i3.wangyi@gmail.com>
2019-08-15 11:53:26 -0700

    Move partition heatlh check method into dataAccessor layer

--- helix-rest/src/main/java/org/apache/helix/rest/server/service/InstanceServiceImpl.java
+++ helix-rest/src/main/java/org/apache/helix/rest/server/service/InstanceServiceImpl.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -34,9 +33,6 @@ import java.util.stream.Collectors;
 
 import org.apache.helix.ConfigAccessor;
 import org.apache.helix.HelixException;
-import org.apache.helix.HelixProperty;
-import org.apache.helix.PropertyKey;
-import org.apache.helix.ZNRecord;
 import org.apache.helix.model.CurrentState;
 import org.apache.helix.model.ExternalView;
 import org.apache.helix.model.InstanceConfig;
@@ -45,7 +41,6 @@ import org.apache.helix.model.RESTConfig;
 import org.apache.helix.rest.client.CustomRestClient;
 import org.apache.helix.rest.client.CustomRestClientFactory;
 import org.apache.helix.rest.common.HelixDataAccessorWrapper;
-import org.apache.helix.rest.server.json.cluster.PartitionHealth;
 import org.apache.helix.rest.server.json.instance.InstanceInfo;
 import org.apache.helix.rest.server.json.instance.StoppableCheck;
 import org.apache.helix.util.InstanceValidationUtil;
@@ -60,9 +55,6 @@ import com.google.common.collect.ImmutableList;
 public class InstanceServiceImpl implements InstanceService {
   private static final Logger LOG = LoggerFactory.getLogger(InstanceServiceImpl.class);
 
-  private static final String PARTITION_HEALTH_KEY = "PARTITION_HEALTH";
-  private static final String IS_HEALTHY_KEY = "IS_HEALTHY";
-  private static final String EXPIRY_KEY = "EXPIRE";
   private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
   private static final ExecutorService POOL = Executors.newCachedThreadPool();
 
@@ -173,7 +165,7 @@ public class InstanceServiceImpl implements InstanceService {
         instancesForCustomInstanceLevelChecks.stream()
             .collect(Collectors.toMap(Function.identity(),
                 instance -> POOL.submit(() -> performCustomInstanceCheck(clusterId, instance,
-                    getBaseUrl(instance, restConfig), customPayLoads))));
+                    restConfig.getBaseUrl(instance), customPayLoads))));
     List<String> instancesForCustomPartitionLevelChecks =
         filterInstancesForNextCheck(customInstanceLevelChecks, finalStoppableChecks);
     if (!instancesForCustomPartitionLevelChecks.isEmpty()) {
@@ -190,20 +182,23 @@ public class InstanceServiceImpl implements InstanceService {
   }
 
   private List<String> filterInstancesForNextCheck(
-      Map<String, Future<StoppableCheck>> futureStoppableChecks,
-      Map<String, StoppableCheck> finalStoppableChecks) {
+      Map<String, Future<StoppableCheck>> futureStoppableCheckByInstance,
+      Map<String, StoppableCheck> finalStoppableCheckByInstance) {
     List<String> instancesForNextCheck = new ArrayList<>();
-    for (Map.Entry<String, Future<StoppableCheck>> entry : futureStoppableChecks.entrySet()) {
+    for (Map.Entry<String, Future<StoppableCheck>> entry : futureStoppableCheckByInstance
+        .entrySet()) {
       String instance = entry.getKey();
       try {
         StoppableCheck stoppableCheck = entry.getValue().get();
         if (!stoppableCheck.isStoppable()) {
-          finalStoppableChecks.put(instance, stoppableCheck);
+          // put the check result of the failed-to-stop instances
+          finalStoppableCheckByInstance.put(instance, stoppableCheck);
         } else {
+          // instance passed this around of check will be checked in the next round
           instancesForNextCheck.add(instance);
         }
       } catch (InterruptedException | ExecutionException e) {
-        LOG.error("Failed to get StoppableChecks in parallel. Instance: {}", instance);
+        LOG.error("Failed to get StoppableChecks in parallel. Instance: {}", instance, e);
       }
     }
 
@@ -234,47 +229,14 @@ public class InstanceServiceImpl implements InstanceService {
 
   private Map<String, StoppableCheck> performPartitionsCheck(List<String> instances,
       RESTConfig restConfig, Map<String, String> customPayLoads) {
-    //TODO: move the heavy partition health preparation in separate class
-    PartitionHealth clusterPartitionsHealth = generatePartitionHealthMapFromZK();
-    // update the health status for those expired partitions on instances
-    Map<String, List<String>> expiredPartitionsByInstance =
-        clusterPartitionsHealth.getExpiredRecords();
-    Map<String, Future<Map<String, Boolean>>> updatedPartitionsHealthStatusByInstance =
-        new HashMap<>();
-    for (Map.Entry<String, List<String>> entry : expiredPartitionsByInstance.entrySet()) {
-      String instance = entry.getKey();
-      // If expiredPartitions list is empty, it means all the partition statues need to be refreshed
-      // by directly calling the instance API.
-      List<String> expiredPartitions = entry.getValue();
-      Callable<Map<String, Boolean>> refreshTask =
-          () -> _customRestClient.getPartitionStoppableCheck(getBaseUrl(instance, restConfig),
-              expiredPartitions, customPayLoads);
-      updatedPartitionsHealthStatusByInstance.put(instance, POOL.submit(refreshTask));
-    }
-    updatedPartitionsHealthStatusByInstance.entrySet().forEach(instanceToPartitionStatus -> {
-      String instance = instanceToPartitionStatus.getKey();
-      try {
-        Map<String, Boolean> updatedPartitionsHealth = instanceToPartitionStatus.getValue().get();
-        updatedPartitionsHealth.entrySet().forEach(
-            partitionToHealthStatus -> clusterPartitionsHealth.updatePartitionHealth(instance,
-                partitionToHealthStatus.getKey(), partitionToHealthStatus.getValue()));
-      } catch (InterruptedException | ExecutionException e) {
-        LOG.error("Failed to get partition status on instance {}", instance, e);
-      }
-    });
-    // sibling checks on partitions health for entire cluster
-    PropertyKey.Builder propertyKeyBuilder = _dataAccessor.keyBuilder();
+    Map<String, Map<String, Boolean>> allPartitionsHealthOnLiveInstance =
+        _dataAccessor.getAllPartitionsHealthOnLiveInstance(restConfig, customPayLoads);
     List<ExternalView> externalViews =
-        _dataAccessor.getChildNames(propertyKeyBuilder.externalViews()).stream()
-            .map(externalView -> (ExternalView) _dataAccessor
-                .getProperty(propertyKeyBuilder.externalView(externalView)))
-            .collect(Collectors.toList());
-
+        _dataAccessor.getChildValues(_dataAccessor.keyBuilder().externalViews());
     Map<String, StoppableCheck> instanceStoppableChecks = new HashMap<>();
     for (String instanceName : instances) {
-      List<String> unHealthyPartitions =
-          InstanceValidationUtil.perPartitionHealthCheck(externalViews,
-              clusterPartitionsHealth.getGlobalPartitionHealth(), instanceName, _dataAccessor);
+      List<String> unHealthyPartitions = InstanceValidationUtil.perPartitionHealthCheck(
+          externalViews, allPartitionsHealthOnLiveInstance, instanceName, _dataAccessor);
       StoppableCheck stoppableCheck = new StoppableCheck(unHealthyPartitions.isEmpty(),
           unHealthyPartitions, StoppableCheck.Category.CUSTOM_PARTITION_CHECK);
       instanceStoppableChecks.put(instanceName, stoppableCheck);
@@ -289,55 +251,6 @@ public class InstanceServiceImpl implements InstanceService {
     // parsing the inputs as string key value pairs
     jsonNode.fields().forEachRemaining(kv -> result.put(kv.getKey(), kv.getValue().asText()));
     return result;
-  }
-
-  // TODO: move the method to {@link HelixDataAccessorWrapper}
-  @VisibleForTesting
-  protected PartitionHealth generatePartitionHealthMapFromZK() {
-    PartitionHealth partitionHealth = new PartitionHealth();
-
-    // Only checks the instances are online with valid reports
-    List<String> liveInstances =
-        _dataAccessor.getChildNames(_dataAccessor.keyBuilder().liveInstances());
-    // Make a parallel batch call for getting all healthreports from ZK.
-    List<HelixProperty> healthReports = _dataAccessor.getProperty(liveInstances.stream()
-        .map(instance -> _dataAccessor.keyBuilder().healthReport(instance, PARTITION_HEALTH_KEY))
-        .collect(Collectors.toList()));
-    for (int i = 0; i < liveInstances.size(); i++) {
-      String instance = liveInstances.get(i);
-      ZNRecord customizedHealth = healthReports.get(i).getRecord();
-
-      // No partition health ZNode found for that instance
-      // Direct call the instance to get partition health report.
-      if (customizedHealth == null) {
-        partitionHealth.addInstanceThatNeedDirectCall(instance);
-      }
-      for (String partitionName : customizedHealth.getMapFields().keySet()) {
-        try {
-          Map<String, String> healthMap = customizedHealth.getMapField(partitionName);
-          if (healthMap == null
-              || Long.parseLong(healthMap.get(EXPIRY_KEY)) < System.currentTimeMillis()) {
-            // Clean all the existing checks. If we do not clean it, when we do the customized
-            // check,
-            // Helix may think these partitions are only partitions holding on the instance.
-            // But it could potentially have some partitions are unhealthy for expired ones.
-            // It could problem for shutting down instances.
-            partitionHealth.addInstanceThatNeedDirectCallWithPartition(instance, partitionName);
-            continue;
-          }
-
-          partitionHealth.addSinglePartitionHealthForInstance(instance, partitionName,
-              Boolean.valueOf(healthMap.get(IS_HEALTHY_KEY)));
-        } catch (Exception e) {
-          LOG.warn(
-              "Error in processing partition level health for instance {}, partition {}, directly querying API",
-              instance, partitionName, e);
-          partitionHealth.addInstanceThatNeedDirectCallWithPartition(instance, partitionName);
-        }
-      }
-    }
-
-    return partitionHealth;
   }
 
   @VisibleForTesting
@@ -388,16 +301,5 @@ public class InstanceServiceImpl implements InstanceService {
     }
 
     return healthStatus;
-  }
-
-  private String getBaseUrl(String instance, RESTConfig restConfig) {
-    String baseUrl = restConfig.get(RESTConfig.SimpleFields.CUSTOMIZED_HEALTH_URL);
-    // pre-assumption of the url, must be format of "http://*/path", the wildcard is replaceable by
-    // the instance vip
-    assert baseUrl.contains("*");
-    // pre-assumption of the instance name, must be format of <instanceVip>_<port>
-    assert instance.contains("_");
-    String instanceVip = instance.substring(0, instance.indexOf('_'));
-    return baseUrl.replace("*", instanceVip);
   }
 }

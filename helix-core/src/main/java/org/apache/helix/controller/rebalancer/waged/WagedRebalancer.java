b273df44fbf531605a6868080929b36c67327f32
Jiajun Wang <1803880+jiajunwang@users.noreply.github.com>
2019-08-28 22:28:10 -0700

    Resubmit the change: Refine the WAGED rebalancer related interfaces for integration (#431)
    * Refine the WAGED rebalancer related interfaces and initial integrate with the BestPossibleStateCalStage.
    
    - Modify the BestPossibleStateCalStage logic to plugin the WAGED rebalancer.
    - Refine ClusterModel to integrate with the ClusterDataDetector implementation.
    - Enabling getting the changed details for Cluster Config in the change detector. Which is required by the WAGED rebalancer.
    
    * Bring back the interface class and algorithm placeholder class that was removed prematurely.

--- helix-core/src/main/java/org/apache/helix/controller/rebalancer/waged/WagedRebalancer.java
+++ helix-core/src/main/java/org/apache/helix/controller/rebalancer/waged/WagedRebalancer.java
@@ -19,10 +19,14 @@ package org.apache.helix.controller.rebalancer.waged;
  * under the License.
  */
 
-import org.apache.helix.HelixException;
 import org.apache.helix.HelixManager;
+import org.apache.helix.HelixRebalanceException;
+import org.apache.helix.controller.changedetector.ResourceChangeDetector;
 import org.apache.helix.controller.dataproviders.ResourceControllerDataProvider;
-import org.apache.helix.controller.rebalancer.GlobalRebalancer;
+import org.apache.helix.controller.rebalancer.DelayedAutoRebalancer;
+import org.apache.helix.controller.rebalancer.internal.MappingCalculator;
+import org.apache.helix.controller.rebalancer.waged.constraints.ConstraintsRebalanceAlgorithm;
+import org.apache.helix.controller.rebalancer.waged.constraints.RebalanceAlgorithm;
 import org.apache.helix.controller.stages.CurrentStateOutput;
 import org.apache.helix.model.IdealState;
 import org.apache.helix.model.Resource;
@@ -36,23 +40,57 @@ import java.util.Map;
  * A placeholder before we have the implementation.
  * Weight-Aware Globally-Even Distribute Rebalancer.
  *
- * @see <a href="Design Document">https://github.com/apache/helix/wiki/Design-Proposal---Weight-Aware-Globally-Even-Distribute-Rebalancer</a>
+ * @see <a href="https://github.com/apache/helix/wiki/Design-Proposal---Weight-Aware-Globally-Even-Distribute-Rebalancer">
+ * Design Document
+ * </a>
  */
-public class WagedRebalancer implements GlobalRebalancer<ResourceControllerDataProvider> {
+public class WagedRebalancer {
   private static final Logger LOG = LoggerFactory.getLogger(WagedRebalancer.class);
 
-  @Override
-  public void init(HelixManager manager) { }
+  // --------- The following fields are placeholders and need replacement. -----------//
+  // TODO Shall we make the metadata store a static threadlocal object as well to avoid reinitialization?
+  private final AssignmentMetadataStore _assignmentMetadataStore;
+  private final RebalanceAlgorithm _rebalanceAlgorithm;
+  // ------------------------------------------------------------------------------------//
 
-  @Override
-  public Map<String, IdealState> computeNewIdealState(CurrentStateOutput currentStateOutput,
-      ResourceControllerDataProvider clusterData, Map<String, Resource> resourceMap)
-      throws HelixException {
-    return new HashMap<>();
+  // The cluster change detector is a stateful object. Make it static to avoid unnecessary
+  // reinitialization.
+  private static final ThreadLocal<ResourceChangeDetector> CHANGE_DETECTOR_THREAD_LOCAL =
+      new ThreadLocal<>();
+  private final MappingCalculator<ResourceControllerDataProvider> _mappingCalculator;
+
+  private ResourceChangeDetector getChangeDetector() {
+    if (CHANGE_DETECTOR_THREAD_LOCAL.get() == null) {
+      CHANGE_DETECTOR_THREAD_LOCAL.set(new ResourceChangeDetector());
+    }
+    return CHANGE_DETECTOR_THREAD_LOCAL.get();
   }
 
-  @Override
-  public RebalanceFailureReason getFailureReason() {
-    return new RebalanceFailureReason(RebalanceFailureType.UNKNOWN_FAILURE);
+  public WagedRebalancer(HelixManager helixManager) {
+    // TODO init the metadata store according to their requirement when integrate, or change to final static method if possible.
+    _assignmentMetadataStore = new AssignmentMetadataStore();
+    // TODO init the algorithm according to the requirement when integrate.
+    _rebalanceAlgorithm = new ConstraintsRebalanceAlgorithm();
+
+    // Use the mapping calculator in DelayedAutoRebalancer for calculating the final assignment
+    // output.
+    // This calculator will translate the best possible assignment into an applicable state mapping
+    // based on the current states.
+    // TODO abstract and separate the mapping calculator logic from the DelayedAutoRebalancer
+    _mappingCalculator = new DelayedAutoRebalancer();
+  }
+
+  /**
+   * Compute the new IdealStates for all the resources input. The IdealStates include both the new
+   * partition assignment (in the listFiles) and the new replica state mapping (in the mapFields).
+   * @param clusterData        The Cluster status data provider.
+   * @param resourceMap        A map containing all the rebalancing resources.
+   * @param currentStateOutput The present Current State of the cluster.
+   * @return A map containing the computed new IdealStates.
+   */
+  public Map<String, IdealState> computeNewIdealStates(ResourceControllerDataProvider clusterData,
+      Map<String, Resource> resourceMap, final CurrentStateOutput currentStateOutput)
+      throws HelixRebalanceException {
+    return new HashMap<>();
   }
 }

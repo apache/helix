38739ddcfd42736f0cc5d4ba882a2656a89c91f7
Yi Wang <i3.wangyi@gmail.com>
2019-09-11 16:48:34 -0700

    Add ResourcetopStateAntiAffinityConstraint
    The more total top state partitions assigned to the instance, the lower the score, vice versa.

---
+++ helix-core/src/main/java/org/apache/helix/controller/rebalancer/waged/constraints/ResourceTopStateAntiAffinityConstraint.java
@@ -0,0 +1,45 @@
+package org.apache.helix.controller.rebalancer.waged.constraints;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.helix.controller.rebalancer.waged.model.AssignableNode;
+import org.apache.helix.controller.rebalancer.waged.model.AssignableReplica;
+import org.apache.helix.controller.rebalancer.waged.model.ClusterContext;
+
+/**
+ * Evaluate the proposed assignment according to the top state replication count on the instacne.
+ * The more total top state partitions assigned to the instance, the lower the score, vice versa.
+ */
+public class ResourceTopStateAntiAffinityConstraint extends SoftConstraint {
+  @Override
+  protected float getAssignmentScore(AssignableNode node, AssignableReplica replica,
+      ClusterContext clusterContext) {
+    if (!replica.isReplicaTopState()) {
+      return (getMaxScore() + getMinScore()) / 2;
+    }
+
+    int curTopPartitionCountForResource = node.getAssignedTopStatePartitionsCount();
+    int doubleMaxTopStateCount = 2 * clusterContext.getEstimatedMaxTopStateCount();
+
+    return Math.max(
+        ((float) doubleMaxTopStateCount - curTopPartitionCountForResource) / doubleMaxTopStateCount,
+        0);
+  }
+}

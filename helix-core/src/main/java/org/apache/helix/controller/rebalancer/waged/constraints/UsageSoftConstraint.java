334089138c0b5ef3e84db77b28b1d11dab4c8d7d
Jiajun Wang <jjwang@linkedin.com>
2019-10-25 15:05:53 -0700

    Address comments.

--- helix-core/src/main/java/org/apache/helix/controller/rebalancer/waged/constraints/UsageSoftConstraint.java
+++ helix-core/src/main/java/org/apache/helix/controller/rebalancer/waged/constraints/UsageSoftConstraint.java
@@ -1,25 +1,27 @@
 package org.apache.helix.controller.rebalancer.waged.constraints;
 
-    /*
-     * Licensed to the Apache Software Foundation (ASF) under one
-     * or more contributor license agreements.  See the NOTICE file
-     * distributed with this work for additional information
-     * regarding copyright ownership.  The ASF licenses this file
-     * to you under the Apache License, Version 2.0 (the
-     * "License"); you may not use this file except in compliance
-     * with the License.  You may obtain a copy of the License at
-     *
-     *   http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing,
-     * software distributed under the License is distributed on an
-     * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-     * KIND, either express or implied.  See the License for the
-     * specific language governing permissions and limitations
-     * under the License.
-     */
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-    import org.apache.commons.math3.analysis.function.Sigmoid;
+import java.util.function.Function;
+
+import org.apache.commons.math3.analysis.function.Sigmoid;
 
 /**
  * The soft constraint that evaluates the assignment proposal based on usage.
@@ -61,8 +63,9 @@ abstract class UsageSoftConstraint extends SoftConstraint {
    * When the usage is close or larger than the estimate, the constraint returns a score that is
    * very close to the min score. Note even in this case, more usage will still be assigned with a
    * smaller score.
+   *
    * @param estimatedUsage The estimated usage that is between [0.0, 1.0]
-   * @param currentUsage The current usage that is between [0.0, 1.0]
+   * @param currentUsage   The current usage that is between [0.0, 1.0]
    * @return The score between [0.0, 1.0] that evaluates the utilization.
    */
   protected double computeUtilizationScore(double estimatedUsage, double currentUsage) {

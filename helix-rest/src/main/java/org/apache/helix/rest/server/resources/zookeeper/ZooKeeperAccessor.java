156013074b67632459215e0044d1da395ef5438e
Hunter Lee <hulee@linkedin.com>
2020-01-16 11:30:38 -0800

    Add ZooKeeperAccessor to helix-rest (#671)
    In order to provide basic ZkClient functionalities in helix-rest, we want to add a ZooKeeperAccessor class that supports endpoints like get(), getChildren(), and exists().
    
    Changelist:
    1. Fix the double-checked locking idiom for byte array ZkBaseDataAccessor
    2. Add ZooKeeperAccessor
    3. Add tests

---
+++ helix-rest/src/main/java/org/apache/helix/rest/server/resources/zookeeper/ZooKeeperAccessor.java
@@ -0,0 +1,191 @@
+package org.apache.helix.rest.server.resources.zookeeper;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+import com.google.common.base.Enums;
+import com.google.common.collect.ImmutableMap;
+import org.apache.helix.AccessOption;
+import org.apache.helix.manager.zk.ZkBaseDataAccessor;
+import org.apache.helix.rest.common.ContextPropertyKeys;
+import org.apache.helix.rest.server.ServerContext;
+import org.apache.helix.rest.server.resources.AbstractResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * ZooKeeperAccessor provides methods for accessing ZooKeeper resources (ZNodes).
+ * It provides basic ZooKeeper features supported by ZkClient.
+ */
+@Path("/zookeeper")
+public class ZooKeeperAccessor extends AbstractResource {
+  private static final Logger LOG = LoggerFactory.getLogger(ZooKeeperAccessor.class.getName());
+  private ZkBaseDataAccessor<byte[]> _zkBaseDataAccessor;
+
+  public enum ZooKeeperCommand {
+    exists,
+    getBinaryData,
+    getStringData,
+    getChildren
+  }
+
+  @GET
+  @Path("{path: .+}")
+  public Response get(@PathParam("path") String path, @QueryParam("command") String commandStr) {
+    ZooKeeperCommand cmd = getZooKeeperCommandIfPresent(commandStr);
+    if (cmd == null) {
+      return badRequest("Invalid ZooKeeper command: " + commandStr);
+    }
+
+    // Lazily initialize ZkBaseDataAccessor
+    ServerContext _serverContext =
+        (ServerContext) _application.getProperties().get(ContextPropertyKeys.SERVER_CONTEXT.name());
+    _zkBaseDataAccessor = _serverContext.getByteArrayZkBaseDataAccessor();
+
+    // Need to prepend a "/" since JAX-RS regex removes it
+    path = "/" + path;
+
+    // Check that the path supplied is valid
+    if (!isPathValid(path)) {
+      String errMsg = "The given path is not a valid ZooKeeper path: " + path;
+      LOG.info(errMsg);
+      return badRequest(errMsg);
+    }
+
+    switch (cmd) {
+      case exists:
+        return exists(_zkBaseDataAccessor, path);
+      case getBinaryData:
+        return getBinaryData(_zkBaseDataAccessor, path);
+      case getStringData:
+        return getStringData(_zkBaseDataAccessor, path);
+      case getChildren:
+        return getChildren(_zkBaseDataAccessor, path);
+      default:
+        String errMsg = "Unsupported command: " + commandStr;
+        LOG.error(errMsg);
+        return badRequest(errMsg);
+    }
+  }
+
+  /**
+   * Checks if a ZNode exists in the given path.
+   * @param zkBaseDataAccessor
+   * @param path
+   * @return true if a ZNode exists, false otherwise
+   */
+  private Response exists(ZkBaseDataAccessor<byte[]> zkBaseDataAccessor, String path) {
+    Map<String, Boolean> result = ImmutableMap.of(ZooKeeperCommand.exists.name(),
+        zkBaseDataAccessor.exists(path, AccessOption.PERSISTENT));
+    return JSONRepresentation(result);
+  }
+
+  /**
+   * Returns a response containing the binary data.
+   * @param zkBaseDataAccessor
+   * @param path
+   * @return
+   */
+  private Response getBinaryData(ZkBaseDataAccessor<byte[]> zkBaseDataAccessor, String path) {
+    byte[] bytes = readBinaryDataFromZK(zkBaseDataAccessor, path);
+    Map<String, byte[]> binaryResult =
+        ImmutableMap.of(ZooKeeperCommand.getBinaryData.name(), bytes);
+    // Note: this serialization (using ObjectMapper) will convert this byte[] into
+    // a Base64 String! The REST client (user) must convert the resulting String back into
+    // a byte[] using Base64.
+    return JSONRepresentation(binaryResult);
+  }
+
+  /**
+   * Returns a response containing the string data.
+   * @param zkBaseDataAccessor
+   * @param path
+   * @return
+   */
+  private Response getStringData(ZkBaseDataAccessor<byte[]> zkBaseDataAccessor, String path) {
+    byte[] bytes = readBinaryDataFromZK(zkBaseDataAccessor, path);
+    Map<String, String> stringResult =
+        ImmutableMap.of(ZooKeeperCommand.getStringData.name(), new String(bytes));
+    return JSONRepresentation(stringResult);
+  }
+
+  /**
+   * Returns byte[] from ZooKeeper.
+   * @param zkBaseDataAccessor
+   * @param path
+   * @return
+   */
+  private byte[] readBinaryDataFromZK(ZkBaseDataAccessor<byte[]> zkBaseDataAccessor, String path) {
+    if (zkBaseDataAccessor.exists(path, AccessOption.PERSISTENT)) {
+      return zkBaseDataAccessor.get(path, null, AccessOption.PERSISTENT);
+    } else {
+      throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+          .entity(String.format("The ZNode at path %s does not exist!", path)).build());
+    }
+  }
+
+  /**
+   * Returns a list of children ZNode names given the path for the parent ZNode.
+   * @param zkBaseDataAccessor
+   * @param path
+   * @return list of child ZNodes
+   */
+  private Response getChildren(ZkBaseDataAccessor<byte[]> zkBaseDataAccessor, String path) {
+    if (_zkBaseDataAccessor.exists(path, AccessOption.PERSISTENT)) {
+      Map<String, List<String>> result = ImmutableMap.of(ZooKeeperCommand.getChildren.name(),
+          _zkBaseDataAccessor.getChildNames(path, AccessOption.PERSISTENT));
+      return JSONRepresentation(result);
+    } else {
+      throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+          .entity(String.format("The ZNode at path %s does not exist", path)).build());
+    }
+  }
+
+  private ZooKeeperCommand getZooKeeperCommandIfPresent(String command) {
+    return Enums.getIfPresent(ZooKeeperCommand.class, command).orNull();
+  }
+
+  /**
+   * Validates whether a given path string is a valid ZK path.
+   *
+   * Valid matches:
+   * /
+   * /abc
+   * /abc/abc/abc/abc
+   * Invalid matches:
+   * null or empty string
+   * /abc/
+   * /abc/abc/abc/abc/
+   **/
+  public static boolean isPathValid(String path) {
+    return path.matches("^/|(/[\\w-]+)+$");
+  }
+}
---
+++ helix-rest/src/test/java/org/apache/helix/rest/server/TestZooKeeperAccessor.java
@@ -0,0 +1,160 @@
+package org.apache.helix.rest.server;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.core.Response;
+
+import org.I0Itec.zkclient.exception.ZkMarshallingError;
+import org.I0Itec.zkclient.serialize.ZkSerializer;
+import org.apache.helix.AccessOption;
+import org.apache.helix.manager.zk.ZkBaseDataAccessor;
+import org.apache.helix.rest.server.util.JerseyUriRequestBuilder;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class TestZooKeeperAccessor extends AbstractTestClass {
+  private ZkBaseDataAccessor<byte[]> _testBaseDataAccessor;
+
+  @BeforeClass
+  public void beforeClass() {
+    _testBaseDataAccessor = new ZkBaseDataAccessor<>(ZK_ADDR, new ZkSerializer() {
+      @Override
+      public byte[] serialize(Object o)
+          throws ZkMarshallingError {
+        return (byte[]) o;
+      }
+
+      @Override
+      public Object deserialize(byte[] bytes)
+          throws ZkMarshallingError {
+        return new String(bytes);
+      }
+    });
+  }
+
+  @AfterClass
+  public void afterClass() {
+    _testBaseDataAccessor.close();
+  }
+
+  @Test
+  public void testExists()
+      throws IOException {
+    String path = "/path";
+    Assert.assertFalse(_testBaseDataAccessor.exists(path, AccessOption.PERSISTENT));
+    Map<String, Boolean> result;
+    String data = new JerseyUriRequestBuilder("zookeeper{}?command=exists").format(path)
+        .isBodyReturnExpected(true).get(this);
+    result = OBJECT_MAPPER.readValue(data, HashMap.class);
+    Assert.assertTrue(result.containsKey("exists"));
+    Assert.assertFalse(result.get("exists"));
+
+    // Create a ZNode and check again
+    String content = "testExists";
+    Assert.assertTrue(
+        _testBaseDataAccessor.create(path, content.getBytes(), AccessOption.PERSISTENT));
+    Assert.assertTrue(_testBaseDataAccessor.exists(path, AccessOption.PERSISTENT));
+
+    data = new JerseyUriRequestBuilder("zookeeper{}?command=exists").format(path)
+        .isBodyReturnExpected(true).get(this);
+    result = OBJECT_MAPPER.readValue(data, HashMap.class);
+    Assert.assertTrue(result.containsKey("exists"));
+    Assert.assertTrue(result.get("exists"));
+
+    // Clean up
+    _testBaseDataAccessor.remove(path, AccessOption.PERSISTENT);
+  }
+
+  @Test
+  public void testGetData()
+      throws IOException {
+    String path = "/path";
+    String content = "testGetData";
+
+    Assert.assertFalse(_testBaseDataAccessor.exists(path, AccessOption.PERSISTENT));
+    // Expect BAD_REQUEST
+    String data = new JerseyUriRequestBuilder("zookeeper{}?command=getStringData").format(path)
+        .isBodyReturnExpected(false)
+        .expectedReturnStatusCode(Response.Status.NOT_FOUND.getStatusCode()).get(this);
+
+    // Now write data and test
+    _testBaseDataAccessor.create(path, content.getBytes(), AccessOption.PERSISTENT);
+
+    // Test getStringData
+    String getStringDataKey = "getStringData";
+    data = new JerseyUriRequestBuilder("zookeeper{}?command=getStringData").format(path)
+        .isBodyReturnExpected(true).get(this);
+    Map<String, String> stringResult = OBJECT_MAPPER.readValue(data, Map.class);
+    Assert.assertTrue(stringResult.containsKey(getStringDataKey));
+    Assert.assertEquals(stringResult.get(getStringDataKey), content);
+
+    // Test getBinaryData
+    String getBinaryDataKey = "getBinaryData";
+    data = new JerseyUriRequestBuilder("zookeeper{}?command=getBinaryData").format(path)
+        .isBodyReturnExpected(true).get(this);
+    Map<String, String> binaryResult = OBJECT_MAPPER.readValue(data, Map.class);
+    Assert.assertTrue(binaryResult.containsKey(getBinaryDataKey));
+    // Note: The response's byte array is encoded into a String using Base64 (for safety),
+    // so the user must decode with Base64 to get the original byte array back
+    byte[] decodedBytes = Base64.getDecoder().decode(binaryResult.get(getBinaryDataKey));
+    Assert.assertEquals(decodedBytes, content.getBytes());
+
+    // Clean up
+    _testBaseDataAccessor.remove(path, AccessOption.PERSISTENT);
+  }
+
+  @Test
+  public void testGetChildren()
+      throws IOException {
+    String path = "/path";
+    String childrenKey = "/children";
+    int numChildren = 20;
+
+    // Create a ZNode and its children
+    for (int i = 0; i < numChildren; i++) {
+      _testBaseDataAccessor.create(path + childrenKey, null, AccessOption.PERSISTENT_SEQUENTIAL);
+    }
+
+    // Verify
+    String getChildrenKey = "getChildren";
+    String data = new JerseyUriRequestBuilder("zookeeper{}?command=getChildren").format(path)
+        .isBodyReturnExpected(true).get(this);
+    Map<String, List<String>> result = OBJECT_MAPPER.readValue(data, HashMap.class);
+    Assert.assertTrue(result.containsKey(getChildrenKey));
+    Assert.assertEquals(result.get(getChildrenKey).size(), numChildren);
+
+    // Check that all children are indeed created with PERSISTENT_SEQUENTIAL
+    result.get(getChildrenKey).forEach(child -> {
+      Assert.assertTrue(child.contains("children"));
+    });
+
+    // Clean up
+    _testBaseDataAccessor.remove(path, AccessOption.PERSISTENT);
+  }
+}

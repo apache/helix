a23d8e4c3cc413ca2f3976ff0cbeeee8d8aafbf2
Hunter Lee <hulee@linkedin.com>
2020-02-03 19:53:47 -0800

    Add MetadataStoreDirectory and ZkMetadataStoreDirectory

---
+++ helix-rest/src/main/java/org/apache/helix/rest/metadatastore/MetadataStoreDirectory.java
@@ -0,0 +1,122 @@
+package org.apache.helix.rest.metadatastore;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.List;
+import java.util.Map;
+
+
+public interface MetadataStoreDirectory {
+
+  /**
+   * Retrieves all existing namespaces in the routing metadata store.
+   * @return
+   */
+  List<String> getAllNamespaces();
+
+  /**
+   * Returns all metadata store realms in the given namespace.
+   * @return
+   */
+  List<String> getAllMetadataStoreRealms(String namespace);
+
+  /**
+   * Returns all path-based sharding keys in the given namespace.
+   * @return
+   */
+  List<String> getAllShardingKeys(String namespace);
+
+  /**
+   * Returns all path-based sharding keys in the given namespace and the realm.
+   * @param namespace
+   * @param realm
+   * @return
+   */
+  List<String> getAllShardingKeysInRealm(String namespace, String realm);
+
+  /**
+   * Returns all sharding keys that have the given path as the prefix substring.
+   * E.g) Given that there are sharding keys: /a/b/c, /a/b/d, /a/e,
+   * getAllShardingKeysUnderPath(namespace, "/a/b") returns ["/a/b/c", "/a/b/d"].
+   * @param namespace
+   * @param path
+   * @return
+   */
+  Map<String, String> getAllShardingKeysUnderPath(String namespace, String path);
+
+  /**
+   * Returns the name of the metadata store realm based on the namespace and the sharding key given.
+   * @param namespace
+   * @param shardingKey
+   * @return
+   */
+  String getMetadataStoreRealm(String namespace, String shardingKey);
+
+  /**
+   * NOTE: The following CRUD methods are idempotent.
+   */
+  /**
+   * Creates a namespace in the routing metadata store.
+   * @param namespace
+   * @return true if successful or if the namespace already exists. false if not successful.
+   */
+  boolean addNamespace(String namespace);
+
+  /**
+   * Deletes a namespace in the routing metadata store.
+   * @param namespace
+   * @return true if successful or if the namespace does not exist. false otherwise.
+   */
+  boolean deleteNamespace(String namespace);
+
+  /**
+   * Creates a realm. If the namespace does not exist, it creates one.
+   * @param namespace
+   * @param realm
+   * @return true if successful or if the realm already exists. false otherwise.
+   */
+  boolean addMetadataStoreRealm(String namespace, String realm);
+
+  /**
+   * Deletes a realm.
+   * @param namespace
+   * @param realm
+   * @return true if successful or the realm or namespace does not exist. false otherwise.
+   */
+  boolean deleteMetadataStoreRealm(String namespace, String realm);
+
+  /**
+   * Creates a mapping between the sharding key to the realm in the given namespace.
+   * @param namespace
+   * @param realm
+   * @param shardingKey
+   * @return false if failed
+   */
+  boolean addShardingKey(String namespace, String realm, String shardingKey);
+
+  /**
+   * Deletes the mapping between the sharding key to the realm in the given namespace.
+   * @param namespace
+   * @param realm
+   * @param shardingKey
+   * @return false if failed; true if the deletion is successful or the key does not exist.
+   */
+  boolean deleteShardingKey(String namespace, String realm, String shardingKey);
+}
---
+++ helix-rest/src/main/java/org/apache/helix/rest/metadatastore/ZkMetadataStoreDirectory.java
@@ -0,0 +1,159 @@
+package org.apache.helix.rest.metadatastore;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.List;
+import java.util.Map;
+
+import org.I0Itec.zkclient.IZkChildListener;
+import org.I0Itec.zkclient.IZkDataListener;
+import org.apache.helix.manager.zk.ZNRecordSerializer;
+import org.apache.helix.manager.zk.client.DedicatedZkClientFactory;
+import org.apache.helix.manager.zk.client.HelixZkClient;
+import org.apache.helix.manager.zk.client.HelixZkClient.ZkClientConfig;
+import org.apache.helix.manager.zk.zookeeper.IZkStateListener;
+import org.apache.zookeeper.Watcher;
+
+
+public class ZkMetadataStoreDirectory implements MetadataStoreDirectory, IZkDataListener, IZkChildListener, IZkStateListener {
+  // TODO: enable the line below when implementation is complete
+//  private final MetadataStoreRoutingDataAccessor _routingDataAccessor;
+  private MetadataStoreRoutingData _routingData;
+
+  public ZkMetadataStoreDirectory(String zkAddress) {
+//    _routingDataAccessor = new ZkRoutingDataAccessor(zkAddress, this); // dependency injection
+    // 1. Create a RoutingDataTrieNode using routingDataAccessor
+    // _routingData = constructRoutingData(_routingDataAccessor);
+  }
+
+  @Override
+  public List<String> getAllNamespaces() {
+    return null;
+  }
+
+  @Override
+  public List<String> getAllMetadataStoreRealms(String namespace) {
+    return null;
+  }
+
+  @Override
+  public List<String> getAllShardingKeys(String namespace) {
+    return null;
+  }
+
+  @Override
+  public List<String> getAllShardingKeysInRealm(String namespace, String realm) {
+    return null;
+  }
+
+  @Override
+  public Map<String, String> getAllShardingKeysUnderPath(String namespace, String path) {
+    return null;
+  }
+
+  @Override
+  public String getMetadataStoreRealm(String namespace, String shardingKey) {
+    return null;
+  }
+
+  @Override
+  public boolean addNamespace(String namespace) {
+    return false;
+  }
+
+  @Override
+  public boolean deleteNamespace(String namespace) {
+    return false;
+  }
+
+  @Override
+  public boolean addMetadataStoreRealm(String namespace, String realm) {
+    return false;
+  }
+
+  @Override
+  public boolean deleteMetadataStoreRealm(String namespace, String realm) {
+    return false;
+  }
+
+  @Override
+  public boolean addShardingKey(String namespace, String realm, String shardingKey) {
+    return false;
+  }
+
+  @Override
+  public boolean deleteShardingKey(String namespace, String realm, String shardingKey) {
+    return false;
+  }
+
+  /*
+    The following methods reconstruct the MetadataStoreRoutingData by listening to child/data/state changes
+   */
+  @Override
+  public void handleChildChange(String s, List<String> list)
+      throws Exception {
+
+  }
+
+  @Override
+  public void handleDataChange(String s, Object o)
+      throws Exception {
+
+  }
+
+  @Override
+  public void handleDataDeleted(String s)
+      throws Exception {
+
+  }
+
+  @Override
+  public void handleStateChanged(Watcher.Event.KeeperState state)
+      throws Exception {
+
+  }
+
+  @Override
+  public void handleNewSession(String sessionId)
+      throws Exception {
+
+  }
+
+  @Override
+  public void handleSessionEstablishmentError(Throwable error)
+      throws Exception {
+
+  }
+
+//  /**
+//   * Reconstructs MetadataStoreRoutingData by reading from the metadata store.
+//   * @param routingDataAccessor
+//   * @return
+//   */
+//  private MetadataStoreRoutingData constructRoutingData(MetadataStoreRoutingDataAccessor routingDataAccessor) {
+//    /**
+//     * Trie construction logic
+//     */
+//
+//    // 1. Construct
+//    // 2. Update the in-memory reference when complete
+//    _routingData = newRoutingData;
+//  }
+}

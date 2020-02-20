f80d76e602feb2814aac8ea489d8cccdc76d6ba3
zhangmeng916 <56051770+zhangmeng916@users.noreply.github.com>
2020-02-20 11:53:44 -0800

    add CustomizedStateAggregation config (#776)
    Add CustomizedViewAggregation as a cluster level config. This config defines the types of customized states that will be aggregated by Helix controller to generate a customized view. If Helix customers would like to have an aggregated view generated for their own states, they will need to add the type of the state to the list view in this config.

--- helix-core/src/main/java/org/apache/helix/model/HelixConfigScope.java
+++ helix-core/src/main/java/org/apache/helix/model/HelixConfigScope.java
@@ -36,7 +36,8 @@ public class HelixConfigScope {
     RESOURCE(2, 0),
     PARTITION(2, 1),
     CONSTRAINT(2, 0),
-    REST(2, 0);
+    REST(2, 0),
+    CUSTOMIZED_STATE_AGGREGATION(2, 0);
 
     final int _zkPathArgNum;
     final int _mapKeyArgNum;
@@ -84,6 +85,8 @@ public class HelixConfigScope {
     template.addEntry(ConfigScopeProperty.PARTICIPANT, 1, "/{clusterName}/CONFIGS/PARTICIPANT");
     template.addEntry(ConfigScopeProperty.RESOURCE, 1, "/{clusterName}/CONFIGS/RESOURCE");
     template.addEntry(ConfigScopeProperty.REST, 2, "/{clusterName}/CONFIGS/REST/{clusterName}");
+    template.addEntry(ConfigScopeProperty.CUSTOMIZED_STATE_AGGREGATION, 2,
+        "/{clusterName}/CONFIGS/CUSTOMIZED_STATE_AGGREGATION/{clusterName}");
   }
 
   final ConfigScopeProperty _type;

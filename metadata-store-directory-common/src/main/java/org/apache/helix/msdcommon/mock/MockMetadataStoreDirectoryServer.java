7e3688f6091d686a35487cfd69e9f0ff6744070a
Hunter Lee <hulee@linkedin.com>
2020-02-25 14:45:28 -0800

    Add HttpRoutingDataReader (#775)
    HttpRoutingDataReader is a component used by new ZkClient APIs to make an HTTP read request to the metadata store directory service to retrieve routing data. ZkClient APIs will construct an internal MetadataStoreRoutingData instance based on the raw routing data retrieved from MSDS.
    
    Note: this change contains modifications to MockMSDS because the actual endpoint names changed. The methods and http server contexts have been updated.

--- metadata-store-directory-common/src/main/java/org/apache/helix/msdcommon/mock/MockMetadataStoreDirectoryServer.java
+++ metadata-store-directory-common/src/main/java/org/apache/helix/msdcommon/mock/MockMetadataStoreDirectoryServer.java
@@ -1,4 +1,4 @@
-package org.apache.helix.rest.metadatastore.mock;
+package org.apache.helix.msdcommon.mock;
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -22,13 +22,19 @@ package org.apache.helix.rest.metadatastore.mock;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.apache.helix.msdcommon.constant.MetadataStoreRoutingConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,14 +47,15 @@ public class MockMetadataStoreDirectoryServer {
   private static final Logger LOG = LoggerFactory.getLogger(MockMetadataStoreDirectoryServer.class);
 
   protected static final String REST_PREFIX = "/admin/v2/namespaces/";
-  protected static final String ZK_REALM_ENDPOINT = "/METADATA_STORE_ROUTING_DATA/";
+  protected static final String ZK_REALM_ENDPOINT =
+      MetadataStoreRoutingConstants.MSDS_GET_ALL_REALMS_ENDPOINT;
   protected static final int NOT_IMPLEMENTED = 501;
   protected static final int OK = 200;
   protected static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
   protected final String _hostname;
   protected final int _mockServerPort;
-  protected final Map<String, List<String>> _routingDataMap;
+  protected final Map<String, Collection<String>> _routingDataMap;
   protected final String _namespace;
   protected HttpServer _server;
   protected final ThreadPoolExecutor _executor =
@@ -68,7 +75,7 @@ public class MockMetadataStoreDirectoryServer {
    * @param routingData <ZK realm, List of ZK path sharding keys>
    */
   public MockMetadataStoreDirectoryServer(String hostname, int port, String namespace,
-      Map<String, List<String>> routingData) {
+      Map<String, Collection<String>> routingData) {
     if (hostname == null || hostname.isEmpty()) {
       throw new IllegalArgumentException("hostname cannot be null or empty!");
     }
@@ -108,20 +115,45 @@ public class MockMetadataStoreDirectoryServer {
    * Dynamically generates HTTP server contexts based on the routing data given.
    */
   private void generateContexts() {
+    // Get all routing data endpoint
+    // Get the result to be in the MetadataStoreShardingKeysByRealm format
+    List<Map<String, Object>> result = _routingDataMap.entrySet().stream().map(entry -> ImmutableMap
+        .of(MetadataStoreRoutingConstants.SINGLE_METADATA_STORE_REALM, entry.getKey(),
+            MetadataStoreRoutingConstants.SHARDING_KEYS, entry.getValue()))
+        .collect(Collectors.toList());
+    _server.createContext(
+        REST_PREFIX + _namespace + MetadataStoreRoutingConstants.MSDS_GET_ALL_ROUTING_DATA_ENDPOINT,
+        createHttpHandler(ImmutableMap
+            .of(MetadataStoreRoutingConstants.SINGLE_METADATA_STORE_NAMESPACE, _namespace,
+                MetadataStoreRoutingConstants.ROUTING_DATA, result)));
+
+    // Get all realms endpoint
+    _server.createContext(REST_PREFIX + _namespace + ZK_REALM_ENDPOINT, createHttpHandler(
+        ImmutableMap
+            .of(MetadataStoreRoutingConstants.METADATA_STORE_REALMS, _routingDataMap.keySet())));
+
+    // Get all sharding keys for a realm endpoint
     _routingDataMap.forEach((zkRealm, shardingKeyList) -> _server
-        .createContext(REST_PREFIX + _namespace + ZK_REALM_ENDPOINT + zkRealm, httpExchange -> {
-          OutputStream outputStream = httpExchange.getResponseBody();
-          String htmlResponse;
-          if (SupportedHttpVerbs.GET.name().equals(httpExchange.getRequestMethod())) {
-            htmlResponse = OBJECT_MAPPER.writeValueAsString(shardingKeyList);
-            httpExchange.sendResponseHeaders(OK, htmlResponse.length());
-          } else {
-            htmlResponse = httpExchange.getRequestMethod() + " is not supported!\n";
-            httpExchange.sendResponseHeaders(NOT_IMPLEMENTED, htmlResponse.length());
-          }
-          outputStream.write(htmlResponse.getBytes());
-          outputStream.flush();
-          outputStream.close();
-        }));
+        .createContext(REST_PREFIX + _namespace + ZK_REALM_ENDPOINT + "/" + zkRealm,
+            createHttpHandler(ImmutableMap
+                .of(MetadataStoreRoutingConstants.SINGLE_METADATA_STORE_REALM, zkRealm,
+                    MetadataStoreRoutingConstants.SHARDING_KEYS, shardingKeyList))));
+  }
+
+  private HttpHandler createHttpHandler(Map<String, Object> keyValuePairs) {
+    return httpExchange -> {
+      OutputStream outputStream = httpExchange.getResponseBody();
+      String htmlResponse;
+      if (SupportedHttpVerbs.GET.name().equals(httpExchange.getRequestMethod())) {
+        htmlResponse = OBJECT_MAPPER.writeValueAsString(keyValuePairs);
+        httpExchange.sendResponseHeaders(OK, htmlResponse.length());
+      } else {
+        htmlResponse = httpExchange.getRequestMethod() + " is not supported!\n";
+        httpExchange.sendResponseHeaders(NOT_IMPLEMENTED, htmlResponse.length());
+      }
+      outputStream.write(htmlResponse.getBytes());
+      outputStream.flush();
+      outputStream.close();
+    };
   }
 }

c130620a404292554d67a989bca47736f67278f0
Hunter Lee <hulee@linkedin.com>
2020-02-24 19:21:25 -0800

    Address comments

--- zookeeper-api/src/main/java/org/apache/helix/zookeeper/util/HttpRoutingDataReader.java
+++ zookeeper-api/src/main/java/org/apache/helix/zookeeper/util/HttpRoutingDataReader.java
@@ -23,10 +23,9 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import com.jcabi.aspects.RetryOnFailure;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.helix.msdcommon.constant.MetadataStoreRoutingConstants;
 import org.apache.helix.msdcommon.datamodel.MetadataStoreRoutingData;
 import org.apache.helix.msdcommon.datamodel.TrieRoutingData;
@@ -35,13 +34,17 @@ import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultBackoffStrategy;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.codehaus.jackson.map.ObjectMapper;
 
 
 public class HttpRoutingDataReader {
-  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  private static final String MSDS_ENDPOINT =
+      System.getProperty(MetadataStoreRoutingConstants.MSDS_SERVER_ENDPOINT_KEY);
+
+  /** Double-checked locking requires that the following fields be volatile */
   private static volatile Map<String, List<String>> _rawRoutingData;
   private static volatile MetadataStoreRoutingData _metadataStoreRoutingData;
 
@@ -57,25 +60,18 @@ public class HttpRoutingDataReader {
    * "metadata store sharding keys", where the sharding keys in a value list all route to
    * the realm address in the key disallows a meaningful mapping to be returned
    */
-  @RetryOnFailure(attempts = 3, delay = 1, unit = TimeUnit.SECONDS)
   public static Map<String, List<String>> getRawRoutingData()
       throws IOException {
+    if (MSDS_ENDPOINT == null || MSDS_ENDPOINT.isEmpty()) {
+      throw new IllegalStateException(
+          "HttpRoutingDataReader was unable to find a valid MSDS endpoint String in System Properties!");
+    }
     if (_rawRoutingData == null) {
       synchronized (HttpRoutingDataReader.class) {
         if (_rawRoutingData == null) {
-          String msdsEndpoint =
-              System.getProperty(MetadataStoreRoutingConstants.MSDS_SERVER_ENDPOINT_KEY);
-          if (msdsEndpoint == null || msdsEndpoint.isEmpty()) {
-            throw new IllegalStateException(
-                "HttpRoutingDataReader was unable to find a valid MSDS endpoint String in System Properties!");
-          }
-
-          // Note: HttpClient's timeout settings are system timeout settings by default
-          CloseableHttpClient httpClient = HttpClients.createDefault();
-          // Update the reference
-          _rawRoutingData = getAllRoutingData(httpClient, msdsEndpoint);
-          // Close any open resources
-          httpClient.close();
+          CloseableHttpResponse routingDataResponse = getAllRoutingData();
+          // Update the reference if reading routingData over HTTP is successful
+          _rawRoutingData = parseRoutingData(routingDataResponse);
         }
       }
     }
@@ -85,8 +81,8 @@ public class HttpRoutingDataReader {
   /**
    * Returns the routing data read from MSDS in a MetadataStoreRoutingData format.
    * @return
-   * @throws IOException
-   * @throws InvalidRoutingDataException
+   * @throws IOException if there is an issue connecting to MSDS
+   * @throws InvalidRoutingDataException if the raw routing data is not valid
    */
   public static MetadataStoreRoutingData getMetadataStoreRoutingData()
       throws IOException, InvalidRoutingDataException {
@@ -102,28 +98,40 @@ public class HttpRoutingDataReader {
 
   /**
    * Makes an HTTP call to fetch all routing data.
-   * @param httpClient
-   * @param msdsEndpoint
    * @return
    * @throws IOException
    */
-  private static Map<String, List<String>> getAllRoutingData(CloseableHttpClient httpClient,
-      String msdsEndpoint)
+  private static CloseableHttpResponse getAllRoutingData()
       throws IOException {
+    // Retry count is 3 by default
     HttpGet requestAllData = new HttpGet(
-        msdsEndpoint + MetadataStoreRoutingConstants.MSDS_GET_ALL_ROUTING_DATA_ENDPOINT);
-    CloseableHttpResponse response = httpClient.execute(requestAllData);
-    HttpEntity entity = response.getEntity();
+        MSDS_ENDPOINT + MetadataStoreRoutingConstants.MSDS_GET_ALL_ROUTING_DATA_ENDPOINT);
+    try (CloseableHttpClient httpClient = HttpClients.custom()
+        .setConnectionBackoffStrategy(new DefaultBackoffStrategy())
+        .setRetryHandler(new DefaultHttpRequestRetryHandler()).build()) {
+      return httpClient.execute(requestAllData);
+    }
+  }
+
+  /**
+   * Returns the raw routing data in a Map< ZkRealm, List of shardingKeys > format.
+   * @param routingDataResponse
+   * @return
+   */
+  private static Map<String, List<String>> parseRoutingData(
+      CloseableHttpResponse routingDataResponse)
+      throws IOException {
+    HttpEntity entity = routingDataResponse.getEntity();
     if (entity != null) {
       String resultStr = EntityUtils.toString(entity);
       @SuppressWarnings("unchecked")
-      Map<String, Object> resultMap = OBJECT_MAPPER.readValue(resultStr, Map.class);
+      Map<String, Object> resultMap = new ObjectMapper().readValue(resultStr, Map.class);
       @SuppressWarnings("unchecked")
       List<Map<String, Object>> routingDataList =
           (List<Map<String, Object>>) resultMap.get(MetadataStoreRoutingConstants.ROUTING_DATA);
       @SuppressWarnings("unchecked")
       Map<String, List<String>> routingData = routingDataList.stream().collect(Collectors.toMap(
-          mapEntry -> (String) mapEntry
+          realmKeyPair -> (String) realmKeyPair
               .get(MetadataStoreRoutingConstants.SINGLE_METADATA_STORE_REALM),
           mapEntry -> (List<String>) mapEntry.get(MetadataStoreRoutingConstants.SHARDING_KEYS)));
       return routingData;
@@ -131,4 +139,3 @@ public class HttpRoutingDataReader {
     return Collections.emptyMap();
   }
 }
-

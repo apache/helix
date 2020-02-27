710c2f854c8a6572538f1cf395669e09046fd43b
Ali Reza Zamani Zadeh Najari <anajari@linkedin.com>
2020-02-26 16:29:27 -0800

    Add basic functionalities for RoutingTableProvider for CustomizedView (#814)
    This commit contains the basic functionalities for
    CustomizedView RoutingTableProvider.
    
    Here are the new added functionalities:
    1- CustomizedViewChangeListener
    2- Addition of CustomizedView to helix PropertyType and PropertyKey
    3- Implementation of CustomizedView cache.
    4- Registering CallbackHandler for CustomizedView.

--- helix-core/src/main/java/org/apache/helix/PropertyKey.java
+++ helix-core/src/main/java/org/apache/helix/PropertyKey.java
@@ -29,6 +29,7 @@ import org.apache.helix.model.ControllerHistory;
 import org.apache.helix.model.CurrentState;
 import org.apache.helix.model.CustomizedStateAggregationConfig;
 import org.apache.helix.model.CustomizedState;
+import org.apache.helix.model.CustomizedView;
 import org.apache.helix.model.Error;
 import org.apache.helix.model.ExternalView;
 import org.apache.helix.model.HealthStat;
@@ -58,6 +59,7 @@ import static org.apache.helix.PropertyType.CUSTOMIZEDSTATES;
 import static org.apache.helix.PropertyType.ERRORS;
 import static org.apache.helix.PropertyType.ERRORS_CONTROLLER;
 import static org.apache.helix.PropertyType.EXTERNALVIEW;
+import static org.apache.helix.PropertyType.CUSTOMIZEDVIEW;
 import static org.apache.helix.PropertyType.HISTORY;
 import static org.apache.helix.PropertyType.IDEALSTATES;
 import static org.apache.helix.PropertyType.INSTANCE_HISTORY;
@@ -72,6 +74,7 @@ import static org.apache.helix.PropertyType.STATEMODELDEFS;
 import static org.apache.helix.PropertyType.STATUSUPDATES;
 import static org.apache.helix.PropertyType.STATUSUPDATES_CONTROLLER;
 import static org.apache.helix.PropertyType.TARGETEXTERNALVIEW;
+
 
 /**
  * Key allowing for type-safe lookups of and conversions to {@link HelixProperty} objects.
@@ -636,6 +639,33 @@ public class PropertyKey {
      */
     public PropertyKey externalView(String resourceName) {
       return new PropertyKey(EXTERNALVIEW, ExternalView.class, _clusterName, resourceName);
+    }
+
+    /**
+     * Get a property key associated with all {@link CustomizedView}
+     * @return {@link PropertyKey}
+     */
+    public PropertyKey customizedView() {
+      return new PropertyKey(CUSTOMIZEDVIEW, CustomizedView.class, _clusterName);
+    }
+
+    /**
+     * Get a property key associated with an {@link CustomizedView} of a type
+     * @param aggregationType
+     * @return {@link PropertyKey}
+     */
+    public PropertyKey customizedView(String aggregationType) {
+      return new PropertyKey(CUSTOMIZEDVIEW, CustomizedView.class, _clusterName, aggregationType);
+    }
+
+    /**
+     * Get a property key associated with an {@link CustomizedView} of a type and resource
+     * @param aggregationType
+     * @return {@link PropertyKey}
+     */
+    public PropertyKey customizedView(String aggregationType, String resourceName) {
+      return new PropertyKey(CUSTOMIZEDVIEW, CustomizedView.class, _clusterName, aggregationType,
+          resourceName);
     }
 
     /**

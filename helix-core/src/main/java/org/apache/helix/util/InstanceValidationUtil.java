89dfe91345bde73a0f6928118a89bd8338399aa5
Hunter Lee <hulee@linkedin.com>
2020-03-05 14:43:28 -0800

    Add logs and throw exceptions in getInstanceById (#858)
    Add logs and throw exceptions in getInstanceById

--- helix-core/src/main/java/org/apache/helix/util/InstanceValidationUtil.java
+++ helix-core/src/main/java/org/apache/helix/util/InstanceValidationUtil.java
@@ -257,6 +257,9 @@ public class InstanceValidationUtil {
   public static boolean isInstanceStable(HelixDataAccessor dataAccessor, String instanceName) {
     PropertyKey.Builder keyBuilder = dataAccessor.keyBuilder();
     ClusterConfig clusterConfig = dataAccessor.getProperty(keyBuilder.clusterConfig());
+    if (clusterConfig == null) {
+      throw new HelixException("Missing cluster config!");
+    }
     if (!clusterConfig.isPersistIntermediateAssignment()) {
       throw new HelixException("isInstanceStable needs persist assignment on!");
     }
@@ -276,7 +279,17 @@ public class InstanceValidationUtil {
       }
       for (String partition : idealState.getPartitionSet()) {
         Map<String, String> isPartitionMap = idealState.getInstanceStateMap(partition);
+        if (isPartitionMap == null) {
+          throw new HelixException(String
+              .format("Partition %s of resource %s does not have an ideal state partition map",
+                  partition, idealStateName));
+        }
         Map<String, String> evPartitionMap = externalView.getStateMap(partition);
+        if (evPartitionMap == null) {
+          throw new HelixException(String
+              .format("Partition %s of resource %s does not have an external view partition map",
+                  partition, idealStateName));
+        }
         if (isPartitionMap.containsKey(instanceName) && (!evPartitionMap.containsKey(instanceName)
             || !evPartitionMap.get(instanceName).equals(isPartitionMap.get(instanceName)))) {
           // only checks the state from IS matches EV. Return false when

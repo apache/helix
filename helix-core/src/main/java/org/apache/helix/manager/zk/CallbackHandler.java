490850d70c69ba9980654125c41332a19ec40678
Molly Gao <mgao@linkedin.com>
2020-06-11 17:39:19 -0700

    Move update invoke time

--- helix-core/src/main/java/org/apache/helix/manager/zk/CallbackHandler.java
+++ helix-core/src/main/java/org/apache/helix/manager/zk/CallbackHandler.java
@@ -452,6 +452,10 @@ public class CallbackHandler implements IZkChildListener, IZkDataListener {
     Type type = changeContext.getType();
     long start = System.currentTimeMillis();
 
+    if (_periodicTriggerExecutor != null) {
+      _lastInvokeTime = System.currentTimeMillis();
+    }
+
     // This allows the listener to work with one change at a time
     synchronized (_manager) {
       if (logger.isInfoEnabled()) {
@@ -473,10 +477,6 @@ public class CallbackHandler implements IZkChildListener, IZkDataListener {
       } else {
         // put SubscribeForChange run in async thread to reduce the latency of zk callback handling.
         subscribeForChangesAsyn(changeContext.getType(), _path, _watchChild);
-      }
-
-      if (_periodicTriggerExecutor != null) {
-        _lastInvokeTime = System.currentTimeMillis();
       }
 
       if (_changeType == IDEAL_STATE) {

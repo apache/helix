2bbc52de4b73e84377eb9ccb17f68b44c1952b70
Ali Reza Zamani Zadeh Najari <anajari@linkedin.com>
2020-06-14 15:02:15 -0700

    Stabilize TestTaskThrottling
    In this commit, the Thread.sleep has been removed from TestTaskThrottling.

--- helix-core/src/test/java/org/apache/helix/integration/task/TestTaskThrottling.java
+++ helix-core/src/test/java/org/apache/helix/integration/task/TestTaskThrottling.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.common.collect.ImmutableMap;
+import org.apache.helix.TestHelper;
 import org.apache.helix.model.ClusterConfig;
 import org.apache.helix.model.HelixConfigScope;
 import org.apache.helix.model.InstanceConfig;
@@ -55,7 +56,7 @@ public class TestTaskThrottling extends TaskTestBase {
    * @throws InterruptedException
    */
   @Test
-  public void testTaskThrottle() throws InterruptedException {
+  public void testTaskThrottle() throws Exception {
     int numTasks = 30 * _numNodes; // 60 tasks
     int perNodeTaskLimitation = 5;
 
@@ -63,17 +64,16 @@ public class TestTaskThrottling extends TaskTestBase {
 
     // 1. Job executed in the participants with no limitation
     String jobName1 = "Job1";
-    Workflow flow = WorkflowGenerator.generateSingleJobWorkflowBuilder(jobName1, jobConfig).build();
-    _driver.start(flow);
-    _driver.pollForJobState(flow.getName(), TaskUtil.getNamespacedJobName(flow.getName(), jobName1),
+    Workflow flow1 = WorkflowGenerator.generateSingleJobWorkflowBuilder(jobName1, jobConfig).build();
+    _driver.start(flow1);
+    _driver.pollForJobState(flow1.getName(), TaskUtil.getNamespacedJobName(flow1.getName(), jobName1),
         TaskState.IN_PROGRESS);
-    // Wait for tasks to be picked up
-    Thread.sleep(1000L);
 
-    Assert.assertEquals(countRunningPartition(flow, jobName1), numTasks);
+    Assert.assertTrue(TestHelper.verify(() -> (countRunningPartition(flow1, jobName1) == numTasks),
+        TestHelper.WAIT_DURATION));
 
-    _driver.stop(flow.getName());
-    _driver.pollForWorkflowState(flow.getName(), TaskState.STOPPED);
+    _driver.stop(flow1.getName());
+    _driver.pollForWorkflowState(flow1.getName(), TaskState.STOPPED);
 
     // 2. Job executed in the participants with max task limitation
 
@@ -87,25 +87,25 @@ public class TestTaskThrottling extends TaskTestBase {
     _gSetupTool.getClusterManagementTool().setConfig(scope, properties);
 
     String jobName2 = "Job2";
-    flow = WorkflowGenerator.generateSingleJobWorkflowBuilder(jobName2, jobConfig).build();
-    _driver.start(flow);
-    _driver.pollForJobState(flow.getName(), TaskUtil.getNamespacedJobName(flow.getName(), jobName2),
+    Workflow flow2 = WorkflowGenerator.generateSingleJobWorkflowBuilder(jobName2, jobConfig).build();
+    _driver.start(flow2);
+    _driver.pollForJobState(flow2.getName(), TaskUtil.getNamespacedJobName(flow2.getName(), jobName2),
         TaskState.IN_PROGRESS);
-    // Wait for tasks to be picked up
-    Thread.sleep(1000L);
 
     // Expect 10 tasks
-    Assert.assertEquals(countRunningPartition(flow, jobName2), _numNodes * perNodeTaskLimitation);
+    Assert.assertTrue(TestHelper.verify(
+        () -> (countRunningPartition(flow2, jobName2) == (_numNodes * perNodeTaskLimitation)),
+        TestHelper.WAIT_DURATION));
 
-    _driver.stop(flow.getName());
-    _driver.pollForWorkflowState(flow.getName(), TaskState.STOPPED);
+    _driver.stop(flow2.getName());
+    _driver.pollForWorkflowState(flow2.getName(), TaskState.STOPPED);
 
     // 3. Ensure job can finish normally
     jobConfig.setJobCommandConfigMap(ImmutableMap.of(MockTask.JOB_DELAY, "10"));
     String jobName3 = "Job3";
-    flow = WorkflowGenerator.generateSingleJobWorkflowBuilder(jobName3, jobConfig).build();
-    _driver.start(flow);
-    _driver.pollForJobState(flow.getName(), TaskUtil.getNamespacedJobName(flow.getName(), jobName3),
+    Workflow flow3 = WorkflowGenerator.generateSingleJobWorkflowBuilder(jobName3, jobConfig).build();
+    _driver.start(flow3);
+    _driver.pollForJobState(flow3.getName(), TaskUtil.getNamespacedJobName(flow3.getName(), jobName3),
         TaskState.COMPLETED);
   }
 
@@ -113,7 +113,7 @@ public class TestTaskThrottling extends TaskTestBase {
   @Test(dependsOnMethods = {
       "testTaskThrottle"
   }, enabled = false)
-  public void testJobPriority() throws InterruptedException {
+  public void testJobPriority() throws Exception {
     int numTasks = 30 * _numNodes;
     int perNodeTaskLimitation = 5;
 
@@ -129,9 +129,10 @@ public class TestTaskThrottling extends TaskTestBase {
     _driver.start(flow1);
     _driver.pollForJobState(flow1.getName(),
         TaskUtil.getNamespacedJobName(flow1.getName(), jobName1), TaskState.IN_PROGRESS);
-    // Wait for tasks to be picked up
-    Thread.sleep(1000L);
-    Assert.assertEquals(countRunningPartition(flow1, jobName1), _numNodes * perNodeTaskLimitation);
+
+    Assert.assertTrue(TestHelper.verify(
+        () -> (countRunningPartition(flow1, jobName1) == (_numNodes * perNodeTaskLimitation)),
+        TestHelper.WAIT_DURATION));
 
     // schedule job2
     String jobName2 = "PriorityJob2";
@@ -140,18 +141,20 @@ public class TestTaskThrottling extends TaskTestBase {
     _driver.start(flow2);
     _driver.pollForJobState(flow2.getName(),
         TaskUtil.getNamespacedJobName(flow2.getName(), jobName2), TaskState.IN_PROGRESS);
-    // Wait for tasks to be picked up
-    Thread.sleep(1500);
-    Assert.assertEquals(countRunningPartition(flow2, jobName2), 0);
+
+    Assert.assertTrue(TestHelper.verify(() -> (countRunningPartition(flow2, jobName2) == 0),
+        TestHelper.WAIT_DURATION));
 
     // Increasing participants capacity
-    perNodeTaskLimitation = 2 * perNodeTaskLimitation;
-    setParticipantsCapacity(perNodeTaskLimitation);
+    setParticipantsCapacity(2 * perNodeTaskLimitation);
 
-    Thread.sleep(1500);
     // Additional capacity should all be used by job1
-    Assert.assertEquals(countRunningPartition(flow1, jobName1), _numNodes * perNodeTaskLimitation);
-    Assert.assertEquals(countRunningPartition(flow2, jobName2), 0);
+    Assert.assertTrue(TestHelper.verify(
+        () -> (countRunningPartition(flow1, jobName1) == (_numNodes * 2 * perNodeTaskLimitation)),
+        TestHelper.WAIT_DURATION));
+
+    Assert.assertTrue(TestHelper.verify(() -> (countRunningPartition(flow2, jobName2) == 0),
+        TestHelper.WAIT_DURATION));
 
     _driver.stop(flow1.getName());
     _driver.pollForWorkflowState(flow1.getName(), TaskState.STOPPED);

apache/helix

commit a1aba60a6701692f42ed650cae88827a26e93183

Adjust the auto rebalancer state assignment logic to reduce top state transition. (#986)
The old state assignment logic assign the states to selected nodes according to the priority of the current replica state that is on the instance. Moreover, the sorting algorithm is designed to prioritize both current topstate and current secondary states equally. The result is that we will have premature mastership handoff to a current seconardy state host before the real desired master host is ready.
For example,
1. The current states are: [N1:M, N2:S, N3,S]
2. The desired states are: [N4:M, N2:S, N1:S]
3. Due to the sorting logic based on current states, we will have a transient preference list ordered like: [N2, N1, N4]. In which case, the controller will assign master to N2 before N4 has a slave state replica.
4. When N4 finishes the Offline to Slave transition, the same sorting logic will sort the preference list to be: [N4, N2, N1]. Then we have another mastership handoff.
To be clear, we don't want step 3. But only the state transition in step 4.

In this PR, we refactor the sorting logic so that it will only move the master whenever the candidate has a "ready" state replica, in which case, only one mastership handoff happens.

helix-core/src/main/java/org/apache/helix/controller/rebalancer/AbstractRebalancer.java
@@ -305,45 +305,60 @@ public abstract class AbstractRebalancer<T extends BaseControllerDataProvider> i
     Set<String> liveAndEnabled = new HashSet<>(liveInstances);
     liveAndEnabled.removeAll(disabledInstancesForPartition);
 
-    for (String state : statesPriorityList) {
-      // Use the the specially ordered preferenceList for choosing instance for top state.
-      if (state.equals(statesPriorityList.get(0))) {
-        List<String> preferenceListForTopState = new ArrayList<>(preferenceList);
-        Collections.sort(preferenceListForTopState,
-            new TopStatePreferenceListComparator(currentStateMap, stateModelDef));
-        preferenceList = preferenceListForTopState;
-      }
+    // Sort the instances based on replicas' state priority in the current state
+    List<String> sortedPreferenceList = new ArrayList<>(preferenceList);
+    sortedPreferenceList.sort(new StatePriorityComparator(currentStateMap, stateModelDef));
 
+    // Assign the state to the instances that appear in the preference list.
+    for (String state : statesPriorityList) {
       int stateCount =
           getStateCount(state, stateModelDef, liveAndEnabled.size(), preferenceList.size());
       for (String instance : preferenceList) {
         if (stateCount <= 0) {
-          break;
+          break; // continue assigning for the next state
         }
-        if (!assigned.contains(instance) && liveAndEnabled.contains(instance)) {
-          if (HelixDefinedState.ERROR.toString().equals(currentStateMap.get(instance))) {
-            bestPossibleStateMap.put(instance, HelixDefinedState.ERROR.toString());
-          } else {
-            bestPossibleStateMap.put(instance, state);
-            stateCount--;
+        if (assigned.contains(instance) || !liveAndEnabled.contains(instance)) {
+          continue; // continue checking for the next available instance
+        }
+        String proposedInstance = instance;
+        // Additional check and alternate the assignment for reducing top state handoff.
+        if (state.equals(stateModelDef.getTopState()) && !stateModelDef.getSecondTopStates()
+            .contains(currentStateMap.getOrDefault(instance, stateModelDef.getInitialState()))) {
+          // If the desired state is the top state, but the instance cannot be transited to the
+          // top state in one hop, try to keep the top state on current host or a host with a closer
+          // state.
+          for (String currentStatePrioritizedInstance : sortedPreferenceList) {
+            if (!assigned.contains(currentStatePrioritizedInstance) && liveAndEnabled
+                .contains(currentStatePrioritizedInstance)) {
+              proposedInstance = currentStatePrioritizedInstance;
+              break;
+            }
           }
-          assigned.add(instance);
+          // Note that if all the current top state instances are not assignable, then we fallback
+          // to the default logic that assigning the state according to preference list order.
         }
+        // Assign the desired state to the proposed instance
+        if (HelixDefinedState.ERROR.toString().equals(currentStateMap.get(proposedInstance))) {
+          bestPossibleStateMap.put(proposedInstance, HelixDefinedState.ERROR.toString());
+        } else {
+          bestPossibleStateMap.put(proposedInstance, state);
+          stateCount--;
+        }
+        assigned.add(proposedInstance);
       }
     }
-
     return bestPossibleStateMap;
   }
 
   /**
-   * Sorter for nodes that sorts according to the CurrentState of the partition. There are only two priorities:
-   * (1) Top-state and second states have priority 0. (2) Other states(or no state) have priority 1.
+   * Sorter for nodes that sorts according to the CurrentState of the partition.
    */
-  protected static class TopStatePreferenceListComparator implements Comparator<String> {
-    protected final Map<String, String> _currentStateMap;
-    protected final StateModelDefinition _stateModelDef;
+  protected static class StatePriorityComparator implements Comparator<String> {
+    private final Map<String, String> _currentStateMap;
+    private final StateModelDefinition _stateModelDef;
 
-    public TopStatePreferenceListComparator(Map<String, String> currentStateMap, StateModelDefinition stateModelDef) {
+    public StatePriorityComparator(Map<String, String> currentStateMap,
+        StateModelDefinition stateModelDef) {
       _currentStateMap = currentStateMap;
       _stateModelDef = stateModelDef;
     }
@@ -352,21 +367,8 @@ public abstract class AbstractRebalancer<T extends BaseControllerDataProvider> i
     public int compare(String ins1, String ins2) {
       String state1 = _currentStateMap.get(ins1);
       String state2 = _currentStateMap.get(ins2);
-
-      String topState = _stateModelDef.getStatesPriorityList().get(0);
-      Set<String> preferredStates = new HashSet<String>(_stateModelDef.getSecondTopStates());
-      preferredStates.add(topState);
-
-      int p1 = 1;
-      int p2 = 1;
-
-      if (state1 != null && preferredStates.contains(state1)) {
-        p1 = 0;
-      }
-      if (state2 != null && preferredStates.contains(state2)) {
-        p2 = 0;
-      }
-
+      int p1 = state1 == null ? Integer.MAX_VALUE : _stateModelDef.getStatePriorityMap().get(state1);
+      int p2 = state2 == null ? Integer.MAX_VALUE : _stateModelDef.getStatePriorityMap().get(state2);
       return p1 - p2;
     }
   }

helix-core/src/test/java/org/apache/helix/controller/rebalancer/TestAbstractRebalancer.java
@@ -49,12 +49,12 @@ public class TestAbstractRebalancer {
           .setCurrentState("test", partition, instance, currentStateMap.get(instance));
     }
     Map<String, String> bestPossibleMap = rebalancer
-        .computeBestPossibleStateForPartition(new HashSet<String>(liveInstances),
+        .computeBestPossibleStateForPartition(new HashSet<>(liveInstances),
             BuiltInStateModelDefinitions.valueOf(stateModelName).getStateModelDefinition(),
-            preferenceList, currentStateOutput, new HashSet<String>(disabledInstancesForPartition),
+            preferenceList, currentStateOutput, new HashSet<>(disabledInstancesForPartition),
             new IdealState("test"), new ClusterConfig("TestCluster"), partition);
 
-    Assert.assertTrue(bestPossibleMap.equals(expectedBestPossibleMap));
+    Assert.assertEquals(bestPossibleMap, expectedBestPossibleMap);
   }
 
   @DataProvider(name = "TestComputeBestPossibleStateInput")

helix-core/src/main/java/org/apache/helix/controller/rebalancer/DelayedAutoRebalancer.java
@@ -357,7 +357,7 @@ public class DelayedAutoRebalancer extends AbstractRebalancer<ResourceController
           .subList(0, subListSize <= instanceToAdd.size() ? subListSize : instanceToAdd.size()));
     }
 
-    // Make all intial state instance not in preference list to be dropped.
+    // Make all initial state instance not in preference list to be dropped.
     Map<String, String> currentMapWithPreferenceList = new HashMap<>(currentStateMap);
     currentMapWithPreferenceList.keySet().retainAll(preferenceList);
 

commit 29ce245ec9bedb0064ff1d2b9e94ddcfd3afaa35

Fix the issue that the instance may not be assigned a replica as expected.
This is to fix a regression which was introduced by PR #986.
The PR tried to prioritize the preference list to avoid unnecessary top state transition. However, there was a bug in the prioritizing logic and if one participant is skipped due to low priority, it won't be picked up again during the cauculating. As a result, this participant won't be assigned with any replica even it is originally in the preference list.
This fix will ensure the skipped participant being checked again until it gets the assignment.

helix-core/src/main/java/org/apache/helix/controller/rebalancer/AbstractRebalancer.java
@@ -24,6 +24,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -364,17 +365,20 @@ public abstract class AbstractRebalancer<T extends BaseControllerDataProvider> i
     liveAndEnabled.removeAll(disabledInstancesForPartition);
 
     // Sort the instances based on replicas' state priority in the current state
-    List<String> sortedPreferenceList = new ArrayList<>(preferenceList);
-    sortedPreferenceList.sort(new StatePriorityComparator(currentStateMap, stateModelDef));
+    List<String> currentStatePrioritizedList = new ArrayList<>(preferenceList);
+    currentStatePrioritizedList.retainAll(liveAndEnabled);
+    currentStatePrioritizedList.sort(new StatePriorityComparator(currentStateMap, stateModelDef));
+    Iterator<String> currentStatePrioritizedInstanceIter = currentStatePrioritizedList.iterator();
 
     // Assign the state to the instances that appear in the preference list.
     for (String state : statesPriorityList) {
       int stateCount =
           getStateCount(state, stateModelDef, liveAndEnabled.size(), preferenceList.size());
-      for (String instance : preferenceList) {
+      for (int i = 0; i < preferenceList.size(); i++) {
         if (stateCount <= 0) {
           break; // continue assigning for the next state
         }
+        String instance = preferenceList.get(i);
         if (assigned.contains(instance) || !liveAndEnabled.contains(instance)) {
           continue; // continue checking for the next available instance
         }
@@ -385,10 +389,19 @@ public abstract class AbstractRebalancer<T extends BaseControllerDataProvider> i
           // If the desired state is the top state, but the instance cannot be transited to the
           // top state in one hop, try to keep the top state on current host or a host with a closer
           // state.
-          for (String currentStatePrioritizedInstance : sortedPreferenceList) {
-            if (!assigned.contains(currentStatePrioritizedInstance) && liveAndEnabled
-                .contains(currentStatePrioritizedInstance)) {
+          while (currentStatePrioritizedInstanceIter.hasNext()) {
+            // Note that it is safe to check the prioritized instance items only once here.
+            // Since the only possible condition when we don't use an instance in this list is that
+            // it has been assigned with some state. And this is not revertable in this method. So
+            // checking it one more time later will only waste time.
+            String currentStatePrioritizedInstance = currentStatePrioritizedInstanceIter.next();
+            if (!assigned.contains(currentStatePrioritizedInstance)) {
               proposedInstance = currentStatePrioritizedInstance;
+              // If we find a different instance for the partition placement, then we need to check
+              // the same instance again or it will not be assigned with any partitions.
+              if (!proposedInstance.equals(instance)) {
+                i--;
+              }
               break;
             }
           }

helix-core/src/main/java/org/apache/helix/model/BuiltInStateModelDefinitions.java
@@ -27,6 +27,7 @@ public enum BuiltInStateModelDefinitions {
   LeaderStandby(new LeaderStandbySMD()),
   StorageSchemata(new StorageSchemataSMD()),
   OnlineOffline(new OnlineOfflineSMD()),
+  OnlineOfflineWithBootstrap(OnlineOfflineWithBootstrapSMD.build()),
   ScheduledTask(new ScheduledTaskSMD()),
   Task(new TaskSMD());
 

helix-core/src/main/java/org/apache/helix/model/OnlineOfflineWithBootstrapSMD.java
@@ -1,0 +1,74 @@
+package org.apache.helix.model;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.helix.HelixDefinedState;
+import org.apache.helix.zookeeper.datamodel.ZNRecord;
+
+/**
+ * Helix built-in state model definition based on Online-Offline but with the additional bootstrap
+ * state.
+ */
+public final class OnlineOfflineWithBootstrapSMD extends StateModelDefinition {
+  public static final String name = "OnlineOfflineWithBootstrap";
+
+  /**
+   * Instantiate from a pre-populated record
+   *
+   * @param record ZNRecord representing a state model definition
+   */
+  private OnlineOfflineWithBootstrapSMD(ZNRecord record) {
+    super(record);
+  }
+
+  public enum States {
+    ONLINE, BOOTSTRAP, OFFLINE
+  }
+
+  /**
+   * Build OnlineOfflineWithBootstrap state model definition
+   *
+   * @return
+   */
+  public static OnlineOfflineWithBootstrapSMD build() {
+    Builder builder = new Builder(name);
+    // init state
+    builder.initialState(States.OFFLINE.name());
+
+    // add states
+    builder.addState(States.ONLINE.name(), 0);
+    builder.addState(States.BOOTSTRAP.name(), 1);
+    builder.addState(States.OFFLINE.name(), 2);
+    for (HelixDefinedState state : HelixDefinedState.values()) {
+    builder.addState(state.name());
+  }
+
+    // add transitions
+    builder.addTransition(States.ONLINE.name(), States.OFFLINE.name(), 0);
+    builder.addTransition(States.OFFLINE.name(), States.BOOTSTRAP.name(), 1);
+    builder.addTransition(States.BOOTSTRAP.name(), States.ONLINE.name(), 2);
+    builder.addTransition(States.OFFLINE.name(), HelixDefinedState.DROPPED.name());
+
+    // bounds
+    builder.dynamicUpperBound(States.ONLINE.name(), "R");
+
+    return new OnlineOfflineWithBootstrapSMD(builder.build().getRecord());
+  }
+}

helix-core/src/test/java/org/apache/helix/controller/rebalancer/TestAbstractRebalancer.java
@@ -56,11 +56,11 @@ public class TestAbstractRebalancer {
             new IdealState("test"), new ClusterConfig("TestCluster"), partition,
             MonitoredAbnormalResolver.DUMMY_STATE_RESOLVER);
 
-    Assert.assertEquals(bestPossibleMap, expectedBestPossibleMap);
+    Assert.assertTrue(bestPossibleMap.equals(expectedBestPossibleMap));
   }
 
   @DataProvider(name = "TestComputeBestPossibleStateInput")
-  public Object[][] loadTestComputeBestPossibleStateInput() {
+  public Object[][] loadTestComputeBestPossiblqeStateInput() {
     final String[] params = {"comment", "stateModel", "liveInstances", "preferenceList", "currentStateMap",
         "disabledInstancesForPartition", "expectedBestPossibleStateMap"};
     return TestInputLoader.loadTestInputs("TestAbstractRebalancer.ComputeBestPossibleState.json", params);
ecc33f6d26b323cec9a2b3450ab7cfc739c7f0a7
Kai Sun <ksun@ksun-mn1.linkedin.biz>
2020-08-16 00:43:03 -0700

    fix TestZkCallbackHandlerLeak.java timeout. #1284

--- helix-core/src/test/java/org/apache/helix/integration/TestZkCallbackHandlerLeak.java
+++ helix-core/src/test/java/org/apache/helix/integration/TestZkCallbackHandlerLeak.java
@@ -200,7 +200,7 @@ public class TestZkCallbackHandlerLeak extends ZkUnitTestBase {
     System.out.println("END " + clusterName + " at " + new Date(System.currentTimeMillis()));
   }
 
-  @Test
+  @Test()
   public void testCbHandlerLeakOnControllerSessionExpiry() throws Exception {
     // Logger.getRootLogger().setLevel(Level.INFO);
     String className = TestHelper.getTestClassName();
@@ -412,7 +412,7 @@ public class TestZkCallbackHandlerLeak extends ZkUnitTestBase {
     System.out.println("END " + clusterName + " at " + new Date(System.currentTimeMillis()));
   }
 
-  @Test
+  @Test(timeOut = 1800 * 1000)
   public void testCurrentStatePathLeakingByAsycRemoval() throws Exception {
     String className = TestHelper.getTestClassName();
     String methodName = TestHelper.getTestMethodName();
@@ -655,7 +655,7 @@ public class TestZkCallbackHandlerLeak extends ZkUnitTestBase {
     // the same time as participant.
     // Currently there are many places to register watch in Zookeeper over the evolution of Helix
     // and ZkClient. We plan for further simplify the logic of watch installation next.
-    boolean result = TestHelper.verify(()-> {
+    boolean result = TestHelper.verify(()->
       Map<String, List<String>> wPaths = ZkTestHelper.getZkWatch(participantToExpire.getZkClient());
       return wPaths.get("existWatches").size() == 1;
     }, TestHelper.WAIT_DURATION);

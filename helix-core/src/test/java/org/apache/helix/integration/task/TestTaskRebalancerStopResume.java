c6dc888e8b800d9b36ac37776ddd0d38f65a5049
Kai Sun <ksun@ksun-mn1.linkedin.biz>
2020-09-22 22:33:27 -0700

    try fix TestTaskRebalancerStopResume.stopAndDeleteQueue #1366, #1394

--- helix-core/src/test/java/org/apache/helix/integration/task/TestTaskRebalancerStopResume.java
+++ helix-core/src/test/java/org/apache/helix/integration/task/TestTaskRebalancerStopResume.java
@@ -418,19 +418,28 @@ public class TestTaskRebalancerStopResume extends TaskTestBase {
     _driver.createQueue(qCfg);
 
     // Enqueue 2 jobs
+    List<String> jobNames = new ArrayList<>();
+    List<JobConfig.Builder> jobBuilders = new ArrayList<>();
+
     Set<String> master = Sets.newHashSet("MASTER");
     JobConfig.Builder job1 = new JobConfig.Builder().setCommand(MockTask.TASK_COMMAND)
         .setTargetResource(WorkflowGenerator.DEFAULT_TGT_DB).setTargetPartitionStates(master);
     String job1Name = "masterJob";
     LOG.info("Enqueuing job1: " + job1Name);
-    _driver.enqueueJob(queueName, job1Name, job1);
+    jobNames.add(job1Name);
+    jobBuilders.add(job1);
+    // _driver.enqueueJob(queueName, job1Name, job1);
 
     Set<String> slave = Sets.newHashSet("SLAVE");
     JobConfig.Builder job2 = new JobConfig.Builder().setCommand(MockTask.TASK_COMMAND)
         .setTargetResource(WorkflowGenerator.DEFAULT_TGT_DB).setTargetPartitionStates(slave);
     String job2Name = "slaveJob";
     LOG.info("Enqueuing job2: " + job2Name);
-    _driver.enqueueJob(queueName, job2Name, job2);
+    jobNames.add(job2Name);
+    jobBuilders.add(job2);
+    //_driver.enqueueJob(queueName, job2Name, job2);
+
+    _driver.enqueueJobs(queueName, jobNames, jobBuilders);
 
     String namespacedJob1 = String.format("%s_%s", queueName, job1Name);
     _driver.pollForJobState(queueName, namespacedJob1, TaskState.COMPLETED);

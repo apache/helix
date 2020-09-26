5eb79cea5134070568f1d949c52c8ce5c2c4846a
Kai Sun <ksun@ksun-mn1.linkedin.biz>
2020-09-26 14:49:04 -0700

    fix #1418 TestSemiAutoRebalance.beforeClass

--- helix-core/src/test/java/org/apache/helix/integration/rebalancer/TestSemiAutoRebalance.java
+++ helix-core/src/test/java/org/apache/helix/integration/rebalancer/TestSemiAutoRebalance.java
@@ -20,8 +20,10 @@ package org.apache.helix.integration.rebalancer;
  */
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 

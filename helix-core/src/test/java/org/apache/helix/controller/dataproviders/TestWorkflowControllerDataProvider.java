ec214b2e809c6895382019243007e0167680502b
Neal Sun <nealsun.0428@gmail.com>
2020-11-04 09:44:08 -0800

    Fix ondemand rebalance flooding and log flooding caused by dangling jobs (#1508)
    This commit changes runtime dag refresh logic to eliminate ondemand rebalance
    flooding caused by dangling jobs. This commit also changes log level to get rid of 
    log flooding caused by missing target resources.

--- helix-core/src/test/java/org/apache/helix/controller/dataproviders/TestWorkflowControllerDataProvider.java
+++ helix-core/src/test/java/org/apache/helix/controller/dataproviders/TestWorkflowControllerDataProvider.java
@@ -19,10 +19,20 @@ package org.apache.helix.controller.dataproviders;
  * under the License.
  */
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+import org.apache.helix.AccessOption;
+import org.apache.helix.PropertyKey;
 import org.apache.helix.TestHelper;
 import org.apache.helix.integration.task.TaskTestBase;
+import org.apache.helix.integration.task.TaskTestUtil;
 import org.apache.helix.integration.task.WorkflowGenerator;
 import org.apache.helix.task.JobConfig;
+import org.apache.helix.task.JobQueue;
+import org.apache.helix.task.RuntimeJobDag;
+import org.apache.helix.task.TaskUtil;
 import org.apache.helix.task.Workflow;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -69,5 +79,61 @@ public class TestWorkflowControllerDataProvider extends TaskTestBase {
     }, TestHelper.WAIT_DURATION);
     Assert.assertTrue(expectedValuesAchieved);
 
+  }
+
+  @Test (dependsOnMethods = "testResourceConfigRefresh")
+  public void testRuntimeDagRefresh() throws Exception {
+    String jobQueueName = TestHelper.getTestMethodName();
+    JobQueue.Builder builder = TaskTestUtil.buildJobQueue(jobQueueName);
+    JobConfig.Builder jobBuilder = JobConfig.Builder.fromMap(WorkflowGenerator.DEFAULT_JOB_CONFIG);
+    builder.enqueueJob(WorkflowGenerator.JOB_NAME_1, jobBuilder);
+    String jobName1 = TaskUtil.getNamespacedJobName(jobQueueName, WorkflowGenerator.JOB_NAME_1);
+    _driver.start(builder.build());
+
+    WorkflowControllerDataProvider cache =
+        new WorkflowControllerDataProvider("CLUSTER_" + TestHelper.getTestClassName());
+
+    Assert.assertTrue(TestHelper.verify(() -> {
+      cache.requireFullRefresh();
+      cache.refresh(_manager.getHelixDataAccessor());
+      return cache.getTaskDataCache().getJobConfig(jobName1) != null;
+    }, TestHelper.WAIT_DURATION));
+    RuntimeJobDag runtimeJobDag = cache.getTaskDataCache().getRuntimeJobDag(jobQueueName);
+    Assert.assertEquals(Collections.singleton(jobName1), runtimeJobDag.getAllNodes());
+
+    // Mimic job running
+    runtimeJobDag.getNextJob();
+
+    // Add job config without adding it to the dag
+    String danglingJobName = TaskUtil.getNamespacedJobName(jobQueueName, "DanglingJob");
+    JobConfig danglingJobConfig = new JobConfig(danglingJobName, jobBuilder.build());
+    PropertyKey.Builder keyBuilder = _manager.getHelixDataAccessor().keyBuilder();
+    _baseAccessor
+        .create(keyBuilder.resourceConfig(danglingJobName).getPath(), danglingJobConfig.getRecord(),
+            AccessOption.PERSISTENT);
+
+    // There shouldn't be a refresh to runtime dag. The dag should only contain one job and the job is inflight.
+    Assert.assertTrue(TestHelper.verify(() -> {
+      cache.requireFullRefresh();
+      cache.refresh(_manager.getHelixDataAccessor());
+      return cache.getTaskDataCache().getJobConfig(danglingJobName) != null;
+    }, TestHelper.WAIT_DURATION));
+    runtimeJobDag = cache.getTaskDataCache().getRuntimeJobDag(jobQueueName);
+    Assert.assertEquals(Collections.singleton(jobName1), runtimeJobDag.getAllNodes());
+    Assert.assertEquals(Collections.singleton(jobName1), runtimeJobDag.getInflightJobList());
+
+    _driver.enqueueJob(jobQueueName, WorkflowGenerator.JOB_NAME_2, jobBuilder);
+    String jobName2 = TaskUtil.getNamespacedJobName(jobQueueName, WorkflowGenerator.JOB_NAME_2);
+
+    // There should be a refresh to runtime dag.
+    Assert.assertTrue(TestHelper.verify(() -> {
+      cache.requireFullRefresh();
+      cache.refresh(_manager.getHelixDataAccessor());
+      return cache.getTaskDataCache().getJobConfig(jobName2) != null;
+    }, TestHelper.WAIT_DURATION));
+    runtimeJobDag = cache.getTaskDataCache().getRuntimeJobDag(jobQueueName);
+    Assert.assertEquals(new HashSet<>(Arrays.asList(jobName1, jobName2)),
+        runtimeJobDag.getAllNodes());
+    Assert.assertEquals(Collections.emptyList(), runtimeJobDag.getInflightJobList());
   }
 }

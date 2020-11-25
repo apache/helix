893c8965de8d3485fe63f11b00ad6ebf2e4137b7
Xiaoyuan Lu <xyuanlu@gmail.com>
2020-11-24 17:15:28 -0800

    Revert "Move subscribeForChange out of critical section (#1540)"
    This reverts commit dac85828f59ca4ef1fee97bce5ba8d380b98b659.

--- helix-core/src/main/java/org/apache/helix/manager/zk/CallbackHandler.java
+++ helix-core/src/main/java/org/apache/helix/manager/zk/CallbackHandler.java
@@ -340,28 +340,26 @@ public class CallbackHandler implements IZkChildListener, IZkDataListener {
   public void invoke(NotificationContext changeContext) throws Exception {
     Type type = changeContext.getType();
     long start = System.currentTimeMillis();
-    if (logger.isInfoEnabled()) {
-      logger.info("{} START: CallbackHandler {}, INVOKE {} listener: {} type: {}",
-          Thread.currentThread().getId(), _uid, _path, _listener, type);
-    }
 
-    synchronized (this) {
+    // This allows the listener to work with one change at a time
+    synchronized (_manager) {
+      if (logger.isInfoEnabled()) {
+        logger
+            .info("{} START: CallbackHandler {}, INVOKE {} listener: {} type: {}", Thread.currentThread().getId(),
+                _uid, _path, _listener, type);
+      }
+
       if (!_expectTypes.contains(type)) {
         logger.warn("Callback handler {} received event in wrong order. Listener: {}, path: {}, "
             + "expected types: {}, but was {}", _uid, _listener, _path, _expectTypes, type);
         return;
+
       }
       _expectTypes = nextNotificationType.get(type);
 
       if (type == Type.INIT || type == Type.FINALIZE || changeContext.getIsChildChange()) {
         subscribeForChanges(changeContext.getType(), _path, _watchChild);
       }
-    }
-
-    // This allows the Helix Manager to work with one change at a time
-    // TODO: Maybe we don't need to sync on _manager for all types of listener. PCould be a
-    // potential improvement candidate.
-    synchronized (_manager) {
       if (_changeType == IDEAL_STATE) {
         IdealStateChangeListener idealStateChangeListener = (IdealStateChangeListener) _listener;
         List<IdealState> idealStates = preFetch(_propertyKey);

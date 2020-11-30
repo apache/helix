09fc0c6252221c6f64cad9ca58f0b1a056ef04c7
Neal Sun <nesun@nesun-mn1.linkedin.biz>
2020-11-30 10:53:17 -0800

    HelixStateTransitionhandler change

--- helix-core/src/main/java/org/apache/helix/messaging/handling/HelixStateTransitionHandler.java
+++ helix-core/src/main/java/org/apache/helix/messaging/handling/HelixStateTransitionHandler.java
@@ -45,6 +45,7 @@ import org.apache.helix.participant.statemachine.StateModel;
 import org.apache.helix.participant.statemachine.StateModelFactory;
 import org.apache.helix.participant.statemachine.StateModelParser;
 import org.apache.helix.participant.statemachine.StateTransitionError;
+import org.apache.helix.task.TaskStateModel;
 import org.apache.helix.util.StatusUpdateUtil;
 import org.apache.helix.zookeeper.datamodel.ZNRecord;
 import org.apache.helix.zookeeper.datamodel.ZNRecordBucketizer;
@@ -86,6 +87,7 @@ public class HelixStateTransitionHandler extends MessageHandler {
   private final CurrentState _currentStateDelta;
   private final HelixManager _manager;
   private final StateModelFactory<? extends StateModel> _stateModelFactory;
+  private final boolean _isTaskMessage;
   volatile boolean _isTimeout = false;
 
   public HelixStateTransitionHandler(StateModelFactory<? extends StateModel> stateModelFactory,
@@ -98,6 +100,7 @@ public class HelixStateTransitionHandler extends MessageHandler {
     _currentStateDelta = currentStateDelta;
     _manager = _notificationContext.getManager();
     _stateModelFactory = stateModelFactory;
+    _isTaskMessage = stateModel instanceof TaskStateModel;
   }
 
   void preHandleMessage() throws Exception {
@@ -137,8 +140,10 @@ public class HelixStateTransitionHandler extends MessageHandler {
       String sessionId = _message.getTgtSessionId();
       String resource = _message.getResourceName();
       ZNRecordBucketizer bucketizer = new ZNRecordBucketizer(_message.getBucketSize());
-      PropertyKey key = accessor.keyBuilder().currentState(instance, sessionId, resource,
-          bucketizer.getBucketName(partitionName));
+      PropertyKey key = _stateModel instanceof TaskStateModel ? accessor.keyBuilder()
+          .taskCurrentState(instance, sessionId, resource, bucketizer.getBucketName(partitionName))
+          : accessor.keyBuilder()
+              .currentState(instance, sessionId, resource, bucketizer.getBucketName(partitionName));
       ZNRecord rec = new ZNRecord(resource);
       Map<String, String> map = new TreeMap<String, String>();
       map.put(CurrentState.CurrentStateProperty.REQUESTED_STATE.name(), null);
@@ -438,9 +443,10 @@ public class HelixStateTransitionHandler extends MessageHandler {
           disablePartition();
         }
 
-        if (!accessor.updateProperty(
-            keyBuilder.currentState(instanceName, _message.getTgtSessionId(), resourceName),
-            currentStateDelta)) {
+        PropertyKey currentStateKey = _stateModel instanceof TaskStateModel ? keyBuilder
+            .taskCurrentState(instanceName, _message.getTgtSessionId(), resourceName)
+            : keyBuilder.currentState(instanceName, _message.getTgtSessionId(), resourceName);
+        if (!accessor.updateProperty(currentStateKey, currentStateDelta)) {
           logger.error("Fails to persist ERROR current state to ZK for resource " + resourceName
               + " partition: " + partition);
         }

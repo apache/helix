5e13cad9203f0dd6eb3640b22714789d32f414ea
Kai Sun <ksun@ksun-mn1.linkedin.biz>
2020-12-08 21:29:26 -0800

    trigger 9

--- helix-core/src/test/java/org/apache/helix/ZkTestHelper.java
+++ helix-core/src/test/java/org/apache/helix/ZkTestHelper.java
@@ -17,7 +17,6 @@ package org.apache.helix;
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.  
-
  */
 
 import java.io.BufferedReader;

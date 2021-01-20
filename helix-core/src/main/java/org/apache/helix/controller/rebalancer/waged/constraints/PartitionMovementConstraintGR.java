2ed9e618101c157b92afaa3f395b0c3ea1ea0dbc
Neal Sun <nesun@nesun-mn1.linkedin.biz>
2021-01-20 14:13:28 -0800

    Add comments

--- helix-core/src/main/java/org/apache/helix/controller/rebalancer/waged/constraints/PartitionMovementConstraintGR.java
+++ helix-core/src/main/java/org/apache/helix/controller/rebalancer/waged/constraints/PartitionMovementConstraintGR.java
@@ -5,7 +5,14 @@ import org.apache.helix.controller.rebalancer.waged.model.AssignableReplica;
 import org.apache.helix.controller.rebalancer.waged.model.ClusterContext;
 import org.apache.helix.controller.rebalancer.waged.model.ClusterModelProvider;
 
-
+/**
+ * Evaluate the proposed assignment according to the potential partition movements cost.
+ * The cost is evaluated based on the difference between the old assignment and the new assignment.
+ * This constraint is for Global Rebalance only, and the calculation is based on the previous
+ * baseline assignment, which is calculated by the previous global rebalance.
+ * Any change to these two assignments will increase the partition movements cost, so that the
+ * evaluated score will become lower.
+ */
 public class PartitionMovementConstraintGR extends PartitionMovementConstraint {
   @Override
   protected double getAssignmentScore(AssignableNode node, AssignableReplica replica,
--- helix-core/src/main/java/org/apache/helix/controller/rebalancer/waged/constraints/PartitionMovementConstraintPR.java
+++ helix-core/src/main/java/org/apache/helix/controller/rebalancer/waged/constraints/PartitionMovementConstraintPR.java
@@ -5,7 +5,17 @@ import org.apache.helix.controller.rebalancer.waged.model.AssignableReplica;
 import org.apache.helix.controller.rebalancer.waged.model.ClusterContext;
 import org.apache.helix.controller.rebalancer.waged.model.ClusterModelProvider;
 
-
+/**
+ * Evaluate the proposed assignment according to the potential partition movements cost.
+ * The cost is evaluated based on the difference between the old assignment and the new assignment.
+ * This constraint is for Partial Rebalance only, and the calculation is based on two previous
+ * assignments:
+ * - Baseline assignment that is calculated regardless of the node state (online/offline), which is
+ * the result of Global Rebalance;
+ * - Previous Best Possible assignment, which is the result of the previous Partial Rebalance
+ * Any change to these two assignments will increase the partition movements cost, so that the
+ * evaluated score will become lower.
+ */
 public class PartitionMovementConstraintPR extends PartitionMovementConstraint {
   @Override
   protected double getAssignmentScore(AssignableNode node, AssignableReplica replica,

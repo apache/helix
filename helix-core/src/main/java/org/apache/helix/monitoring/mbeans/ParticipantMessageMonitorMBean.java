0a5682fdd27832e8039f7134a53d817c15b7b452
Meng Zhang <mnzhang@linkedin.com>
2021-03-30 15:45:53 -0700

    Revert "Change participant message monitor to use dynamic metric (#1685)" (#1693)
    This reverts commit d5f5273d483ba54c51c79d497dead190cf758bb6.

--- helix-core/src/main/java/org/apache/helix/monitoring/mbeans/ParticipantMessageMonitorMBean.java
+++ helix-core/src/main/java/org/apache/helix/monitoring/mbeans/ParticipantMessageMonitorMBean.java
@@ -8,6 +8,7 @@ package org.apache.helix.monitoring.mbeans;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
@@ -20,7 +21,7 @@ package org.apache.helix.monitoring.mbeans;
 
 import org.apache.helix.monitoring.SensorNameProvider;
 
-@Deprecated
+
 public interface ParticipantMessageMonitorMBean extends SensorNameProvider {
   public long getReceivedMessages();
   public long getDiscardedMessages();

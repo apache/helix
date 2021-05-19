f980f4624166a8c82f10e36d9fe4ecb056318ece
Huizhi Lu <5187721+huizhilu@users.noreply.github.com>
2021-05-19 15:45:11 -0700

    Add java api for enable/disable cluster pause mode (#1740)
    Cluster pause mode feature is going to be added. This commit adds java api to set cluster pause mode: void setClusterManagementMode(ClusterManagementModeRequest request);

--- helix-core/src/main/java/org/apache/helix/model/PauseSignal.java
+++ helix-core/src/main/java/org/apache/helix/model/PauseSignal.java
@@ -19,6 +19,8 @@ package org.apache.helix.model;
  * under the License.
  */
 
+import java.time.Instant;
+
 import org.apache.helix.HelixProperty;
 import org.apache.helix.zookeeper.datamodel.ZNRecord;
 
@@ -26,9 +28,18 @@ import org.apache.helix.zookeeper.datamodel.ZNRecord;
  * Represent a pause in the cluster
  */
 public class PauseSignal extends HelixProperty {
+  private static final String DEFAULT_PAUSE_ID = "pause";
 
   public enum PauseSignalProperty {
-    REASON
+    REASON,
+    CLUSTER_PAUSE,
+    FROM_HOST,
+    CANCEL_PENDING_ST,
+    TRIGGER_TIME
+  }
+
+  public PauseSignal() {
+    this(DEFAULT_PAUSE_ID);
   }
 
   /**
@@ -62,5 +73,34 @@ public class PauseSignal extends HelixProperty {
   @Override
   public boolean isValid() {
     return true;
+  }
+
+  public void setClusterPause(boolean pause) {
+    _record.setBooleanField(PauseSignalProperty.CLUSTER_PAUSE.name(), pause);
+  }
+
+  public boolean isClusterPause() {
+    return _record.getBooleanField(PauseSignalProperty.CLUSTER_PAUSE.name(), false);
+  }
+
+  public void setFromHost(String host) {
+    _record.setSimpleField(PauseSignalProperty.FROM_HOST.name(), host);
+  }
+
+  public String getFromHost() {
+    return _record.getSimpleField(PauseSignalProperty.FROM_HOST.name());
+  }
+
+  public void setCancelPendingST(boolean cancel) {
+    _record.setBooleanField(PauseSignalProperty.CANCEL_PENDING_ST.name(), cancel);
+  }
+
+  public boolean getCancelPendingST() {
+    return _record.getBooleanField(PauseSignalProperty.CANCEL_PENDING_ST.name(), false);
+  }
+
+  public void setTriggerTime(long time) {
+    _record.setSimpleField(PauseSignalProperty.TRIGGER_TIME.name(),
+        Instant.ofEpochMilli(time).toString());
   }
 }

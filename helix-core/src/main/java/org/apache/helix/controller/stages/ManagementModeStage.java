e96fab51e61e4274bac3e03381dca9c4c9ac2487
Huizhi Lu <5187721+pkuwm@users.noreply.github.com>
2021-08-24 10:28:18 -0700

    Separate duplicate check logic into private method

--- helix-core/src/main/java/org/apache/helix/controller/stages/ManagementModeStage.java
+++ helix-core/src/main/java/org/apache/helix/controller/stages/ManagementModeStage.java
@@ -135,31 +135,18 @@ public class ManagementModeStage extends AbstractBaseStage {
 
   private void recordClusterStatus(ClusterManagementMode mode, PauseSignal pauseSignal,
       String controllerName, HelixDataAccessor accessor) {
-    // update cluster status
+    // Read cluster status from metadata store
     PropertyKey statusPropertyKey = accessor.keyBuilder().clusterStatus();
     ClusterStatus clusterStatus = accessor.getProperty(statusPropertyKey);
     if (clusterStatus == null) {
       clusterStatus = new ClusterStatus();
     }
 
-    ClusterManagementMode.Type recordedType = clusterStatus.getManagementMode();
-    ClusterManagementMode.Status recordedStatus = clusterStatus.getManagementModeStatus();
-
-    if (mode.getMode().equals(recordedType) && mode.getStatus().equals(recordedStatus)) {
+    if (mode.getMode().equals(clusterStatus.getManagementMode())
+        && mode.getStatus().equals(clusterStatus.getManagementModeStatus())) {
       // No need to update status and avoid duplicates when it's the same as metadata store
       LOG.debug("Skip recording duplicate status mode={}, status={}", mode.getMode(),
           mode.getStatus());
-      return;
-    }
-
-    // If there is any pending message sent by users, status could be computed as in progress.
-    // Skip recording status change to avoid confusion after cluster is already fully frozen.
-    if (ClusterManagementMode.Type.CLUSTER_FREEZE.equals(recordedType)
-        && ClusterManagementMode.Status.COMPLETED.equals(recordedStatus)
-        && ClusterManagementMode.Type.CLUSTER_FREEZE.equals(mode.getMode())
-        && ClusterManagementMode.Status.IN_PROGRESS.equals(mode.getStatus())) {
-      LOG.info("Skip recording status mode={}, status={}, because cluster is fully frozen",
-          mode.getMode(), mode.getStatus());
       return;
     }
 

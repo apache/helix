d87399dca0052bb4f31754dd9a51e9539d57b1c7
Xiaoyuan Lu <xyuanlu@gmail.com>
2021-11-02 14:43:50 -0700

    2

--- helix-rest/src/main/java/org/apache/helix/rest/common/datamodel/RestSnapShot.java
+++ helix-rest/src/main/java/org/apache/helix/rest/common/datamodel/RestSnapShot.java
@@ -1,27 +1,13 @@
 package org.apache.helix.rest.common.datamodel;
 
-import java.util.List;
-
-import org.apache.helix.HelixProperty;
-import org.apache.helix.PropertyKey;
-import org.apache.helix.rest.common.HelixDataAccessorWrapper;
-
 
 public class RestSnapShot {
-  private HelixDataAccessorWrapper _helixDataAccessorWrapper;
+  /* An Snapshot object should contain all the Helix related info that an implementation of
+  * OperationAbstractClass would need.
+  */
 
-  public RestSnapShot(HelixDataAccessorWrapper wrapper) {
-    _helixDataAccessorWrapper = wrapper;
-  }
+  // TODO: Define a Enum class for all Helix info types like ExternalView, InstanceConfig etc. An
+  // implementation of OperationAbstractClass will need to define what are the types needed.
 
-  public <T extends HelixProperty> T getProperty(PropertyKey key) {
-    return _helixDataAccessorWrapper.getProperty(key);
-  }
-  public List<String> getChildNames(PropertyKey key) {
-    return _helixDataAccessorWrapper.getChildNames(key);
-  }
-
-  public PropertyKey.Builder keyBuilder() {
-    return _helixDataAccessorWrapper.keyBuilder();
-  }
+  // TODO: Support hierarchical Snapshot type for other services besides cluster MaintenanceService.
 }

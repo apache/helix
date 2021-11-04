3d244c33e702466c6b61c7d5117610ca6e99848c
Xiaoyuan Lu <xyuanlu@gmail.com>
2021-11-04 14:55:20 -0700

    address comments

--- helix-rest/src/main/java/org/apache/helix/rest/clusterMaintenanceService/MaintenanceManagementInstanceInfo.java
+++ helix-rest/src/main/java/org/apache/helix/rest/clusterMaintenanceService/MaintenanceManagementInstanceInfo.java
@@ -25,44 +25,57 @@ import java.util.List;
 
 public class MaintenanceManagementInstanceInfo {
 
-  public enum Status {
-    SUCCESS,
-    FAILURE
+  public enum OperationalStatus {
+    SUCCESS, FAILURE
   }
-  private List<Object> operationResult;
-  private Status status;
+
+  private String operationResult;
+  private OperationalStatus status;
   private List<String> messages;
 
-  public MaintenanceManagementInstanceInfo(Status status) {
+  public MaintenanceManagementInstanceInfo(OperationalStatus status) {
     this.status = status;
-    this.operationResult = new ArrayList<>();
     this.messages = new ArrayList<>();
   }
 
-  public MaintenanceManagementInstanceInfo(Status status, List<Object> newoperationResult) {
+  public MaintenanceManagementInstanceInfo(OperationalStatus status, String newoperationResult) {
     this.status = status;
-    this.operationResult = new ArrayList<>(newoperationResult);
+    this.operationResult = newoperationResult;
     this.messages = new ArrayList<>();
   }
 
-  public List<String> getMessages(){
+  public List<String> getMessages() {
     return messages;
   }
 
-  public List<Object> getOperationResult(){
+  public String getOperationResult() {
     return operationResult;
+  }
+
+  public boolean hasOperationResult() {
+    return !operationResult.isEmpty();
+  }
+
+  public void setOperationResult(String result) {
+    operationResult = result;
   }
 
   public void addFailureMessage(List<String> msg) {
     messages.addAll(msg);
   }
 
-  public boolean isSuccessful() {return status.equals(Status.SUCCESS);}
-
-  public void mergeResult(MaintenanceManagementInstanceInfo info) {
-    operationResult.addAll(info.getOperationResult());
-    messages.addAll(info.getMessages());
-    status =  info.isSuccessful() && isSuccessful() ? Status.SUCCESS : Status.FAILURE;
+  public boolean isSuccessful() {
+    return status.equals(OperationalStatus.SUCCESS);
   }
 
+  public void mergeResultStatus(MaintenanceManagementInstanceInfo info) {
+    messages.addAll(info.getMessages());
+    status = info.isSuccessful() && isSuccessful() ? OperationalStatus.SUCCESS
+        : OperationalStatus.FAILURE;
+    if (info.hasOperationResult()) {
+      operationResult =
+          this.hasOperationResult() ? operationResult + "," + info.getOperationResult()
+              : info.getOperationResult();
+    }
+  }
 }
--- helix-rest/src/main/java/org/apache/helix/rest/common/datamodel/RestSnapShot.java
+++ helix-rest/src/main/java/org/apache/helix/rest/common/datamodel/RestSnapShot.java
@@ -1,10 +1,13 @@
 package org.apache.helix.rest.common.datamodel;
 
 
+/* This Snapshot can extend Snapshot from common/core module
+ * once there is more generic snapshot.
+ */
 public class RestSnapShot {
   /* An Snapshot object should contain all the Helix related info that an implementation of
-  * OperationAbstractClass would need.
-  */
+   * OperationAbstractClass would need.
+   */
 
   // TODO: Define a Enum class for all Helix info types like ExternalView, InstanceConfig etc. An
   // implementation of OperationAbstractClass will need to define what are the types needed.

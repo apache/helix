5b2fa30aafba0b795c081a57f3ae5ce5b2e25131
Micah Stubbs <micah.stubbs@gmail.com>
2022-05-10 14:46:07 -0700

    Revert "WIP state with new Reactive Form"
    This reverts commit f9e2e375773b8e4428e2a887234d482b591682bb.

--- helix-front/client/app/shared/dialog/input-dialog/input-dialog.component.ts
+++ helix-front/client/app/shared/dialog/input-dialog/input-dialog.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
 import { MAT_DIALOG_DATA } from '@angular/material';
-import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'hi-input-dialog',
@@ -12,8 +11,7 @@ export class InputDialogComponent implements OnInit {
 
   title: string;
   message: string;
-  // values: any[];
-  myForm: FormGroup;
+  values: any[];
 
   constructor(
     @Inject(MAT_DIALOG_DATA) protected data: any,
@@ -23,33 +21,16 @@ export class InputDialogComponent implements OnInit {
   ngOnInit() {
     this.title = (this.data && this.data.title) || 'Input';
     this.message = (this.data && this.data.message) || 'Please enter:';
-    // this.values = (this.data && this.data.values) || {
-    //   'input': {
-    //     label: 'Anything you want',
-    //     type: 'input'
-    //   }
-    // };
-    this.myForm = new FormGroup({
-      values: new FormControl((this.data && this.data.values) || {
-        'input': {
-          label: 'Anything you want',
-          type: 'input'
-        }
-      }) 
-    })
-    console.log(this.myForm)
+    this.values = (this.data && this.data.values) || {
+      'input': {
+        label: 'Anything you want',
+        type: 'input'
+      }
+    };
   }
 
-  // onSubmit() {
-  //   this.dialogRef.close(this.values);
-  // }
-
-  onSubmit(form: FormGroup) {
-    this.dialogRef.close(form.value.values);
-    console.log('Form Valid?', form.valid)
-    console.log('Title', form.value.title)
-    console.log('Message', form.value.message)
-    console.log('Values', JSON.stringify(form.value.values))
+  onSubmit() {
+    this.dialogRef.close(this.values);
   }
 
   onCancel() {

04dee7a631e95246baef3faafad4082a8728509b
Micah Stubbs <micah.stubbs@gmail.com>
2022-05-12 13:30:57 -0700

    NG_DISABLE_VERSION_CHECK=1 npx @angular/cli@9 update @angular/material@9 --force

--- helix-front/client/app/shared/data-table/data-table.component.ts
+++ helix-front/client/app/shared/data-table/data-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { MatDialog } from '@angular/material/dialog';
 
 import { Settings } from '../../core/settings';
 import { InputDialogComponent } from '../dialog/input-dialog/input-dialog.component';
--- helix-front/client/app/shared/node-viewer/node-viewer.component.ts
+++ helix-front/client/app/shared/node-viewer/node-viewer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, Output, ViewChild, ViewEncapsulation, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MatDialog } from '@angular/material';
+import { MatDialog } from '@angular/material/dialog';
 
 import * as _ from 'lodash';
 

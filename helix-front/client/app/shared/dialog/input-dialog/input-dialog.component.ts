30e44d855d1984cc61b188e49332e3515b16122c
Micah Stubbs <micah.stubbs@gmail.com>
2022-05-12 13:30:57 -0700

    NG_DISABLE_VERSION_CHECK=1 npx @angular/cli@9 update @angular/material@9 --force

--- helix-front/client/app/shared/dialog/input-dialog/input-dialog.component.ts
+++ helix-front/client/app/shared/dialog/input-dialog/input-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef } from '@angular/material';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'hi-input-dialog',

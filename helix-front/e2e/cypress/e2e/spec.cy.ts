db136c4ff067fbb38fde3629af23d36217fa40df
Micah Stubbs <micah.stubbs@gmail.com>
2022-06-07 12:04:09 -0700

    remove old protractor files

--- helix-front/e2e/cypress/e2e/spec.cy.ts
+++ helix-front/e2e/cypress/e2e/spec.cy.ts
@@ -1,23 +1,6 @@
-import { HelixUIPage } from './app.po';
-
 describe('helix-ui App', () => {
-  let page: HelixUIPage;
-
-  beforeEach(() => {
-    page = new HelixUIPage();
-  });
-
-  it('should display message contains Cluster', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).contains('Cluster');
-  });
-});
-
-
-// describe('My First Test', () => {
-//   it('Visits the initial project page', () => {
-//     cy.visit('/')
-//     cy.contains('Welcome')
-//     cy.contains('sandbox app is running!')
-//   })
-// })
+  it('Displays a message that contains Cluster', () => {
+    cy.visit('/')
+    cy.contains('Cluster')
+  })
+})

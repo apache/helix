3164d00093894d6ede1f750516ee7089a205f617
Micah Stubbs <micah.stubbs@gmail.com>
2022-06-07 10:54:21 -0700

    add setupJest.ts and remove client/test.ts

---
+++ helix-front/setupJest.ts
@@ -0,0 +1,47 @@
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+import 'jest-preset-angular';
+import 'zone.js';
+import 'zone.js/testing';
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+  {
+    teardown: { destroyAfterEach: true },
+  }
+);
+
+Object.defineProperty(window, 'CSS', { value: null });
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => {
+    return {
+      display: 'none',
+      appearance: ['-webkit-appearance']
+    };
+  }
+});
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>'
+});
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true
+    };
+  }
+});
+
+
+// from old client/test.ts
+
+// // Then we find all the tests.
+// const context = require.context('./', true, /\.spec\.ts$/);
+// // And load the modules.
+// context.keys().map(context);
+

4ca153b7e5a24f9918bdd99fd5068e256db07c6f
Micah Stubbs <micah.stubbs@gmail.com>
2022-09-03 10:44:05 -0700

    add missing moduleNameMapper entry for ace-builds

--- helix-front/jest.config.js
+++ helix-front/jest.config.js
@@ -6,4 +6,7 @@ module.exports = {
   coverageReporters: ['html', 'lcov', 'json', 'text', 'jest-html-reporters'],
   coverageDirectory: './coverage',
   moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  moduleNameMapper: {
+    'ace-builds': '<rootDir>/node_modules/ace-builds/src-noconflict/ace.js',
+  },
 };

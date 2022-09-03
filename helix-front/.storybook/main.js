372b65fdb6c466889dec30d6ab9a7e23275db931
Micah Stubbs <micah.stubbs@gmail.com>
2022-09-03 10:26:22 -0700

    add storybook config

---
+++ helix-front/.storybook/main.js
@@ -0,0 +1,16 @@
+// .storybook/main.js
+
+module.exports = {
+  addons: ['@storybook/addon-essentials'],
+  babel: async (options) => ({
+    // Update your babel configuration here
+    ...options,
+  }),
+  framework: '@storybook/angular',
+  stories: ['../src/**/*.stories.@(js|mdx)'],
+  webpackFinal: async (config, { _configType }) => {
+    // Make whatever fine-grained changes you need
+    // Return the altered config
+    return config;
+  },
+};

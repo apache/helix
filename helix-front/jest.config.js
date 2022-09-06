46309d27c08376a3aa43b569967667f00a71321c
Micah Stubbs <micah.stubbs@gmail.com>
2022-09-06 13:33:44 -0700

    Helix Front storybook visual tests (#2204)
    Add visual tests and config for helix-front.

--- helix-front/jest.config.js
+++ helix-front/jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  roots: ['./client'],
+  roots: ['./src'],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
   collectCoverage: true,
   coverageReporters: ['html', 'lcov', 'json', 'text', 'jest-html-reporters'],
   coverageDirectory: './coverage',
   moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  moduleNameMapper: {
+    'ace-builds': '<rootDir>/node_modules/ace-builds/src-noconflict/ace.js',
+  },
 };

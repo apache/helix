498993d74ee8825eaac748da346b40f1c16df330
Xiaoyuan Lu <xyuanlu@gmail.com>
2022-10-14 15:44:40 -0700

    add comments

--- meta-client/src/main/java/org/apache/helix/metaclient/api/MetaClientInterface.java
+++ meta-client/src/main/java/org/apache/helix/metaclient/api/MetaClientInterface.java
@@ -23,8 +23,10 @@ import java.util.List;
 
 
 public interface MetaClientInterface<T> {
+
   enum EntryMode {
-    //The node will be deleted upon the client's disconnect.
+    //The node will be removed automatically when the session associated with the creation
+    // of the node expires.
     EPHEMERAL,
     //The node will not be automatically deleted upon client's disconnect.
     PERSISTENT
@@ -43,49 +45,114 @@ public interface MetaClientInterface<T> {
 
   //synced CRUD API
   void create(final String key, T data, final EntryMode mode);
+
   void create(final String key, T data, final EntryMode mode, long ttl);
+
   /**
-   * Set data for a given key.
+   * Set the data for the entry of the given key if it exists and the given version matches the
+   * version of the node (if the given version is -1, it matches any node's versions).
    */
   void set(final String key, T data, int version);
+
   /**
    * Update existing data of a given key using an updater. This method will issue a read to get
    * current data and apply updater upon the current data.
-   * return: the updated value.
+   * @param updater : An updater that modifies the entry value.
+   * @return: the updated value.
    */
   T update(String key, DataUpdater<T> updater);
-  Stat exists(final String key);
-  T get(String key);
+
   /**
-   * transactional support
+   * Check if there is an entry for the given key.
+   * @param key
+   * @return return a Stat object if the entry exists. Return null otherwise.
+   */
+  Stat exists(final String key);
+
+  /**
+   * Fetch the data for a given key.
+   * TODO: define exception type when key does not exist
+   */
+  T get(String key);
+
+  /**
+   * API for transaction. The list of operation will be executed as an atomic operation.
+   * @param ops a list of operations. These operations will all be executed or non of them.
+   * @return
    */
   List<OpResult> transactionOP(final Iterable<Op> ops);
+
   /**
-   * For metadata storage that has hierarchical key space (e.g. ZK), the path would be a parent path,
-   * For metadata storage that has non-hierarchical key space (e.g. etcd), the path would be a prefix path.
+   * Return a list of sub entries for the given keys
+   * @param path: For metadata storage that has hierarchical key space (e.g. ZK), the path would be
+   *            a parent path,
+   *            For metadata storage that has non-hierarchical key space (e.g. etcd), the path would
+   *            be a prefix path.
    */
   List<String> getSubEntryKeys(final String path);
+
+  /**
+   * Return the number of sub entries for the given keys
+   * @param path: For metadata storage that has hierarchical key space (e.g. ZK), the path would be
+   *            a parent path,
+   *            For metadata storage that has non-hierarchical key space (e.g. etcd), the path would
+   *            be a prefix path.
+   */
   int countSubEntries(final String path);
+
+  /**
+   * Remove the entry associated with the given key.
+   * For metadata storage that has hierarchical key space, the entry can only be deleted if the key
+   * has no child entry.
+   * TODO: throws
+   * @param path
+   * @return
+   */
   boolean delete(String path);
 
+  /**
+   * Remove the entry associated with the given key.
+   * For metadata storage that has hierarchical key space, remove all its child entries as well
+   * For metadata storage that has non-hierarchical key space, this API is the same as delete()
+   * @param path
+   * @return
+   */
+  boolean recursiveDelete(String path);
 
-  /* async CRUD API */
+  /* Asynchronous methods return immediately.
+   * They take a callback object that will be executed either on successful execution of the request
+   * or on error with an appropriate return code indicating the error.
+   */
   void asyncCreate(final String key, T data, int version, long ttl,
       AsyncCallback.VoidCallback cb);
+
   void asyncSet(final String key, T data, int version, AsyncCallback.VoidCallback cb);
+
   void asyncUpdate(final String key, DataUpdater<T> updater, AsyncCallback.VoidCallback cb);
+
   void asyncGet(final String key, AsyncCallback.DataCallback cb);
-  void countSubEntries(final String path, AsyncCallback.DataCallback cb);
+
+  void asyncCountSubEntries(final String path, AsyncCallback.DataCallback cb);
+
   void asyncExist(final String key, AsyncCallback.StatCallback cb);
+
   void asyncDelete(final String keys, AsyncCallback.VoidCallback cb);
+
   void asyncTransaction(final String keys, AsyncCallback.TransactionCallback cb);
 
-  // batched API. return result to user when all request finishes.
+  /* Batched APIs return result to user when all request finishes.
+   * These calls are not executed as a transaction.
+   */
   boolean[] create(List<String> key, List<T> data, List<EntryMode> mode, List<Long> ttl);
+
   boolean[] set(List<String> keys, List<T> values, List<Integer> version);
+
   List<T> update(List<String> keys, List<DataUpdater<T>> updater);
+
   List<T> get(List<String> keys);
+
   List<Stat> exists(List<String> keys);
+
   boolean[] delete(List<String> keys);
 
 }

8877783a1a4a5e291b6388b6fc2eaaafdeff3f83
Micah Stubbs <mstubbs@linkedin.com>
2022-10-17 13:40:56 -0700

    handle config more cleanly

--- helix-front/server/config.example.ts
+++ helix-front/server/config.example.ts
@@ -5,9 +5,7 @@ export const HELIX_ENDPOINTS = {
     },
   ],
 };
-
 export const SESSION_STORE = undefined;
-
 export const SSL = {
   port: 0,
   keyfile: '',
@@ -15,10 +13,13 @@ export const SSL = {
   passfile: '',
   cafiles: [],
 };
-
 export const LDAP = {
   uri: 'ldap://example.com',
   base: 'DC=example,DC=com',
   principalSuffix: '@example.com',
   adminGroup: 'admin',
 };
+
+export const IDENTITY_TOKEN_SOURCE = undefined; // 'www.example.com';
+export const CUSTOM_IDENTITY_TOKEN_REQUEST_BODY = {};
+export const TOKEN_RESPONSE_KEY = 'token';

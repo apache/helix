6674712a61dabdf4b2f2aff5c4665f258cc8fd31
Qi (Quincy) Qu <qqu@linkedin.com>
2022-10-18 10:57:15 -0700

    Remove manual GC between tests

--- helix-core/src/test/java/org/apache/helix/common/ZkTestBase.java
+++ helix-core/src/test/java/org/apache/helix/common/ZkTestBase.java
@@ -117,7 +117,6 @@ public class ZkTestBase {
   protected static final String CONTROLLER_CLUSTER_PREFIX = "CONTROLLER_CLUSTER";
   protected final String CONTROLLER_PREFIX = "controller";
   protected final String PARTICIPANT_PREFIX = "localhost";
-  private static final long MANUAL_GC_PAUSE = 4000L;
 
   /*
    * Multiple ZK references
@@ -236,10 +235,6 @@ public class ZkTestBase {
     String testClassName = this.getShortClassName();
     System.out.println("BeforeClass: " + testClassName + " called.");
     cleanupJMXObjects();
-    // Giving each test some time to settle (such as gc pause, etc).
-    // Note that this is the best effort we could make to stabilize tests, not a complete solution
-    Runtime.getRuntime().gc();
-    Thread.sleep(MANUAL_GC_PAUSE);
   }
 
   @BeforeMethod

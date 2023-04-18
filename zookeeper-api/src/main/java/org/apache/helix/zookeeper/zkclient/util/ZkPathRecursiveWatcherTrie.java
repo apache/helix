b078ecc2411949dedc5457aadf7592b70f4bb4ef
Xiaoyuan Lu <xyuanlu@gmail.com>
2023-04-17 17:22:00 -0700

    comment

--- zookeeper-api/src/main/java/org/apache/helix/zookeeper/zkclient/util/ZkPathRecursiveWatcherTrie.java
+++ zookeeper-api/src/main/java/org/apache/helix/zookeeper/zkclient/util/ZkPathRecursiveWatcherTrie.java
@@ -1,5 +1,24 @@
 package org.apache.helix.zookeeper.zkclient.util;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -34,7 +53,7 @@ public class ZkPathRecursiveWatcherTrie {
   private final ReadWriteLock lock = new ReentrantReadWriteLock(true);
   // A read lock is acquired on the whole trie for only read access like finding particular
   // trie node or longest prefix
-  private final Lock readLock = lock.readLock();
+  //private final Lock readLock = lock.readLock();
   // A write lock is acquired when modifying the trie.
   private final Lock writeLock = lock.writeLock();
 

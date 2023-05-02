0c2b1dba780067caad7730bf5de6c2c8a806bb43
Komal Desai <98000016+desaikomal@users.noreply.github.com>
2023-05-02 15:11:09 -0700

    Cleanup - ExponenentialBackOff (#2469)
    Co-authored-by: Komal Desai <kdesai@kdesai-mn1.linkedin.biz>

--- zookeeper-api/src/main/java/org/apache/helix/zookeeper/zkclient/util/ExponentialBackoffStrategy.java
+++ zookeeper-api/src/main/java/org/apache/helix/zookeeper/zkclient/util/ExponentialBackoffStrategy.java
@@ -23,31 +23,22 @@ import java.util.Random;
 
 
 public class ExponentialBackoffStrategy {
-  private final long INIT_RETRY_INTERVAL = 500;
-  private final long _maxRetryInterval;
-  private final boolean _addJitter;
-  private final Random _ran;
+  private static long INIT_RETRY_INTERVAL = 500;
 
-  public ExponentialBackoffStrategy(long maxRetryInterval, boolean addJitter) {
-    _maxRetryInterval = maxRetryInterval;
-    _addJitter = addJitter;
-    _ran = new Random(System.currentTimeMillis());
-  }
-
-  public long getNextWaitInterval(int numberOfTriesFailed) {
+  public static long getWaitInterval(long currTime, long maxRetryInterval, boolean addJitter, int numberOfTriesFailed) {
+    Random ran = new Random(currTime);
     double exponentialMultiplier = Math.pow(2.0, numberOfTriesFailed - 1);
     double result = exponentialMultiplier * INIT_RETRY_INTERVAL;
 
-    if (_maxRetryInterval > 0 && result > _maxRetryInterval) {
-      result = _maxRetryInterval;
+    if (maxRetryInterval > 0 && result > maxRetryInterval) {
+      result = maxRetryInterval;
     }
 
-    if (_addJitter) {
+    if (addJitter) {
       // Adding jitter so the real result would be 75% to 100% of the original result.
       // Don't directly add jitter here, since it may exceed the max retry interval setup
-      result = result * (0.75 + _ran.nextDouble() % 0.25);
+      result = result * (0.75 + ran.nextDouble() % 0.25);
     }
-
     return (long) result;
   }
 }

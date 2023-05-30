20cc6ba0d4d3430438acabaf2ad937a0633d569e
Komal Desai <98000016+desaikomal@users.noreply.github.com>
2023-05-30 11:08:20 -0700

    Fix TestZkCacheAsyncOpSingleThread test failure. (#2504)
    Fix TestZkCacheAsyncOpSingleThread test failure.
    ---------
    
    Co-authored-by: Komal Desai <kdesai@kdesai-mn1.linkedin.biz>

--- helix-core/src/test/java/org/apache/helix/manager/zk/TestZkCacheAsyncOpSingleThread.java
+++ helix-core/src/test/java/org/apache/helix/manager/zk/TestZkCacheAsyncOpSingleThread.java
@@ -174,15 +174,10 @@ public class TestZkCacheAsyncOpSingleThread extends ZkUnitTestBase {
 
     // wait zkEventThread update zkCache
     // verify wtCache
-    for (int i = 0; i < 10; i++) {
-      // TestHelper.printCache(accessor._zkCache);
-      ret = TestHelper.verifyZkCache(zkCacheInitPaths, accessor._zkCache._cache, _gZkClient, true);
-      if (ret)
-        break;
-      Thread.sleep(100);
-    }
+    ret = TestHelper.verify(() -> {
+      return TestHelper.verifyZkCache(zkCacheInitPaths, accessor._zkCache._cache, _gZkClient, true);
+      }, 30 * 100);
 
-    // System.out.println("ret: " + ret);
     Assert.assertTrue(ret, "zkCache doesn't match data on Zk");
 
     // update each current state 10 times by external base accessor

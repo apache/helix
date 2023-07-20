9f36c1062743664175118b3b4698aa73600b09d4
Marcos Rico Peng <55266861+Marcosrico@users.noreply.github.com>
2023-07-20 13:05:37 -0400

    Multithreading stress test lattice - CRUD puppies and Listener Tests (#2548)
    ---------
    
    Co-authored-by: mapeng <mapeng@linkedin.com>

---
+++ meta-client/src/test/java/org/apache/helix/metaclient/impl/zk/TestMultiThreadStressTest/DeletePuppy.java
@@ -0,0 +1,67 @@
+package org.apache.helix.metaclient.impl.zk.TestMultiThreadStressTest;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.helix.metaclient.api.MetaClientInterface;
+import org.apache.helix.metaclient.puppy.AbstractPuppy;
+import org.apache.helix.metaclient.puppy.PuppySpec;
+
+import java.util.Random;
+
+public class DeletePuppy extends AbstractPuppy {
+
+  private final Random _random;
+  private final String _parentPath = "/test";
+
+  public DeletePuppy(MetaClientInterface<String> metaclient, PuppySpec puppySpec) {
+    super(metaclient, puppySpec);
+    _random = new Random();
+  }
+
+  @Override
+  protected void bark() {
+    int randomNumber = _random.nextInt(_puppySpec.getNumberDiffPaths());
+    if (shouldIntroduceError()) {
+      try {
+        _metaclient.delete("invalid");
+        _unhandledErrorCounter++;
+      } catch (IllegalArgumentException e) {
+        System.out.println(Thread.currentThread().getName() + " intentionally deleted an invalid path" + " at time: " + System.currentTimeMillis() );
+      }
+    } else {
+      System.out.println(Thread.currentThread().getName() + " is attempting to delete node: " + randomNumber + " at time: " + System.currentTimeMillis());
+      if (_metaclient.delete(_parentPath + "/" + randomNumber)) {
+        System.out.println(Thread.currentThread().getName() + " successfully deleted node " + randomNumber + " at time: " + System.currentTimeMillis());
+        _eventChangeCounterMap.put(String.valueOf(randomNumber), _eventChangeCounterMap.getOrDefault(String.valueOf(randomNumber), 0) + 1);
+      } else {
+        System.out.println(Thread.currentThread().getName() + " failed to delete node " + randomNumber + " at time: " + System.currentTimeMillis() + ", it does not exist");
+      }
+    }
+  }
+
+  @Override
+  protected void cleanup() {
+    _metaclient.recursiveDelete(_parentPath);
+  }
+
+  private boolean shouldIntroduceError() {
+    return _random.nextFloat() < _puppySpec.getErrorRate();
+  }
+}
---
+++ meta-client/src/test/java/org/apache/helix/metaclient/impl/zk/TestMultiThreadStressTest/SetPuppy.java
@@ -0,0 +1,71 @@
+package org.apache.helix.metaclient.impl.zk.TestMultiThreadStressTest;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.helix.metaclient.api.MetaClientInterface;
+import org.apache.helix.metaclient.exception.MetaClientNoNodeException;
+import org.apache.helix.metaclient.puppy.AbstractPuppy;
+import org.apache.helix.metaclient.puppy.PuppySpec;
+
+import java.util.Random;
+
+public class SetPuppy extends AbstractPuppy {
+
+  private final Random _random;
+  private final String _parentPath = "/test";
+
+  public SetPuppy(MetaClientInterface<String> metaclient, PuppySpec puppySpec) {
+    super(metaclient, puppySpec);
+    _random = new Random();
+  }
+
+  @Override
+  protected void bark() {
+    int randomNumber = _random.nextInt(_puppySpec.getNumberDiffPaths());
+    if (shouldIntroduceError()) {
+      try {
+        _metaclient.set("invalid", "test", -1);
+      } catch (IllegalArgumentException e) {
+        System.out.println(Thread.currentThread().getName() + " intentionally called set on an invalid path" + " at time: " + System.currentTimeMillis());
+      }
+    } else {
+      try {
+        System.out.println(Thread.currentThread().getName() + " is attempting to set node: " + randomNumber + " at time: " + System.currentTimeMillis());
+        _metaclient.set(_parentPath + "/" + randomNumber, "test", -1);
+        _eventChangeCounterMap.put(String.valueOf(randomNumber), _eventChangeCounterMap.getOrDefault(String.valueOf(randomNumber), 0) + 1);
+        System.out.println(
+            Thread.currentThread().getName() + " successfully set node " + randomNumber + " at time: "
+                + System.currentTimeMillis());
+      } catch (MetaClientNoNodeException e) {
+        System.out.println(Thread.currentThread().getName() + " failed to set node " + randomNumber + " at time: " + System.currentTimeMillis() + ", it does not exist");
+      }
+    }
+  }
+
+  @Override
+  protected void cleanup() {
+    _metaclient.recursiveDelete(_parentPath);
+  }
+
+  private boolean shouldIntroduceError() {
+    float randomValue = _random.nextFloat();
+    return randomValue < _puppySpec.getErrorRate();
+  }
+}

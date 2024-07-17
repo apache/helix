54f2e784c9065901adca858a51388a5826b69c2f
xyuanlu <xyuanlu@gmail.com>
2024-07-17 16:45:49 -0700

    Add protobuff definition and an empty grpc service  (#2834)
    Add protobuff definition and an empty grpc service

---
+++ helix-gateway/src/main/java/org/apache/helix/gateway/grpcservice/HelixGatewayServiceService.java
@@ -0,0 +1,34 @@
+package org.apache.helix.gateway.grpcservice;
+
+import proto.org.apache.helix.gateway.*;
+import proto.org.apache.helix.gateway.HelixGatewayServiceOuterClass.*;
+import io.grpc.stub.StreamObserver;
+
+public class HelixGatewayServiceService extends HelixGatewayServiceGrpc.HelixGatewayServiceImplBase {
+
+  @Override
+  public StreamObserver<proto.org.apache.helix.gateway.HelixGatewayServiceOuterClass.ShardStateMessage> report(
+      StreamObserver<proto.org.apache.helix.gateway.HelixGatewayServiceOuterClass.TransitionMessage> responseObserver) {
+
+    return new StreamObserver<ShardStateMessage>() {
+
+      @Override
+      public void onNext(ShardStateMessage request) {
+        // called when a client sends a message
+        //....
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        // called when a client sends an error
+        //....
+      }
+
+      @Override
+      public void onCompleted() {
+        // called when the client completes
+        //....
+      }
+    };
+  }
+}

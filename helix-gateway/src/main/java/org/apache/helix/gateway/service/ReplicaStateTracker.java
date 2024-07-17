b151c324e50670b83a8e06b41b15965a5d805f40
xyuanlu <xyuanlu@gmail.com>
2024-07-17 16:45:49 -0700

    Add protobuff definition and an empty grpc service  (#2834)
    Add protobuff definition and an empty grpc service

---
+++ helix-gateway/src/main/java/org/apache/helix/gateway/service/ReplicaStateTracker.java
@@ -0,0 +1,12 @@
+package org.apache.helix.gateway.service;
+
+public class ReplicaStateTracker {
+
+  boolean compareTargetState(){
+    return true;
+  }
+
+  boolean updateReplicaState() {
+    return true;
+  }
+}

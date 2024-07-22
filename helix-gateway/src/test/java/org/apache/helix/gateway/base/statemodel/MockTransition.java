3019872866d1c4fbec9bd5f98c5505964e9771f9
Junkai Xue <junkai.xue@gmail.com>
2024-07-22 13:27:43 -0700

    Build HelixGateway integration test base (#2842)
    This test base contains all the setups including:
    1. Mock participants which hybrid with gateway services.
    2. Controller with start/stop.
    3. Resource creation.
    4. Only Online/Offline state model.
    5. Gateway services with start/stop (now leave empty until gateway service is implemented).
    
    Also start a dummy test to extend test base to make sure all set ups are ready except gateway services.

---
+++ helix-gateway/src/test/java/org/apache/helix/gateway/base/statemodel/MockTransition.java
@@ -0,0 +1,41 @@
+package org.apache.helix.gateway.base.statemodel;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.helix.NotificationContext;
+import org.apache.helix.model.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MockTransition {
+  private static Logger LOG = LoggerFactory.getLogger(MockTransition.class);
+
+  // called by state model transition functions
+  public void doTransition(Message message, NotificationContext context)
+      throws InterruptedException {
+    LOG.info("default doTransition() invoked");
+  }
+
+  // called by state model reset function
+  public void doReset() {
+    LOG.info("default doReset() invoked");
+  }
+
+}

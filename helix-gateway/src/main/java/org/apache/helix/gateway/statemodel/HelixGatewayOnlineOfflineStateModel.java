754dd58a4a79d868db76f34943a105297456f828
xyuanlu <xyuanlu@gmail.com>
2024-07-22 17:28:17 -0700

    Gateway service - service structure dummy class  (#2840)
    Gateway service - service structure dummy class

--- helix-gateway/src/main/java/org/apache/helix/gateway/statemodel/HelixGatewayOnlineOfflineStateModel.java
+++ helix-gateway/src/main/java/org/apache/helix/gateway/statemodel/HelixGatewayOnlineOfflineStateModel.java
@@ -2,13 +2,13 @@ package org.apache.helix.gateway.statemodel;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.helix.NotificationContext;
-import org.apache.helix.gateway.service.ClusterManager;
+import org.apache.helix.gateway.service.GatewayServiceManager;
 import org.apache.helix.model.Message;
 import org.apache.helix.participant.statemachine.StateModel;
 
 public class HelixGatewayOnlineOfflineStateModel extends StateModel {
   private boolean _firstTime = true;
-  private ClusterManager _clusterManager;
+  private GatewayServiceManager _gatewayServiceManager;
 
   private String _resourceName;
   private String _partitionKey;
@@ -16,33 +16,33 @@ public class HelixGatewayOnlineOfflineStateModel extends StateModel {
   private AtomicBoolean _completed;
 
   public HelixGatewayOnlineOfflineStateModel(String resourceName, String partitionKey,
-      ClusterManager clusterManager) {
+      GatewayServiceManager gatewayServiceManager) {
     _resourceName = resourceName;
     _partitionKey = partitionKey;
-    _clusterManager = clusterManager;
+    _gatewayServiceManager = gatewayServiceManager;
   }
 
   public void onBecomeOnlineFromOffline(Message message, NotificationContext context) {
     if (_firstTime) {
-      wait(_clusterManager.sendMessage());
+      wait(_gatewayServiceManager.sendTransitionRequestToApplicationInstance());
       System.out.println(
           "Message for " + message.getPartitionName() + " instance " + message.getTgtName() + " with ADD for "
               + message.getResourceName() + " processed");
       _firstTime = false;
     }
-    wait(_clusterManager.sendMessage());
+    wait(_gatewayServiceManager.sendTransitionRequestToApplicationInstance());
     System.out.println("Message for " + message.getPartitionName() + " instance " + message.getTgtName()
         + " with CHANGE_ROLE_OFFLINE_ONLINE for " + message.getResourceName() + " processed");
   }
 
   public void onBecomeOfflineFromOnline(Message message, NotificationContext context) {
-    wait(_clusterManager.sendMessage());
+    wait(_gatewayServiceManager.sendTransitionRequestToApplicationInstance());
     System.out.println("Message for " + message.getPartitionName() + " instance " + message.getTgtName()
         + " with CHANGE_ROLE_ONLINE_OFFLINE for " + message.getResourceName() + " processed");
   }
 
   public void onBecomeDroppedFromOffline(Message message, NotificationContext context) {
-    wait(_clusterManager.sendMessage());
+    wait(_gatewayServiceManager.sendTransitionRequestToApplicationInstance());
     System.out.println(
         "Message for " + message.getPartitionName() + " instance " + message.getTgtName() + " with REMOVE for "
             + message.getResourceName() + " processed");

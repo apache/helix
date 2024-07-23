6c3845c7c167ab92838d3c1d3ccc89d7f4d9ce00
xyuanlu <xyuanlu@gmail.com>
2024-07-22 17:28:17 -0700

    Gateway service - service structure dummy class  (#2840)
    Gateway service - service structure dummy class

---
+++ helix-gateway/src/main/java/org/apache/helix/gateway/service/HelixGatewayServiceProcessor.java
@@ -0,0 +1,11 @@
+package org.apache.helix.gateway.service;
+
+/**
+ * Translate from/to GRPC function call to Helix Gateway Service event.
+ */
+public interface HelixGatewayServiceProcessor {
+
+  public boolean sendStateTransitionMessage(String instanceName);
+
+  public void sendEventToManager(GatewayServiceManager.GateWayServiceEvent event);
+}

b35dbd9fd646082615ff1bc417b9074d4af9745f
xyuanlu <xyuanlu@gmail.com>
2024-07-25 15:00:12 -0700

    Implement GatewayServiceManager (#2844)
    Implement GatewayServiceManager

--- helix-gateway/src/main/java/org/apache/helix/gateway/util/StateTransitionMessageTranslateUtil.java
+++ helix-gateway/src/main/java/org/apache/helix/gateway/util/StateTransitionMessageTranslateUtil.java
@@ -1,17 +1,68 @@
 package org.apache.helix.gateway.util;
 
-import org.apache.helix.gateway.service.GatewayServiceManager;
-import proto.org.apache.helix.gateway.HelixGatewayServiceOuterClass.TransitionMessage;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.helix.gateway.constant.GatewayServiceEventType;
+import org.apache.helix.gateway.service.GatewayServiceEvent;
+import proto.org.apache.helix.gateway.HelixGatewayServiceOuterClass;
+import proto.org.apache.helix.gateway.HelixGatewayServiceOuterClass.ShardState;
 import proto.org.apache.helix.gateway.HelixGatewayServiceOuterClass.ShardStateMessage;
+import proto.org.apache.helix.gateway.HelixGatewayServiceOuterClass.ShardTransitionStatus;
+import proto.org.apache.helix.gateway.HelixGatewayServiceOuterClass.TransitionMessage;
 
 
 public final class StateTransitionMessageTranslateUtil {
 
-  public static TransitionMessage translateSTMsgToProto() {
+  public static TransitionMessage translateSTMsgToTransitionMessage() {
     return null;
   }
 
-  public static GatewayServiceManager.GateWayServiceEvent translateProtoToSTMsg(ShardStateMessage message) {
-    return null;
+  /**
+   * Translate from user sent ShardStateMessage message to Helix Gateway Service event.
+   */
+  public static GatewayServiceEvent translateShardStateMessageToEvent(ShardStateMessage request) {
+
+    GatewayServiceEvent.GateWayServiceEventBuilder builder;
+    if (request.hasShardState()) { // init connection to gateway service
+      ShardState shardState = request.getShardState();
+      Map<String, String> shardStateMap = new HashMap<>();
+      for (HelixGatewayServiceOuterClass.SingleResourceState resourceState : shardState.getResourceStateList()) {
+        for (HelixGatewayServiceOuterClass.SingleShardState state : resourceState.getShardStatesList()) {
+          shardStateMap.put(resourceState.getResource() + "_" + state.getShardName(), state.getCurrentState());
+        }
+      }
+      builder = new GatewayServiceEvent.GateWayServiceEventBuilder(GatewayServiceEventType.CONNECT).setClusterName(
+          shardState.getClusterName()).setParticipantName(shardState.getInstanceName());
+    } else {
+      ShardTransitionStatus shardTransitionStatus = request.getShardTransitionStatus();
+      // this is status update for established connection
+      List<HelixGatewayServiceOuterClass.SingleShardTransitionStatus> status =
+          shardTransitionStatus.getShardTransitionStatusList();
+      List<GatewayServiceEvent.StateTransitionResult> stResult = new ArrayList<>();
+      for (HelixGatewayServiceOuterClass.SingleShardTransitionStatus shardTransition : status) {
+        GatewayServiceEvent.StateTransitionResult result =
+            new GatewayServiceEvent.StateTransitionResult(shardTransition.getTransitionID(),
+                shardTransition.getCurrentState(), shardTransition.getCurrentState());
+        stResult.add(result);
+      }
+      builder = new GatewayServiceEvent.GateWayServiceEventBuilder(GatewayServiceEventType.UPDATE).setClusterName(
+              shardTransitionStatus.getClusterName())
+          .setParticipantName(shardTransitionStatus.getInstanceName())
+          .setStateTransitionStatusMap(stResult);
+    }
+    return builder.build();
+  }
+
+  /**
+   * Translate termination event to GatewayServiceEvent.
+   */
+
+  public static GatewayServiceEvent translateClientCloseToEvent(String instanceName, String clusterName) {
+    GatewayServiceEvent.GateWayServiceEventBuilder builder =
+        new GatewayServiceEvent.GateWayServiceEventBuilder(GatewayServiceEventType.DISCONNECT).setClusterName(
+            clusterName).setParticipantName(instanceName);
+    return builder.build();
   }
 }

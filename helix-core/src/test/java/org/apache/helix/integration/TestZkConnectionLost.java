b794c458335cc69f973a7402d6965c3064e4495f
Junkai Xue <jxue@linkedin.com>
2024-07-30 12:49:09 -0700

    Fix unstable test testDisconnectWhenConnectionBreak
    Make the thread force interrupted and check the state of thread be terminated then
    start the zk.

--- helix-core/src/test/java/org/apache/helix/integration/TestZkConnectionLost.java
+++ helix-core/src/test/java/org/apache/helix/integration/TestZkConnectionLost.java
@@ -136,7 +136,9 @@ public class TestZkConnectionLost extends TaskTestBase {
       Assert.assertTrue(disconnected.get());
       Assert.assertFalse(controllerManager.isConnected());
     } finally {
-      testThread.interrupt();
+      testThread.stop();
+      TestHelper.verify(() -> testThread.getState().equals(Thread.State.TERMINATED),
+          TestHelper.WAIT_DURATION);
       _zkServerRef.set(TestHelper.startZkServer(_zkAddr, null, false));
     }
   }

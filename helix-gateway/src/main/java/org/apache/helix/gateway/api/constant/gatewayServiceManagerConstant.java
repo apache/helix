3e8e55a51564787c6174d8198c8ea4b1accd2064
xyuanlu <xyuanlu@gmail.com>
2024-10-16 18:24:33 -0700

    add log and refine code style

--- helix-gateway/src/main/java/org/apache/helix/gateway/api/constant/gatewayServiceManagerConstant.java
+++
@@ -1,6 +0,0 @@
-package org.apache.helix.gateway.api.constant;
-
-public class gatewayServiceManagerConstant {
-  public static final String TARGET_STATE_ASSIGNMENT_KEY_NAME = "Assignment";
-  public static final String TIMESTAMP_KEY = "Timestamp";
-}
